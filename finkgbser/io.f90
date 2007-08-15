0d0a3b1eed584907bc33ef58c4946c8b218377a8
jounikarjalainen <>
2007-08-15 07:25:49 +0000

    Changed readstate to use a namelist

--- finkgbser/io.f90
+++ finkgbser/io.f90
@@ -62,17 +62,22 @@ module io
   end subroutine writestate
 
 
-  subroutine readstate(filename,array0,R,Lz)
+
+  subroutine readstate(filename,N,xs,ys,zs,uxs,uys,uzs,rods,R,Lz)
   implicit none
   character(len=*), intent(in) :: filename
-  type(particledat), dimension(:), pointer :: array0
+  integer,intent(out) :: N
+  real(dp),dimension(:),pointer :: xs,ys,zs,uxs,uys,uzs
+  logical,dimension(:),pointer :: rods
   real(dp), intent(out) :: R,Lz
   integer,parameter :: readunit=17   
-  integer :: ios, N,astat,i
-  character(len=3) :: charvar
+  integer :: ios,astat,i
+  character(len=5) :: charvar
   integer,dimension(:),allocatable :: help
+  integer :: GB,Xe
+  character(len=30) :: errorstring="statefile format is incompatible"
     R=0.
-    Lz=0.  
+    Lz=0.       
     !Koittaa avata annetun tiedoston
     open(readunit,file=filename,status='old',iostat=ios)
     if (ios/=0) then
@@ -81,38 +86,56 @@ module io
       stop;
     end if
     !Luetaan hiukkasten lukum‰‰r‰ tiedostosta
-    read(readunit,*) charvar,R,charvar,Lz
-    read(readunit,*) charvar,N
-    read(readunit,*) charvar
+    read(readunit,nml=gbxecyl) 
+    N=partccount;  !write(*,*) N
+    GB=GBcount;    !write(*,*) GB
+    Xe=Xecount;    !write(*,*) Xe
+    R=radius;      !write(*,*) R
+    Lz=height;     !write(*,*) Lz
+    if (R<=0) then 
+      write(*,*) 'readstate: Cylinder radius is not a positive number.'
+      stop;
+    end if
+    if(N/=(GB+Xe)) then
+      write(*,*) 'readstate: GB+Xe/=N, particlecount doesnt match in &
+                 the file',filename
+      stop;
+    end if
     !Varaa muistin taulukolle
-    allocate(array0(N),help(N),stat=astat)
+    allocate(xs(N),ys(N),zs(N),uxs(N),uys(N),uzs(N),rods(N),help(N),stat=astat)
     if (astat/=0) then
-      write(*,*) 'readstate:Virhe varattaessa muistia: array0,help'
+      write(*,*) 'readstate:Virhe varattaessa muistia'
       stop;
     end if
     !Luetaan  hiukkasten x-koordinaatit
-    read(readunit,*) array0(1:N)%x
-    read(readunit,*) charvar
+    read(readunit,*) charvar;  !write(*,*) charvar
+    read(readunit,*) xs(1:N);  
+    read(readunit,*) charvar; !write(*,*) charvar
+    !if(charvar(1:1)/='$') write(*,*) errorstring; stop;  
     !Luetaan y-koordinaatit
-    read(readunit,*) array0(1:N)%y
-    read(readunit,*) charvar,array0(1:N)%z
+    read(readunit,*) ys(1:N)   
+    read(readunit,*) charvar
+    read(readunit,*) zs(1:N)
     !Luetaan rod-tieto
     read(readunit,*) charvar
     read(readunit,*) help(1:N)
     !Luetaan orientaatiovektoreiden komponentit
     read(readunit,*) charvar
-    read(readunit,*) array0(1:N)%ux
-    read(readunit,*) charvar,array0(1:N)%uy
-    read(readunit,*) charvar,array0(1:N)%uz
-    close(readunit) 
+    read(readunit,*) uxs(1:N)
+    read(readunit,*) charvar
+    read(readunit,*) uys(1:N)
+    read(readunit,*) charvar
+    read(readunit,*) uzs(1:N)
+    close(readunit)    
     do i=1,N
       if(help(i)==1) then
-        array0(i)%rod=.true.
+        rods(i)=.true.
       else
-        array0(i)%rod=.false.
+        rods(i)=.false.
       end if
     end do
   end subroutine readstate
+
 
 
   subroutine readerror(filename)
