de5754cecd3e9f9ac6bf6193601fba9144992b85
jounikarjalainen <>
2008-12-18 09:04:30 +0000

    Renamed variables for clarification.

--- gbcyl/src/verlet.f90
+++ gbcyl/src/verlet.f90
@@ -15,15 +15,16 @@ module verlet
   public :: totpairV
 
 
+
   private
 
   real(dp), save :: r_list_
   real(dp), save :: r_cutoff_
   integer, save :: n_particles_
   integer, save :: n_neighbours_
-  real(dp), dimension(:, :), allocatable, target, save :: xyzlist
-  integer, dimension(:), allocatable, target, save :: Nvlist
-  integer, dimension(:, :), allocatable, target, save ::  vlist
+  real(dp), dimension(:, :), allocatable, target, save :: xyz_list_
+  integer, dimension(:), allocatable, target, save :: neighbour_counts_
+  integer, dimension(:, :), allocatable, target, save ::  neighbours_
   integer, save :: n_neighbours_max_
   namelist /verlet_nml/ n_particles_, n_neighbours_, r_list_, r_cutoff_, &
     n_neighbours_max_
@@ -47,8 +48,9 @@ module verlet
     if (n_particles_ < n_neighbours_) then
       n_neighbours_ = n_particles_
     end if
-    allocate(xyzlist(3, n_particles_), vlist(n_particles_, n_neighbours_), & 
-           & Nvlist(n_particles_), stat = astat)
+    allocate(xyz_list_(3, n_particles_), &
+      neighbours_(n_particles_, n_neighbours_), & 
+      neighbour_counts_(n_particles_), stat = astat)
     if (astat /= 0) then
       write (*, *) 'initvlist: Virhe varattaessa muistia.'
       write (*, *) 'Ohjelman suoritus keskeytyy.'
@@ -61,9 +63,9 @@ module verlet
 
   subroutine freevlist()
     implicit none  
-    if (allocated(xyzlist)) deallocate(xyzlist)
-    if (allocated(vlist)) deallocate(vlist)
-    if (allocated(Nvlist)) deallocate(Nvlist)
+    if (allocated(xyz_list_)) deallocate(xyz_list_)
+    if (allocated(neighbours_)) deallocate(neighbours_)
+    if (allocated(neighbour_counts_)) deallocate(neighbour_counts_)
   end subroutine freevlist
 
 
@@ -72,9 +74,9 @@ module verlet
     implicit none
     integer, intent(in) :: write_unit
     write(write_unit, NML = verlet_nml)
-    write(write_unit, *) xyzlist(1:3, 1:n_particles_)
-    write(write_unit, *) Nvlist(1:n_particles_)
-    write(write_unit, *) vlist(1:n_particles_, 1:n_neighbours_)
+    write(write_unit, *) xyz_list_(1:3, 1:n_particles_)
+    write(write_unit, *) neighbour_counts_(1:n_particles_)
+    write(write_unit, *) neighbours_(1:n_particles_, 1:n_neighbours_)
   end subroutine save_state
 
 
@@ -85,12 +87,16 @@ module verlet
     implicit none
     integer, intent(in) :: read_unit
     read(read_unit, NML = verlet_nml)
-    if(.not. allocated(xyzlist)) allocate(xyzlist(3, n_particles_))
-    read(read_unit, *) xyzlist(1:3, 1:n_particles_)
-    if(.not. allocated(Nvlist)) allocate(Nvlist(n_particles_))
-    read(read_unit, *) Nvlist(1:n_particles_)
-    if(.not. allocated(vlist)) allocate(vlist(n_particles_, n_neighbours_))
-    read(read_unit, *) vlist(1:n_particles_, 1:n_neighbours_)
+    if(.not. allocated(xyz_list_)) allocate(xyz_list_(3, n_particles_))
+    read(read_unit, *) xyz_list_(1:3, 1:n_particles_)
+    if(.not. allocated(neighbour_counts_)) then 
+      allocate(neighbour_counts_(n_particles_))
+    end if
+    read(read_unit, *) neighbour_counts_(1:n_particles_)
+    if(.not. allocated(neighbours_)) then
+      allocate(neighbours_(n_particles_, n_neighbours_))
+    end if
+    read(read_unit, *) neighbours_(1:n_particles_, 1:n_neighbours_)
   end subroutine load_state
 
 
@@ -104,14 +110,11 @@ module verlet
     real(dp), intent(out) :: Vtot 
     real(dp) :: pairE
     logical, intent(out) :: ovrlp
-    integer,dimension(:,:), pointer :: verletl
-    integer,dimension(:), pointer :: nvlist
     Vtot = 0.0
-    call getvlist(verletl, nvlist)
     do i=1, n_particles
-      if(nvlist(i) == 0) cycle;
-      do jj = 1, nvlist(i)
-        j = verletl(i, jj)
+      if(neighbour_counts_(i) == 0) cycle;
+      do jj = 1, neighbour_counts_(i)
+        j = neighbours_(i, jj)
         if(j <= i) cycle;
         if (rij(particles(i), particles(j)) < r_cutoff_) then 
           call pairV(particles(i), particles(j), pairE, ovrlp)
@@ -124,14 +127,14 @@ module verlet
 
 
 
-  !Asettaa osoittimen vlistp naapurilistaan, sekä osoittimen Nvlistp
-  !naapurien lukumäärän sisältävään listan
-  subroutine getvlist(vlistp, Nvlistp)
+  !! Asettaa osoittimen neighbours_p naapurilistaan, sekä osoittimen 
+  !! neighbour_counts_p naapurien lukumäärän sisältävään listaan.
+  subroutine getvlist(neighbours_p, neighbour_counts_p)
     implicit none
-    integer, dimension(:,:), pointer :: vlistp
-    integer, dimension(:), pointer :: Nvlistp
-    vlistp=>vlist
-    Nvlistp=>Nvlist 
+    integer, dimension(:,:), pointer :: neighbours_p
+    integer, dimension(:), pointer :: neighbour_counts_p
+    neighbours_p=>neighbours_
+    neighbour_counts_p=>neighbour_counts_ 
   end subroutine getvlist
 
 
@@ -143,9 +146,9 @@ module verlet
     integer, intent(in) :: n_particles
     real(dp),dimension(:,:), pointer :: xyz
     integer :: i, j
-    xyz=>xyzlist
+    xyz=>xyz_list_
     do i = 1, n_particles
-      Nvlist(i) = 0
+      neighbour_counts_(i) = 0
       xyz(1, i) = particles(i)%x
       xyz(2, i) = particles(i)%y
       xyz(3, i) = particles(i)%z
@@ -153,10 +156,10 @@ module verlet
     do i = 1, (n_particles-1)
       do j = (i+1), n_particles
          if(rij(particles(i), particles(j)) < r_list_ ) then
-           Nvlist(i) = Nvlist(i)+1
-           Nvlist(j) = Nvlist(j)+1
-           vlist(i, Nvlist(i)) = j
-           vlist(j, Nvlist(j)) = i
+           neighbour_counts_(i) = neighbour_counts_(i)+1
+           neighbour_counts_(j) = neighbour_counts_(j)+1
+           neighbours_(i, neighbour_counts_(i)) = j
+           neighbours_(j, neighbour_counts_(j)) = i
          end if
       end do
     end do
@@ -175,9 +178,9 @@ module verlet
 
     dmax1=0.0
     do i=1, n_particles
-      oldxyz%x = xyzlist(1,i)
-      oldxyz%y = xyzlist(2,i)
-      oldxyz%z = xyzlist(3,i)
+      oldxyz%x = xyz_list_(1,i)
+      oldxyz%y = xyz_list_(2,i)
+      oldxyz%z = xyz_list_(3,i)
       oldxyz%ux = 0.0
       oldxyz%uy = 0.0
       oldxyz%uz = 0.0
