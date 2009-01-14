a89d003ad3b59d309e6dcb0cf4ae2f223da6364f
jounikarjalainen <>
2009-01-14 15:39:01 +0000

    Made tau1-parameter bracketing but compiling results in internal compiler error.

--- gbcyl-analysis/src/tau1_module.f90
+++ gbcyl-analysis/src/tau1_module.f90
@@ -5,28 +5,43 @@ use orientational_ordering
 use particle, only: particledat
 implicit none
 
+
+
 PRIVATE 
 
-  public :: tau1
+  public :: tau1_routine
 
+  
+
+
+  interface 
+    function tau1(rs, n, d)
+    use nrtype, only: dp
+    real(dp), dimension(:), intent(in) :: rs
+    integer, intent(in) :: n
+    real(dp), intent(in) :: d
+    end function
+  end interface
+
+
+
+  real(dp), dimension(:), allocatable :: rs_
   real(dp), dimension(3), save :: director_
-  type(particledat), dimension(:), save :: particles_
   integer, save :: n_particles_
-
+ 
 contains
 
-
-
-  subroutine tau1(particles, n_particles, value, layer_distance, direction)
+  subroutine tau1_routine(particles, n_particles, value, layer_distance, &
+    direction)
     implicit none
     type(particledat), dimension(:), intent(in) :: particles
     integer, intent(in) :: n_particles
     real(dp), intent(out) :: value
     real(dp), intent(out) :: layer_distance
-    real(dp), intent(out) :: direction
+    real(dp), dimension(3), intent(out) :: direction
     real(dp) :: ax, bx, cx, fa, fb, fc 
-    particles_ = particles
-    n_particles_ = n_particles
+    integer :: i
+    real(dp), dimension(3) :: position
     !! 1. choose direction by calculating the order parameter and the 
     !!    corresponding eigenvector
     call orientation_parameter(particles, n_particles, value, director_)
@@ -34,20 +49,30 @@ contains
     !! 2.1. select initial points     
     !!      (how? Must be given by user if cannot be automated robustly.)
     !! 2.2. bracket with mnbrak
+    n_particles_ = n_particles
+    if(.not. allocated(rs_)) allocate(rs_(n_particles_))
+    do i = 1, n_particles_
+      position = (/particles(i)%x, particles(i)%y, particles(i)%z/)
+      rs_(i) = dot_product(position, director_)
+    end do
     ax = 1.0
     bx = 2.0
-    call mnbrak(ax, bx, cx, fa, fb, fc, -tau1)
+    call mnbrak(ax, bx, cx, fa, fb, fc, -tau1_unary)
     !! (2.3. if needed, improve with brent.)
     direction = director_
     value = fb
     layer_distance = bx
-  end subroutine tau1
+  end subroutine tau1_routine
 
 
 
-  function tau1(d) result(value)
+  function tau1_unary(layer_distance) 
     implicit none
-    
+    real(dp), intent(in) :: layer_distance
+    real(dp) :: tau1_unary 
+    tau1_unary = tau1(rs_, n_particles_, layer_distance)   
   end function
+
+
 
 end module tau1_module
