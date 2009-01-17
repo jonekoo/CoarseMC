36a932f5832cbb7d6bfcc5e5e48993d45c92529c
jounikarjalainen <>
2009-01-17 15:03:36 +0000

    Moved sweep back to mc_sweep module because it needs private data of the module.

--- gbcyl/src/mc_sweep.f90
+++ gbcyl/src/mc_sweep.f90
@@ -1,7 +1,7 @@
 module mc_sweep
   use nrtype, only : dp
   use energy, only: total_energy, potential_energy
-  use cylinder, only: setLz, setR, scale
+  use cylinder, only: setLz, setR, scale, volume, getHeight
   use particle, only : particledat, move, setmaxmoves, getmaxmoves
   use mtmod, only : grnd
   use verlet, only : updatelist, totpairV
@@ -9,14 +9,14 @@ module mc_sweep
 
 
   public :: init
-!  public :: sweep
+  public :: sweep
   public :: save_state
   public :: load_state
   public :: updatemaxvalues
 
 
 
-!!  private 
+  private 
 
   integer, save :: n_accepted_moves_
   integer, save :: n_accepted_scalings_
@@ -59,6 +59,60 @@ module mc_sweep
     integer, intent(in) :: read_unit
     read(read_unit, NML = mcsweep_nml)
   end subroutine
+
+
+
+  subroutine sweep(particles, n_particles)
+    implicit none
+    intrinsic log, real
+    type(particledat), dimension(:), intent(inout) :: particles
+    integer, intent(in) :: n_particles
+    integer :: i
+    real(dp) :: Eold = 0.0, Enew = 0.0, totEold = 0.0, totEnew = 0.0
+    real(dp) :: Lz0, Lzn
+    real(dp) :: V0, Vn
+    logical :: accept = .true.
+    logical :: overlap
+    type(particledat) :: newparticle 
+    !! Trial moves of particles
+    do i = 1, n_particles
+      Enew = 0
+      Eold = 0
+      overlap = .false.
+      call move(particles(i), newparticle)
+      call potential_energy(particles, n_particles, newparticle, i, Enew, & 
+        overlap)
+      if(.not. overlap) then 
+        call potential_energy(particles, n_particles, particles(i), i, & 
+          Eold, overlap)   
+        accept = acceptchange(Eold, Enew, overlap)       
+        if(accept) then
+          particles(i) = newparticle
+          n_accepted_moves_ = n_accepted_moves_ + 1
+        end if
+      end if 
+    end do
+    !! Trial volume change
+    overlap = .false.
+    V0 = volume()
+    Lz0 = getHeight()
+    Lzn = Lz0 + (2.0 * grnd() - 1.0) * max_scaling_
+    call changeLz(particles, n_particles, Lz0, Lzn);
+    Vn = volume()
+    call totpairV(particles, n_particles, totEnew, overlap)
+    call changeLz(particles, n_particles, Lzn, Lz0);
+    if (overlap) return; !! If the new state results in an overlap, 
+                         !! old state is restored
+    call totpairV(particles, n_particles, totEold, overlap)  
+    totEnew = totEnew+pressure_*Vn-real(n_particles)*temperature_*log(Vn)   
+    totEold = totEold+pressure_*V0-real(n_particles)*temperature_*log(V0)
+    accept = acceptchange(totEold, totEnew, overlap)
+    if (accept) then
+      call changeLz(particles, n_particles, Lz0, Lzn);
+      n_accepted_scalings_ = n_accepted_scalings_ + 1
+    end if 
+    call updatelist(particles, n_particles)
+  end subroutine sweep
 
 
 
