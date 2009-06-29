3b5ed148997a12528fbc5fbe2c771100e8819ea1
jounikarjalainen <>
2009-06-29 15:13:46 +0000

    Changed input from command line arguments to a namelist.

--- gbcyl/src/gbcyl.f90
+++ gbcyl/src/gbcyl.f90
@@ -1,34 +1,43 @@
+
 program gbcyl
-  use mc_engine
+  use mc_engine, only: init, run, finalize, equilibration_sweeps, &
+    & production_sweeps, read_restart
   implicit none
   intrinsic command_argument_count, get_command_argument
-  character(len=32) :: argument
-  character(len=32) :: start_type
-  integer :: n_equilibration_sweeps
-  integer :: n_production_sweeps
-  integer :: n_args
-  n_args = command_argument_count()
-  if(n_args > 0) then
-    call get_command_argument(1, argument) 
-    start_type = argument
-    if(start_type == "restart") then
-      call read_restart   
-      if(n_args == 3) then
-        call get_command_argument(2, argument)
-        read(argument, *) n_equilibration_sweeps
-        call equilibration_sweeps(n_equilibration_sweeps)
-        call get_command_argument(3, argument)
-        read(argument, *) n_production_sweeps
-        call production_sweeps(n_production_sweeps)
-      end if
-    else
-      !! Print usage
-    end if
-  else 
+  character(len=32) :: restart = 'no'
+  integer :: n_equilibration_sweeps = -1
+  integer :: n_production_sweeps = -1
+  namelist /start/ restart, n_production_sweeps, n_equilibration_sweeps
+!!  integer :: n_args
+!!  n_args = command_argument_count()
+!!  if(n_args > 0) then
+!!    call get_command_argument(1, argument) 
+!!    start_type = argument
+!!    if(start_type == "restart") then
+!!      call read_restart   
+!!      if(n_args == 3) then
+!!        call get_command_argument(2, argument)
+!!        read(argument, *) n_equilibration_sweeps
+!!        call equilibration_sweeps(n_equilibration_sweeps)
+!!        call get_command_argument(3, argument)
+!!        read(argument, *) n_production_sweeps
+!!        call production_sweeps(n_production_sweeps)
+!!      end if
+!!    else
+!!      !! Print usage
+!!    end if
+!!  else 
+!!    call init
+!!  end if
+  read(*, nml = start)
+  if (restart == 'yes') then
+    if (0 .le. n_equilibration_sweeps) &
+      call equilibration_sweeps(n_equilibration_sweeps)
+    if (0 .le. n_production_sweeps) &
+      call production_sweeps(n_production_sweeps) 
+  else
     call init
-  end if
+  end if 
   call run 
   call finalize
 end program gbcyl
-
-
