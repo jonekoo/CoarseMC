71b5a8454c97241cc8ef7ebfbf5f04611ed8e2d3
jounikarjalainen <>
2009-10-13 20:26:56 +0000

    Added repwall and attwall to the module. Removed old save, load and initialization routines. Added documentation. Added routine for saving parameters with parameter_writer. Added routine for reading parameters with parameterizer. Added poly_box support. Removed dependency from cylinder module. Cosmetics.

--- gbcyl/src/particlewall.f90
+++ gbcyl/src/particlewall.f90
@@ -3,181 +3,200 @@
 !! liittyviä parametreja ja aliohjelmia.  
 
 module particlewall
-  use cylinder,only : getRadius
-  use particle, only : particledat
+  use particle, only: particledat 
   use nrtype
+  use class_poly_box
+  use class_parameterizer
+  use class_parameter_writer
   implicit none
+  private
 
   public :: initptwall
-  public :: save_state
-  public :: load_state
-  public :: prtclwallV  
+  public :: particlewall_potential
   public :: repwall
   public :: attwall
   public :: gbwall
-
-  private
+  public :: particlewall_write_parameters
     
-  !! :TODO: Give these better names and document.
+  !! @see Micheletti et. al. Journal of Chemical Physics 123, 224705 for 
+  !! definitions of these parameters.
   !!
-  real(dp), save :: alpha_A_, alpha_B_, K_w_
-  real(dp), parameter :: LJdist=1.7 
-    !LJdist = GB-partikkelin vuorovaikutuskeskuksen etäisyydet
-    !partikkelin keskipisteestä.  
-  logical, save :: ua
-  namelist /particlewall_nml/ alpha_A_, alpha_B_, K_w_, ua
+  real(dp), save :: alpha_A_ = 1._dp
+  real(dp), save :: alpha_B_ = 1._dp
+  real(dp), save :: K_w_ = 8._dp
+  
+  !! The distance of LJ interaction sites from the Gay-Berne particle center
+  !! along the unique axis.
+  real(dp), save :: LJdist = 1.7_dp 
+  logical, save :: is_uniform_alignment = .false.
 
-
-
-
-  interface 
-    function repwall(x, rc, sig)
-    use nrtype
-    use nr, only : rf, rd
-    real(dp),intent(in) :: x, rc, sig
-    real(dp) :: repwall
-    end function
+  interface initptwall
+    module procedure initptwall_parameterizer
   end interface
-  
-  interface
-    function attwall(x,rc,sig)
-    use nrtype
-    use nr, only : rf, rd
-    real(dp), intent(in) :: x, rc, sig
-    real(dp) :: attwall
-    end function attwall
-  end interface 
-  
+
+  interface particlewall_potential
+    module procedure gbwall
+  end interface
 
 contains 
 
+  subroutine initptwall_parameterizer(reader)
+    type(parameterizer), intent(in) :: reader
+    call get_parameter(reader, 'Kw', K_w_)
+    call get_parameter(reader, 'alpha_A', alpha_A_) 
+    call get_parameter(reader, 'alpha_B', alpha_B_) 
+    call get_parameter(reader, 'Kw', K_w_)
+    call get_parameter(reader, 'LJ_dist', LJdist) 
+    call get_parameter(reader, 'is_uniform_alignment', is_uniform_alignment)
+  end subroutine
 
+  subroutine particlewall_write_parameters(writer)
+    type(parameter_writer), intent(in) :: writer
+    call write_comment(writer, 'Particle-wall interaction parameters')
+    call write_parameter(writer, 'alpha_A', alpha_A_) 
+    call write_parameter(writer, 'alpha_B', alpha_B_) 
+    call write_parameter(writer, 'Kw', K_w_)
+    call write_parameter(writer, 'LJ_dist', LJdist) 
+    call write_parameter(writer, 'is_uniform_alignment', is_uniform_alignment)
+  end subroutine
 
-  !! Alustaa modulin arvolla i:
-  !! 1=random planar
-  !! 2=uniform alignment
-  !! 3=weak homeotropic
-  subroutine initptwall(i, strength)
+  subroutine gbwall(gbparticle, simbox, Eptwall,ovrlp)
     implicit none
-    integer, intent(in) :: i
-    real(dp) :: strength
-    K_w_ = strength
-    select case(i)
-    case(1)
-      ua = .false.
-      alpha_A_ = 1.0
-      alpha_B_ = 1.0
-    case(2)
-      ua = .true.
-      alpha_A_ = 1.0
-      alpha_B_ = 1.0
-    case(3)
-      ua = .false.
-      alpha_A_ = 1.0
-      alpha_B_ = 0.0
-      K_w_ = 2*K_w_      
-    case default 
-      write (*,*) 'Virhe alustettaessa modulia particlewall.'
-      write (*,*) 'initialize(i) , i /= {1, 2, 3}'
-    end select    
-  end subroutine initptwall
-
-
-
-  subroutine save_state(write_unit)
-    implicit none
-    integer, intent(in) :: write_unit
-    write(write_unit, NML = particlewall_nml)
-  end subroutine save_state
-
-
-
-  subroutine load_state(read_unit)
-    implicit none
-    integer, intent(in) :: read_unit
-    read(read_unit, NML = particlewall_nml)
-  end subroutine load_state
-
-
-
-  !Palauttaa yhden partikkelin ja seinän välisen 
-  !vuorovaikutuksen energian
-  subroutine prtclwallV(prtcl,ptwallE,ovrlp)
-    implicit none
-    real(dp), intent(out) :: ptwallE
-    logical, intent(out) :: ovrlp
-    type(particledat), intent(in) :: prtcl
-    ovrlp=.false.
-    ptwallE=0.0
-    if(prtcl%rod) then 
-      call gbwall(prtcl,ptwallE,ovrlp)
-    !else 
-    !  call xewall(prtcl,ptwallE,ovrlp)
-    end if    
-  end subroutine prtclwallV
-
-
-
-  !Palauttaa GB-partikkelin ja seinän välisen
-  !vuorovaikutuksen energian. rwcut=GB-partikkelin vuorovaikutuskeskuksen
-  !pienin mahdollinen etäisyys sylinterin seinästä. Syy: muuten ylivuoto
-  !hypergeometrisissa
-  subroutine gbwall(prtcldata, Eptwall,ovrlp)
-    implicit none
-    type(particledat), intent(in) :: prtcldata
+    type(particledat), intent(in) :: gbparticle
+    type(poly_box), intent(in) :: simbox
     real(dp), intent(out) :: Eptwall
     logical, intent(out) :: ovrlp
-    real(dp),parameter :: sig=1.0
-    real(dp) :: rsiteA,rsiteB
-    real(dp) :: repulA,attracA,repulB,attracB,fu,Rc
-    real(dp) :: rAperR,rBperR
-    real(dp),parameter :: rperRcut=0.999
-    real(dp),parameter :: rwcut=0.4 
+    real(dp), parameter :: sig=1._dp
+    real(dp) :: rsiteA, rsiteB
+    real(dp) :: repulA, attracA, repulB, attracB, fu, Rc
+    real(dp) :: rAperR, rBperR
     ovrlp = .false.
-    Eptwall = 0.0   
-    Rc = getradius()
-    call rArB(prtcldata, rsiteA, rsiteB)
+    Eptwall = 0._dp
+    Rc = get_x(simbox)/2._dp !! :TODO: Remove this dependency by making a cylinder wall object.
+    call rArB(gbparticle, rsiteA, rsiteB)
     if(rsiteA >= Rc .or. rsiteB >= Rc) then
       ovrlp = .true.
       return;
     end if
-    rAperR  = rsiteA/Rc
-    rBperR  = rsiteB/Rc
-    attracA = attwall(rAperR,Rc,sig)
-    repulA  = repwall(rAperR,Rc,sig)
-    attracB = attwall(rBperR,Rc,sig)
-    repulB  = repwall(rBperR,Rc,sig)
-    if(.not. ua) then
-      fu = 1.0
-    else if (ua) then
-      fu = angular(prtcldata)  
+    rAperR  = rsiteA / Rc
+    rBperR  = rsiteB / Rc
+    attracA = attwall(rAperR, Rc, sig)
+    repulA  = repwall(rAperR, Rc, sig)
+    attracB = attwall(rBperR, Rc, sig)
+    repulB  = repwall(rBperR, Rc, sig)
+    if (is_uniform_alignment) then
+      fu = angular(gbparticle)  
+    else 
+      fu = 1._dp
     end if
-    Eptwall = fu*K_w_*((repulA-alpha_A_*attracA)+(repulB-alpha_B_*attracB))     
+    Eptwall = fu * K_w_ * ((repulA - alpha_A_ * attracA) + (repulB - alpha_B_ &
+      * attracB))     
   end subroutine gbwall
 
-  !Palauttaa partikkelin ja seinän välisten
-  !vuorovaikutuskeskusten etäisyyden sylinterin keskipisteestä.
-  subroutine rArB(particle,rA,rB)
+  !! Returns the distances of the interaction sites from the axis of the 
+  !! cylinder.
+  !! 
+  !! @p particle the particle of which interaction sites are calcula 
+  !! @p r_a the distance of site A from the cylinder center
+  !! @p rb the distance of site B from the cylinder center
+  subroutine rarb(particle, r_a, r_b)
     implicit none
     intrinsic sqrt
     type(particledat), intent(in) :: particle
-    real(dp),intent(out) :: rA,rB
-    real(dp) :: xA,yA,xB,yB
-   
-    xA=particle%x+LJdist*particle%ux
-    yA=particle%y+LJdist*particle%uy
-    xB=particle%x-LJdist*particle%ux
-    yB=particle%y-LJdist*particle%uy
-    rA=sqrt(xA**2 + yA**2)
-    rB=sqrt(xB**2 + yB**2)
-  end subroutine rArB
+    real(dp), intent(out) :: r_a, r_b
+    real(dp) :: x_a, y_a, x_b, y_b
+    x_a=particle%x+LJdist*particle%ux
+    y_a=particle%y+LJdist*particle%uy
+    x_b=particle%x-LJdist*particle%ux
+    y_b=particle%y-LJdist*particle%uy
+    r_a=sqrt(x_a**2 + y_a**2)
+    r_b=sqrt(x_b**2 + y_b**2)
+  end subroutine
 
 
 
   real(dp) function angular(particle)
     implicit none
-    type(particledat),intent(in) :: particle
+    type(particledat), intent(in) :: particle
     angular=(particle%uz)**2
   end function angular
+
+
+
+!! Calculates the absolute value of the repulsive part of the interaction
+!! between a smooth infinitely thick cylinder shape wall and a Lennard-Jones
+!! particle inside it. 
+!! x=r/rc where r=particles distance from the center of the cylinder,
+!! rc=radius of the cylinder
+!! sigwall=contact distance between the particle and the wall
+!!
+function repwall(xa, rc, sigwall)
+use nrtype
+use nr, only : rf, rd
+implicit none
+real(dp), intent(in) :: xa, rc, sigwall
+real(dp) :: repwall
+real(dp), dimension(5) :: as, bs
+real(dp) :: pa, pb, z
+real(dp) :: rfk, rdk
+real(dp) :: Ek, Kk, coeff
+real(dp) :: cc, q
+interface 
+function horner(a, n, x) 
+  use nrtype
+  real(dp), dimension(:), intent(in) :: a
+  integer, intent(in) :: n 
+  real(dp), intent(in) :: x
+  real(dp) :: horner
+end function horner
+end interface
+  z = xa**2
+  cc = 0._dp
+  q = 1._dp - z
+  rfk = real(rf(real(cc, sp), real(q, sp), 1._sp), dp)
+  rdk = real(rd(real(cc, sp), real(q, sp), 1._sp), dp)
+  Ek = rfk - z * rdk / 3._dp   !! Complete elliptic integrals of the second and
+  Kk = rfk                     !! first kind
+
+  !! as are the coefficients for the polynomial in equation (11) and
+  !! bs are the coefficients for the polynomial in equation (12).
+  !! pa and pb are the corresponding polynomial values. 
+  !! They have been multiplied by 315 and divided by two just to make things 
+  !! look a bit cleaner.  
+  !! Coefficient of the highest order term is first on the array as required by
+  !! the horner function. 
+  as(1:5) = (/35._dp, 4052._dp, 16434._dp, 11156._dp, 1091._dp/)
+  bs(1:5) = (/1400._dp, 6304._dp, -1200._dp, -5728._dp, -776._dp/)
+  
+  pa = horner(as, 5, z)
+  pb = horner(bs, 5, z)
+  coeff = 4._dp * atan(1._dp) * (sigwall**12) / (128._dp * 45._dp * ((rc) * &
+  (1._dp - z))**9)
+  repwall = coeff * (pa * Ek + pb * Kk)
+end function repwall
+
+
+
+function attwall(xa, rc, sigwall)
+use nrtype
+use nr, only : rf, rd
+implicit none
+real(dp), intent(in) :: xa, rc, sigwall
+real(dp) :: attwall
+real(dp) :: coeff, z, Ek, Kk
+real(dp) :: rfk, rdk
+real(dp) :: cc, q
+z = xa**2
+cc = 0._dp
+q = 1._dp - z
+rfk = real(rf(real(cc, sp), real(q, sp), 1._sp), dp)
+rdk = real(rd(real(cc, sp), real(q, sp), 1._sp), dp)
+Ek = rfk - z * rdk / 3._dp
+Kk = rfk
+coeff = 4._dp * atan(1._dp) * sigwall / (12._dp * (rc * (1._dp - z))**3)
+attwall = coeff * ((7._dp + z) * Ek + 4._dp * (z - 1._dp) * Kk)
+end function attwall
+
+
 
 end module particlewall
