061d115e798f3cf7e6b29b5d0386ed0fd7bbb923
jounikarjalainen <>
2009-10-13 19:09:09 +0000

    Added documentation to subroutines. Changed list update criterion to a more transparent one. Added support for poly_box type. Added support for list initialization with parameterization. Added support for parameter writing with parameter_writer. Removed old save and load routines. Moved pairV from particle to here.

--- gbcyl/src/verlet.f90
+++ gbcyl/src/verlet.f90
@@ -1,47 +1,92 @@
-!Verlet'n listan käsittelyyn tarvittavia muuttujia ja aliohjelmia. 
 module verlet
+  !! Module for calculating pair interactions of particles using a Verlet 
+  !! neighbour list. 
   use nrtype, only: dp
-  use particle, only: particledat, rij, differences, pairV
+  use particle, only: particledat, position, getmaxmoves
+  use class_poly_box
+  use gayberne, only: potential
+  use class_parameterizer
+  use class_parameter_writer
   implicit none 
-
-
+  private  
 
   public :: initvlist
   public :: updatelist
   public :: freevlist
-  public :: save_state
-  public :: load_state
-  public :: totpairV
-  public :: singleparticleV
-
-
-
-  private
-
+  public :: pairV
+  public :: pair_interactions
+  public :: newlist
+  public :: verlet_write_parameters
+ 
   real(dp), save :: r_list_
   real(dp), save :: r_cutoff_
   integer, save :: n_particles_
   integer, save :: n_neighbours_
-  real(dp), dimension(:, :), allocatable, target, save :: xyz_list_
-  integer, dimension(:), allocatable, target, save :: neighbour_counts_
-  integer, dimension(:, :), allocatable, target, save ::  neighbours_
+  real(dp), dimension(:, :), allocatable, save :: xyz_list_
+  integer, dimension(:), allocatable, save :: neighbour_counts_
+  integer, dimension(:, :), allocatable, save ::  neighbours_
   integer, save :: n_neighbours_max_
   namelist /verlet_nml/ n_particles_, n_neighbours_, r_list_, r_cutoff_, &
     n_neighbours_max_
 
+  interface pair_interactions
+    module procedure totpairV, singleparticleV
+  end interface
 
+  interface initvlist
+    module procedure initvlist_parameterizer, initvlist_r
+  end interface
 
   contains
 
-  !Alustaa naapurilistan
-  subroutine initvlist(particles, n_particles)
-    implicit none
+  !! Writes verlet list parameters to parameter file.
+  !!
+  !! @p writer writes the parameters given by the routine
+  !! 
+  subroutine verlet_write_parameters(writer)
+    type(parameter_writer), intent(in) :: writer
+    call write_comment(writer, 'Verlet neighbourlist parameters')
+    call write_parameter(writer, 'r_list', r_list_)
+    call write_parameter(writer, 'r_cutoff', r_cutoff_)
+  end subroutine
+
+  !! Initializes the verlet neighbour list.
+  !! 
+  !! @p particles the array of particles
+  !! @p n_particles the number of particles
+  !! @p simbox the simulation cell
+  !! @p reader gives the parameters
+  !! 
+  subroutine initvlist_parameterizer(particles, n_particles, simbox, reader)
+    type(particledat), dimension(:), intent(in) :: particles
+    integer :: n_particles
+    type(poly_box), intent(in) :: simbox
+    type(parameterizer), intent(in) :: reader
+    real(dp) :: r_list
+    real(dp) :: r_cutoff
+    call get_parameter(reader, 'r_list', r_list)
+    call get_parameter(reader, 'r_cutoff', r_cutoff)
+    call initvlist(particles, n_particles, simbox, r_list, r_cutoff)
+  end subroutine
+
+  !! Initializes the verlet neighbour list. 
+  !! 
+  !! @p particles the array of particles
+  !! @p n_particles the number of particles in @p particles
+  !! @p simbox the simulation cell
+  !! @p r_list the radius of the neighbourlist
+  !! @p r_cutoff the cutoff radius of the pair interactions
+  !! 
+  subroutine initvlist_r(particles, n_particles, simbox, r_list, r_cutoff)
     intrinsic min
     type(particledat), dimension(:), intent(in) :: particles
     integer, intent(in) :: n_particles
+    type(poly_box), intent(in) :: simbox
+    real(dp), intent(in) :: r_list
+    real(dp), intent(in) :: r_cutoff
     integer :: astat
-    r_list_ = 6.8
-    r_cutoff_ = 5.5
+    r_list_ = r_list
+    r_cutoff_ = r_cutoff
     n_neighbours_max_ = 500
     n_particles_ = n_particles
     n_neighbours_ = min(n_particles_, n_neighbours_max_) 
@@ -52,178 +97,176 @@ module verlet
       neighbours_(n_particles_, n_neighbours_), & 
       neighbour_counts_(n_particles_), stat = astat)
     if (astat /= 0) then
-      write (*, *) 'initvlist: Virhe varattaessa muistia.'
-      write (*, *) 'Ohjelman suoritus keskeytyy.'
-      stop;
+      stop 'Could not allocate memory for verlet list.'
     end if
-    call newlist(particles, n_particles)
-  end subroutine initvlist
+    call newlist(particles, n_particles, simbox)
+  end subroutine
 
-
-
+  !! Frees the arrays needed by the verlet list.
+  !! 
   subroutine freevlist()
-    implicit none  
     if (allocated(xyz_list_)) deallocate(xyz_list_)
     if (allocated(neighbours_)) deallocate(neighbours_)
     if (allocated(neighbour_counts_)) deallocate(neighbour_counts_)
   end subroutine freevlist
 
-
-
-  subroutine save_state(write_unit)
-    implicit none
-    integer, intent(in) :: write_unit
-    write(write_unit, NML = verlet_nml)
-    write(write_unit, *) xyz_list_(1:3, 1:n_particles_)
-    write(write_unit, *) neighbour_counts_(1:n_particles_)
-    write(write_unit, *) neighbours_(1:n_particles_, 1:n_neighbours_)
-  end subroutine save_state
-
-
-
-  !! :TODO: probably needs checking if the tables have already been
-  !! allocated and to which size they are allocated.
-  subroutine load_state(read_unit)
-    implicit none
-    integer, intent(in) :: read_unit
-    read(read_unit, NML = verlet_nml)
-    if(.not. allocated(xyz_list_)) allocate(xyz_list_(3, n_particles_))
-    read(read_unit, *) xyz_list_(1:3, 1:n_particles_)
-    if(.not. allocated(neighbour_counts_)) then 
-      allocate(neighbour_counts_(n_particles_))
-    end if
-    read(read_unit, *) neighbour_counts_(1:n_particles_)
-    if(.not. allocated(neighbours_)) then
-      allocate(neighbours_(n_particles_, n_neighbours_))
-    end if
-    read(read_unit, *) neighbours_(1:n_particles_, 1:n_neighbours_)
-  end subroutine load_state
-
-
-
-  !Palauttaa parivuorovaikutuksen kokonaisenergian
-  subroutine totpairV(particles, n_particles, Vtot, ovrlp)
-    implicit none
+  !! Calculates the total pair interaction energy of all @p particles. Uses
+  !! Verlet neighbour list. 
+  !!
+  !! @p particles the array of all particles
+  !! @p n_particles the number of particles
+  !! @p simbox the simulation cell
+  !! @p Vtot the total pair interaction energy
+  !! @p overlap true if some particles overlap with each other
+  !! 
+  subroutine totpairV(particles, n_particles, simbox, Vtot, overlap)
     type(particledat), dimension(:), intent(in) :: particles
     integer, intent(in) :: n_particles
-    integer :: i,j,jj
+    type(poly_box), intent(in) :: simbox
+    integer :: i, j, jj
     real(dp), intent(out) :: Vtot 
     real(dp) :: pairE
-    logical, intent(out) :: ovrlp
-    Vtot = 0.0
-    do i=1, n_particles
-      if(neighbour_counts_(i) == 0) cycle;
+    logical, intent(out) :: overlap
+    Vtot = 0._dp
+    overlap = .false.
+    call updatelist(particles, n_particles, simbox)
+    do i = 1, n_particles
+      if(neighbour_counts_(i) == 0) cycle
       do jj = 1, neighbour_counts_(i)
         j = neighbours_(i, jj)
-        if(j <= i) cycle;
-        if (rij(particles(i), particles(j)) < r_cutoff_) then 
-          call pairV(particles(i), particles(j), pairE, ovrlp)
-          if (ovrlp) return;
-          Vtot = Vtot+pairE      
-        end if 
+        if(j <= i) cycle
+        call pairV(particles(i), particles(j), simbox, pairE, overlap)
+        if (overlap) return
+        Vtot = Vtot + pairE      
       end do
     end do
-  end subroutine totpairV
+  end subroutine
 
-
-
-  !! Laskee hiukkasen i vuorovaikutusenergian muiden hiukkasten
-  !! kanssa
-  subroutine singleparticleV(particles, n_particles, particlei, i, &
-    singleV, ovrlp)
-    implicit none
-    integer, intent(in) :: i
-    type(particledat),dimension(:), intent(in) :: particles
+  !! Calculates the interaction energy of @p particlei with all the other 
+  !! particles.
+  !!
+  !! @p particles the array of particles
+  !! @p n_particles the number of particles in @p particles
+  !! @p simbox the simulation cell
+  !! @p particlei the particle to which the interactions are calculated
+  !! @p i the index of @p particlei in @p particles
+  !! @p singleV the interaction energy of @p particlei with other particles.
+  !! @p overlap true if particlei and some other particle overlap
+  !!
+  subroutine singleparticleV(particles, n_particles, simbox, particlei, i, &
+    singleV, overlap)
+    type(particledat), dimension(:), intent(in) :: particles
     integer, intent(in) :: n_particles
     type(particledat), intent(in) :: particlei
+    integer, intent(in) :: i
+    type(poly_box), intent(in) :: simbox
     real(dp), intent(out) :: singleV
-    logical, intent(out) :: ovrlp
+    logical, intent(out) :: overlap
     integer :: j
-    real(dp) :: pairE = 0.0
+    real(dp) :: pairE
+    real(dp) :: distance_moved 
     type(particledat) :: particlevij    
-    singleV = 0.0
+    singleV = 0._dp
+    overlap = .false.
+    distance_moved = min_distance(simbox, xyz_list_(1:3, i), &
+    position(particlei))
+    if(distance_moved > 0.5_dp * (r_list_ - r_cutoff_)) then
+      call newlist(particles, n_particles, simbox)
+    end if
     do j = 1, neighbour_counts_(i)
       particlevij = particles(neighbours_(i, j))
-      if (rij(particlei, particlevij) < r_cutoff_) then 
-        call pairV(particlei, particlevij, pairE, ovrlp)
-        if (ovrlp) return;
+      call pairV(particlei, particlevij, simbox, pairE, overlap)
+      if (overlap) then
+        exit
+      else
         singleV = singleV + pairE
       end if
     end do
-  end subroutine singleparticleV
+  end subroutine
 
+  !! Calculates the interaction energy of a pair of particles. 
+  !! 
+  !! @p particlei the first particle
+  !! @p particlej the second particle
+  !! @p simbox the simulation cell
+  !! @p potE the interaction energy
+  !! @p overlap is true if the two particles overlap each other
+  !! 
+  !! :TODO: put this somewhere else. Problem is that you need to initialize 
+  !! the potential module to use this. 
+  !! 
+  subroutine pairV(particlei, particlej, simbox, potE, overlap)
+    type(particledat), intent(in) :: particlei 
+    type(particledat), intent(in) :: particlej
+    type(poly_box), intent(in) :: simbox
+    real(dp), intent(out) :: potE
+    logical, intent(out) :: overlap
+    real(dp), dimension(3) :: rij, ui, uj
+    potE = 0._dp
+    overlap = .false.
+    rij = min_image(simbox, position(particlei), position(particlej))
+    if (sqrt(dot_product(rij, rij)) < r_cutoff_) then
+      ui(1)=particlei%ux
+      ui(2)=particlei%uy
+      ui(3)=particlei%uz
+      uj(1)=particlej%ux
+      uj(2)=particlej%uy
+      uj(3)=particlej%uz
+      call potential(ui, uj, rij, potE, overlap)  
+    end if
+  end subroutine pairV
 
-
-  !! Asettaa osoittimen neighbours_p naapurilistaan, sekä osoittimen 
-  !! neighbour_counts_p naapurien lukumäärän sisältävään listaan.
-  subroutine getvlist(neighbours_p, neighbour_counts_p)
-    implicit none
-    integer, dimension(:,:), pointer :: neighbours_p
-    integer, dimension(:), pointer :: neighbour_counts_p
-    neighbours_p=>neighbours_
-    neighbour_counts_p=>neighbour_counts_ 
-  end subroutine getvlist
-
-
-
-  !Muodostaa uuden naapurilistan hiukkastaulukosta particles
-  subroutine newlist(particles, n_particles)
-    implicit none
+  !! Makes a new Verlet neighbourlist
+  !!
+  !! @p particles the array of particles
+  !! @p n_particles the number of particles
+  !! @p simbox the simulation cell
+  !!
+  subroutine newlist(particles, n_particles, simbox)
     type(particledat), dimension(:), intent(in) :: particles
     integer, intent(in) :: n_particles
-    real(dp),dimension(:,:), pointer :: xyz
+    type(poly_box), intent(in) :: simbox
     integer :: i, j
-    xyz=>xyz_list_
+    real(dp) :: r_ij
     do i = 1, n_particles
       neighbour_counts_(i) = 0
-      xyz(1, i) = particles(i)%x
-      xyz(2, i) = particles(i)%y
-      xyz(3, i) = particles(i)%z
+      xyz_list_(1, i) = particles(i)%x
+      xyz_list_(2, i) = particles(i)%y
+      xyz_list_(3, i) = particles(i)%z
     end do 
-    do i = 1, (n_particles-1)
-      do j = (i+1), n_particles
-         if(rij(particles(i), particles(j)) < r_list_ ) then
-           neighbour_counts_(i) = neighbour_counts_(i)+1
-           neighbour_counts_(j) = neighbour_counts_(j)+1
-           neighbours_(i, neighbour_counts_(i)) = j
-           neighbours_(j, neighbour_counts_(j)) = i
-         end if
+    do i = 1, n_particles - 1
+      do j = i + 1, n_particles
+        r_ij = min_distance(simbox, position(particles(i)), &
+        position(particles(j)))
+        if(r_ij < r_list_ ) then
+          neighbour_counts_(i) = neighbour_counts_(i) + 1
+          neighbour_counts_(j) = neighbour_counts_(j) + 1 
+          neighbours_(i, neighbour_counts_(i)) = j
+          neighbours_(j, neighbour_counts_(j)) = i
+        end if
       end do
     end do
   end subroutine newlist 
 
-
-
-  !Päivittää naapurilistan tarvittaessa
-  subroutine updatelist(particles, n_particles)
-    implicit none
+  !! Updates the neighbourlist if needed.
+  !!
+  !! @p particles the array of particles to be included in the list
+  !! @p n_particles the number of particles in @p particles
+  !! @p simbox the simulation cell 
+  !!
+  subroutine updatelist(particles, n_particles, simbox)
+    intrinsic max, abs
     type(particledat), dimension(:), intent(in) :: particles     
     integer, intent(in) :: n_particles
+    type(poly_box), intent(in) :: simbox
     integer :: i
-    real(dp) :: dmax1 = 0.0, dx, dy, dz
-    type(particledat) :: oldxyz
-
-    dmax1=0.0
-    do i=1, n_particles
-      oldxyz%x = xyz_list_(1,i)
-      oldxyz%y = xyz_list_(2,i)
-      oldxyz%z = xyz_list_(3,i)
-      oldxyz%ux = 0.0
-      oldxyz%uy = 0.0
-      oldxyz%uz = 0.0
-      oldxyz%rod = .true.
-      call differences(oldxyz, particles(i), dx, dy, dz)
-      dx = abs(dx)
-      dy = abs(dy)
-      dz = abs(dz)
-      dmax1 = max(dmax1, dx, dy, dz)
-      if(dmax1 > (0.29*(r_list_-r_cutoff_))) then
-        call newlist(particles, n_particles)
-        exit;
+    do i = 1, n_particles
+      if(min_distance(simbox, xyz_list_(1:3, i), position(particles(i))) > &
+      0.5_dp * (r_list_ - r_cutoff_)) then
+        call newlist(particles, n_particles, simbox)
+        exit
       end if
     end do  
   end subroutine updatelist
-
-
 
 end module verlet
