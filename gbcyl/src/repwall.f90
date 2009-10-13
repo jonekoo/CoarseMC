49226717e2a345ed6ff0be4d3fe39ee800c89610
jounikarjalainen <>
2009-10-13 19:16:35 +0000

    Cosmetics.

--- gbcyl/src/repwall.f90
+++ gbcyl/src/repwall.f90
@@ -5,38 +5,38 @@
 !! rc=radius of the cylinder
 !! sigwall=contact distance between the particle and the wall
 
-function repwall(xa,rc,sigwall)
+function repwall(xa, rc, sigwall)
 use nrtype
-use nr, only : rf,rd
+use nr, only : rf, rd
 implicit none
-real(dp), intent(in) :: xa,rc,sigwall
+real(dp), intent(in) :: xa, rc, sigwall
 real(dp) :: repwall
-real(dp), dimension(5) :: as,bs
-real(dp) :: pa,pb,z
-real(sp) :: rfk,rdk
-real(dp) :: Ek,Kk,coeff
-real(sp) :: cc,q
-  interface 
-  function horner(a,n,x) 
-    use nrtype
-    real(dp), dimension(:), intent(in) :: a
-    integer, intent(in) :: n 
-    real(dp), intent(in) :: x
-    real(dp) :: horner
-  end function horner
-  end interface
-
-z=xa**2
-cc=0.0
-q=1.0-z
-rfk=rf(cc,q,1.0_sp)
-rdk=rd(cc,q,1.0_sp)
-Ek=rfk-z*rdk/3.0_dp   !! Complete elliptic integrals of the second and
-Kk=rfk                      !! first kind
-as(1:5)=(/35.0_dp,4052.0_dp,16434.0_dp,11156.0_dp,1091.0_dp/)
-bs(1:5)=(/1400.0_dp,6304.0_dp,-1200.0_dp,-5728.0_dp,-776.0_dp/)
-pa=horner(as,5,z)
-pb=horner(bs,5,z)
-coeff=4.0_dp*atan(1.0)*(sigwall**12)/(128.0_dp*45.0_dp*((rc)*(1.0_dp-z))**9)
-repwall=coeff*(pa*Ek+pb*Kk)
+real(dp), dimension(5) :: as, bs
+real(dp) :: pa, pb, z
+real(dp) :: rfk, rdk
+real(dp) :: Ek, Kk, coeff
+real(dp) :: cc, q
+interface 
+function horner(a, n, x) 
+  use nrtype
+  real(dp), dimension(:), intent(in) :: a
+  integer, intent(in) :: n 
+  real(dp), intent(in) :: x
+  real(dp) :: horner
+end function horner
+end interface
+  z = xa**2
+  cc = 0._dp
+  q = 1._dp - z
+  rfk = real(rf(real(cc, sp), real(q, sp), 1._sp), dp)
+  rdk = real(rd(real(cc, sp), real(q, sp), 1._sp), dp)
+  Ek = rfk - z * rdk / 3._dp   !! Complete elliptic integrals of the second and
+  Kk = rfk                     !! first kind
+  as(1:5) = (/35._dp, 4052._dp, 16434._dp, 11156._dp, 1091._dp/)
+  bs(1:5) = (/1400._dp, 6304._dp, -1200._dp, -5728._dp, -776._dp/)
+  pa = horner(as, 5, z)
+  pb = horner(bs, 5, z)
+  coeff = 4._dp * atan(1._dp) * (sigwall**12) / (128._dp * 45._dp * ((rc) * &
+  (1._dp - z))**9)
+  repwall = coeff * (pa * Ek + pb * Kk)
 end function repwall
