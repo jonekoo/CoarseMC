9a3978b5420e023b188fe7b186304b37ce092109
jounikarjalainen <>
2009-11-19 09:59:17 +0000

    Added public interfaces to avoid problems when including modules that have subroutines with the same name.

--- gbcyl/src/box.f90
+++ gbcyl/src/box.f90
@@ -1,8 +1,29 @@
 module box
 use nrtype
+use particle
 implicit none
 
+public :: boxdat
+public :: new_box
+public :: make_box
+public :: create_box
+public :: min_image
+public :: min_distance
+public :: get_x, get_y, get_z
+public :: set_x
+public :: set_y
+public :: set_z
+public :: volume
+public :: scale
+public :: set_xperiodicity
+public :: set_yperiodicity
+public :: set_zperiodicity
+public :: write_to_stdio
+
+private
+
 type boxdat
+  private
   real(dp) :: lx
   real(dp) :: ly
   real(dp) :: lz
@@ -11,10 +32,79 @@ type boxdat
   logical :: zperiodic    
 end type boxdat
 
+interface min_image
+  module procedure min_image_1, min_image_2
+end interface
+
+interface min_distance
+  module procedure box_min_distance
+end interface
+
+interface scale
+  module procedure box_scale
+end interface
+
+interface set_x
+  module procedure box_set_x
+end interface
+
+interface set_y
+  module procedure box_set_y
+end interface
+
+interface set_z
+  module procedure box_set_z
+end interface
+
+interface get_x
+  module procedure box_get_x
+end interface
+
+interface get_y
+  module procedure box_get_y
+end interface
+
+interface get_z
+  module procedure box_get_z
+end interface
+
+interface set_xperiodicity
+  module procedure box_set_xperiodicity
+end interface
+
+interface set_yperiodicity
+  module procedure box_set_yperiodicity
+end interface
+
+interface set_zperiodicity
+  module procedure box_set_zperiodicity
+end interface
+
+interface write_to_stdio
+  module procedure box_write_to_stdio
+end interface
+
+interface volume
+  module procedure box_volume
+end interface
+
+interface make_box
+  module procedure box_make_box, box_copy
+end interface
+
+interface create_box
+  module procedure box_create_box
+end interface
+
 contains 
 
   !! :TODO: Assignment operator, if needed. 
 
+  function new_box(side) result(b)
+    type(boxdat) :: b
+    real(dp), intent(in) :: side
+    call box_make_box(b, side)
+  end function
 
   !! Returns a "well-defined" periodic cubic box with sidelengths @p side.
   !! This routine is meant to be a initializing routine after which 
@@ -24,43 +114,155 @@ contains
   !!
   !! @return a cubic box.
   !!  
-  type(boxdat) function make_box(side)
-  implicit none
-  real(dp), intent(in) :: side
-    make_box%lx = side
-    make_box%ly = side
-    make_box%lz = side
-    make_box%xperiodic = .true.
-    make_box%yperiodic = .true.
-    make_box%zperiodic = .true.
-  end function
+  subroutine box_make_box(simbox, side) 
+    type(boxdat), intent(out) :: simbox
+    real(dp), intent(in) :: side
+    simbox%lx = side
+    simbox%ly = side
+    simbox%lz = side
+    simbox%xperiodic = .true.
+    simbox%yperiodic = .true.
+    simbox%zperiodic = .true.
+  end subroutine
 
+  subroutine box_copy(copy, simbox)
+    type(boxdat), intent(out) :: copy
+    type(boxdat), intent(in) :: simbox
+    copy = simbox
+  end subroutine
 
+  subroutine box_create_box(bp, box_string)
+    type(boxdat), intent(inout) :: bp
+    character(len = *), intent(in) :: box_string
+    character(len = 5) :: type_id
+    integer :: ios
+    read(box_string, *, iostat = ios) type_id, bp%lx, bp%ly, bp%lz, bp%xperiodic, bp%yperiodic, bp%zperiodic
+    if (ios /= 0) then
+      write(6, *) 'box_create_box: failed to read box with iostat = ', ios, '. Stopping.'
+      stop 
+    else if('box' /= type_id) then
+      write(6, *) 'box_create_box: Warning converting from ', trim(adjustl(type_id)), 'to box!'
+    end if
+  end subroutine
 
-  real(dp) function box_x(a_box)
+  real(dp) pure function box_get_x(a_box)
     type(boxdat), intent(in) :: a_box
-    box_x = a_box%lx
+    box_get_x = a_box%lx
   end function
 
-
-
-  real(dp) function box_y(a_box)
+  real(dp) pure function box_get_y(a_box)
     type(boxdat), intent(in) :: a_box
-    box_y = a_box%ly
+    box_get_y = a_box%ly
   end function
 
-
-
-  real(dp) function box_z(a_box)
+  real(dp) pure function box_get_z(a_box)
     type(boxdat), intent(in) :: a_box
-    box_z = a_box%lz
+    box_get_z = a_box%lz
   end function
 
+  subroutine box_set_x(a_box, x)
+    implicit none
+    type(boxdat), intent(inout) :: a_box
+    real(dp), intent(in) :: x
+    a_box%lx = x
+  end subroutine 
+  
+  subroutine box_set_y(a_box, y)
+    implicit none
+    type(boxdat), intent(inout) :: a_box
+    real(dp), intent(in) :: y
+    a_box%ly = y
+  end subroutine 
+  
+  subroutine box_set_z(a_box, z)
+    implicit none
+    type(boxdat), intent(inout) :: a_box
+    real(dp), intent(in) :: z
+    a_box%lz = z
+  end subroutine 
 
+  subroutine box_set_xperiodicity(a_box, is_periodic)
+    type(boxdat), intent(inout) :: a_box
+    logical, intent(in) :: is_periodic
+    a_box%xperiodic = is_periodic
+  end subroutine
 
-  !!subroutine box_x(a_box, lx)
-  !!  type(boxdat, intent(in) :: a_box
-  !!  a_box%lx = x
-  !!end subroutine box_x
+  subroutine box_set_yperiodicity(a_box, is_periodic)
+    type(boxdat), intent(inout) :: a_box
+    logical, intent(in) :: is_periodic
+    a_box%yperiodic = is_periodic
+  end subroutine
+
+  subroutine box_set_zperiodicity(a_box, is_periodic)
+    type(boxdat), intent(inout) :: a_box
+    logical, intent(in) :: is_periodic
+    a_box%zperiodic = is_periodic
+  end subroutine
+
+  pure function box_volume(simbox)
+    real(dp) :: box_volume
+    type(boxdat), intent(in) :: simbox
+    box_volume = simbox%lx * simbox%ly * simbox%lz
+  end function
+
+  function box_min_distance(simbox, r_i, r_j)
+    real(dp) :: box_min_distance
+    type(boxdat), intent(in) :: simbox
+    real(dp), dimension(3), intent(in) :: r_i
+    real(dp), dimension(3), intent(in) :: r_j
+    real(dp), dimension(3) :: r_ij
+    r_ij = min_image(simbox, r_i, r_j)
+    box_min_distance = sqrt(dot_product(r_ij, r_ij))
+  end function 
+
+  function min_image_2(simbox, r_i, r_j) result(r_ij)
+    real(dp), dimension(3) :: r_ij
+    type(boxdat), intent(in) :: simbox
+    real(dp), dimension(3), intent(in) :: r_i
+    real(dp), dimension(3), intent(in) :: r_j
+    r_ij = r_j - r_i
+    r_ij = min_image_1(simbox, r_ij)
+  end function
+
+  recursive function min_image_1(simbox, r)
+    implicit none
+    real(dp), dimension(3) :: min_image_1
+    type(boxdat), intent(in) :: simbox
+    real(dp), dimension(3), intent(in) :: r
+    !! Make periodic transformations
+    min_image_1 = r
+    if (simbox%xperiodic) then
+      min_image_1(1) = r(1) - simbox%lx * anint(r(1)/simbox%lx)
+    end if
+    if (simbox%yperiodic) then
+      min_image_1(2) = r(2) - simbox%ly * anint(r(2)/simbox%ly)
+    end if
+    if (simbox%zperiodic) then
+      min_image_1(3) = r(3) - simbox%lz * anint(r(3)/simbox%lz)
+    end if
+  end function
+
+  subroutine box_scale(simbox, max_scaling, rng)
+    type(boxdat), intent(inout) :: simbox
+    real(dp), intent(in) :: max_scaling
+    real(dp) :: dx, dy, dz
+    interface
+      function rng()
+        real(8) :: rng
+      end function
+    end interface
+    !! Make isotropic scaling. How to implement other kinds?
+    dx = (2._dp * real(rng(), dp) - 1._dp) * max_scaling
+    call set_x(simbox, get_x(simbox) + dx)  
+    dy = (2._dp * real(rng(), dp) - 1._dp) * max_scaling
+    call set_y(simbox, get_y(simbox) + dy)  
+    dz = (2._dp * real(rng(), dp) - 1._dp) * max_scaling
+    call set_z(simbox, get_z(simbox) + dz)  
+  end subroutine
+
+  subroutine box_write_to_stdio(simbox)
+    type(boxdat), intent(in) :: simbox
+    write(*,*) simbox
+  end subroutine
 
 end module box
--- gbcyl/src/cylinder.f90
+++ gbcyl/src/cylinder.f90
@@ -1,134 +1,187 @@
 module cylinder
-  use nrtype
-
-  real(dp), private, save :: radius_
-  real(dp), private, save :: height_
-  namelist /cylinder_nml/ radius_, height_
-  PRIVATE :: cylinder_nml
+use nrtype
+use box
+implicit none
   
+type cylinderdat
+  private
+  type(boxdat) :: b
+end type cylinderdat
 
+interface min_image
+  module procedure cylinder_min_image
+end interface
+
+interface min_distance
+  module procedure cylinder_min_distance
+end interface
+
+interface scale
+  module procedure cyl_scale
+end interface
+
+interface write_to_stdio
+  module procedure cylinder_write_to_stdio
+end interface
+
+interface set_x
+  module procedure cylinder_set_x
+end interface
+
+interface set_y
+  module procedure cylinder_set_y
+end interface
+
+interface set_z
+  module procedure cylinder_set_z
+end interface
+
+interface get_x
+  module procedure cylinder_get_x
+end interface
+
+interface get_y
+  module procedure cylinder_get_y
+end interface
+
+interface get_z
+  module procedure cylinder_get_z
+end interface
+
+interface make_box
+  module procedure cylinder_make_box, cylinder_copy
+end interface
+
+interface volume
+  module procedure cylinder_volume
+end interface
+
+interface create_box
+  module procedure cylinder_create_box
+end interface
 
   contains
+  
+  function new_cylinder(diameter, height) result(nc)
+    type(cylinderdat), pointer :: nc
+    real(dp), intent(in) :: diameter, height
+    allocate(nc)
+    call make_box(nc, diameter, height)
+  end function
+   
+  subroutine cylinder_make_box(cylbox, diameter, height) 
+    type(cylinderdat), intent(out) :: cylbox
+    real(dp), intent(in) :: diameter
+    real(dp), intent(in) :: height
+    call make_box(cylbox%b, diameter)
+    call set_z(cylbox, height)
+    call set_x(cylbox, diameter)
+    call set_xperiodicity(cylbox%b, .false.)
+    call set_yperiodicity(cylbox%b, .false.)
+  end subroutine
 
+  subroutine cylinder_copy(copy, cylbox) 
+    type(cylinderdat), intent(out) :: copy
+    type(cylinderdat), intent(in) :: cylbox
+    copy = cylbox
+  end subroutine
 
+  subroutine cylinder_create_box(cylbox, box_string)
+    type(cylinderdat), intent(out) :: cylbox
+    character(len = *), intent(in) :: box_string
+    call create_box(cylbox%b, box_string)
+  end subroutine
 
-  !Alustaa sylinterin säteen ja korkeuden
-  subroutine initcylinder(radius,height)
-    implicit none
-    real(dp),intent(in) :: radius, height
-    radius_=radius
-    height_=height
-  end subroutine initcylinder
-
-
- 
-  subroutine save_state(write_unit)
-    implicit none
-    integer, intent(in) :: write_unit
-    write(write_unit, NML=cylinder_nml)
-  end subroutine save_state
-
-
-
-  subroutine load_state(read_unit)
-    implicit none
-    integer, intent(in) :: read_unit
-    read(read_unit, NML=cylinder_nml)
-  end subroutine load_state
-
-
-
-  !Palauttaa tämän sylinterin tilavuuden
-  real(dp) pure function volume()
-    implicit none
-    intrinsic atan
-    real(dp) :: pi
-    pi=4.0*atan(1.0)
-    volume = pi*radius_**2*height_
-  end function volume
-
-
-
-  !Muuttaa tämän sylinterin sädettä
-  subroutine setR(newR)
-    implicit none
-    real(dp),intent(in) :: newR
-    radius_=newR 
-  end subroutine setR
-
-
-
-  subroutine setLz(newLz)
-    implicit none
-    real(dp), intent(in) :: newLz
-    height_=newLz
-  end subroutine setLz
-
-
-
-  !Skaalaa koordinaatit uudelle R:lle, kun vanha R on R0
-  subroutine scale(x0,y0,xnew,ynew,R0,Rnew)
-    implicit none
-    real(dp),intent(in) :: x0, y0, R0,Rnew
-    real(dp),intent(out) :: xnew, ynew
-    xnew=Rnew/R0*x0
-    ynew=Rnew/R0*y0      
-  end subroutine scale
-
-
-
-  !Palauttaa etäisyyden sylinterin akselilta
-  real(dp) pure function rdist(x, y)
-    implicit none
-    real(dp), intent(in) :: x, y
-    rdist=sqrt(x**2+y**2)
-  end function rdist
-
-
-
-  !Palauttaa annetut karteesiset koordinaatit
-  !sylinterikoordinaateissa. z-koordinaatti on
-  !tässä sylinterissä perioidinen
-  subroutine cylindrical(x,y,z,r,phi,zc)
-    implicit none
-    intrinsic atan2,nint
-    real(dp), intent(in) :: x,y,z
-    real(dp), intent(out) :: r, phi, zc 
-    r=rdist(x,y)
-    phi=atan2(y,x)
-    zc=z-nint(z/height_)*height_
-  end subroutine cylindrical
-
-
-
-  !Sylinterin ominaisuuksiin liittyviä aliohjelmia ja funktioita. 
-
-  !Palauttaa tämän sylinterin säteen
-  pure function getradius() result(radius)
-    implicit none
+  pure function radius(cylbox)
     real(dp) :: radius
-    radius=radius_
-  end function getradius
+    type(cylinderdat), intent(in) :: cylbox
+    radius = get_x(cylbox) / 2._dp
+  end function
 
-
-
-  !Palauttaa tämän sylinterin korkeuden
-  pure function getHeight() result(height)
-    implicit none
+  pure function height(cylbox)
     real(dp) :: height
-    height=height_
-  end function getHeight
+    type(cylinderdat), intent(in) :: cylbox
+    height = get_z(cylbox)
+  end function
 
+  pure function cylinder_get_x(cylbox)
+    real(dp) :: cylinder_get_x
+    type(cylinderdat), intent(in) :: cylbox
+    cylinder_get_x = get_x(cylbox%b)
+  end function
 
+  pure function cylinder_get_y(cylbox)
+    real(dp) :: cylinder_get_y
+    type(cylinderdat), intent(in) :: cylbox
+    cylinder_get_y = get_y(cylbox%b)
+  end function
 
-  pure function shellVolume(rad,drad) result(vol)
-    implicit none
+  pure function cylinder_get_z(cylbox)
+    real(dp) :: cylinder_get_z
+    type(cylinderdat), intent(in) :: cylbox
+    cylinder_get_z = get_z(cylbox%b)
+  end function
+
+  subroutine cylinder_set_x(cylbox, x)
+    type(cylinderdat), intent(inout) :: cylbox
+    real(dp), intent(in) :: x
+    call set_x(cylbox%b, x)
+    call set_y(cylbox%b, x)
+  end subroutine
+
+  subroutine cylinder_set_y(cylbox, y)
+    type(cylinderdat), intent(inout) :: cylbox
+    real(dp), intent(in) :: y
+    call set_x(cylbox%b, y)
+    call set_y(cylbox%b, y)
+  end subroutine
+
+  subroutine cylinder_set_z(cylbox, z)
+    type(cylinderdat), intent(inout) :: cylbox
+    real(dp), intent(in) :: z
+    call set_z(cylbox%b, z)
+  end subroutine
+
+  pure function cylinder_volume(cylbox)
     intrinsic atan
-    real(dp),intent(in) :: rad,drad
-    real(dp) :: Pi,vol
- 
-    Pi=4*atan(1.0)
-    vol=2.0*Pi*height_*rad*drad
-  end function shellVolume
+    real(dp) :: cylinder_volume
+    type(cylinderdat), intent(in) :: cylbox
+    real(dp) :: pi
+    pi = 4._dp * atan(1._dp)
+    cylinder_volume = pi * radius(cylbox)**2 * height(cylbox)
+  end function
+
+  function cylinder_min_image(cylbox, r1, r2) result(min_i)
+    real(dp), dimension(3) :: min_i
+    type(cylinderdat), intent(in) :: cylbox
+    real(dp), dimension(3), intent(in) :: r1
+    real(dp), dimension(3), intent(in) :: r2
+    min_i = min_image(cylbox%b, r1, r2)
+  end function
+
+  function cylinder_min_distance(cylbox, r1, r2) result(min_d)
+    real(dp) :: min_d
+    type(cylinderdat), intent(in) :: cylbox
+    real(dp), dimension(3) :: r1, r2
+    min_d = min_distance(cylbox%b, r1, r2)
+  end function
+
+  subroutine cyl_scale(cylbox, max_scaling, rng)
+    type(cylinderdat), intent(inout) :: cylbox
+    real(dp), intent(in) :: max_scaling
+    real(dp) :: dz
+    interface 
+      function rng()
+        real(8) :: rng
+      end function rng
+    end interface
+    dz = (2._dp * real(rng(), dp) - 1._dp) * max_scaling
+    call set_z(cylbox, get_z(cylbox) + dz)  
+  end subroutine
+
+  subroutine cylinder_write_to_stdio(cylbox)
+    type(cylinderdat) :: cylbox
+    call write_to_stdio(cylbox%b)
+  end subroutine
 
 end module cylinder
