739567c024a4c1a1cf0a1ae78418bdee1ce229ae
jounikarjalainen <>
2009-11-19 09:47:45 +0000

    Cosmetics.

--- gbcyl/src/attwall.f90
+++ gbcyl/src/attwall.f90
@@ -1,19 +1,19 @@
-function attwall(xa,rc,sigwall)
+function attwall(xa, rc, sigwall)
 use nrtype
-use nr, only : rf,rd
+use nr, only : rf, rd
 implicit none
-real(dp), intent(in) :: xa,rc,sigwall
+real(dp), intent(in) :: xa, rc, sigwall
 real(dp) :: attwall
-real(dp) :: coeff,z,Ek,Kk
-real(sp) :: rfk,rdk
-real(sp) :: cc,q
-z=xa**2
-cc=0.0
-q=1.0-z
-rfk=rf(cc,q,1.0_sp)
-rdk=rd(cc,q,1.0_sp)
-Ek=rfk-z*rdk/3.0_dp
-Kk=rfk
-coeff=4.0_dp*atan(1.0)*sigwall/(12*(rc*(1.0_dp-z))**3)
-attwall=coeff*((7.0_dp+z)*Ek+4.0_dp*(z-1.0_dp)*Kk)
+real(dp) :: coeff, z, Ek, Kk
+real(dp) :: rfk, rdk
+real(dp) :: cc, q
+z = xa**2
+cc = 0._dp
+q = 1._dp - z
+rfk = real(rf(real(cc, sp), real(q, sp), 1._sp), dp)
+rdk = real(rd(real(cc, sp), real(q, sp), 1._sp), dp)
+Ek = rfk - z * rdk / 3._dp
+Kk = rfk
+coeff = 4._dp * atan(1._dp) * sigwall / (12._dp * (rc * (1._dp - z))**3)
+attwall = coeff * ((7._dp + z) * Ek + 4._dp * (z - 1._dp) * Kk)
 end function attwall
