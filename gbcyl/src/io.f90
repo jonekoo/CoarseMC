3c5a9a9b11e8c57f3a0597ecc5c6a71d687f8ae7
jounikarjalainen <>
2009-11-19 10:22:30 +0000

    Added initialization with parameterizer. Changed output to use more general fmt_char_dp function instead of fixed format. Added reading of last state from the sample file for restarting.

--- gbcyl/src/io.f90
+++ gbcyl/src/io.f90
@@ -1,65 +1,67 @@
 module io
-  use nrtype, only: dp
-  use particle, only: particledat
+  use nrtype
+  use particle
+  use utils
+  use mpi
+  use class_parameter_writer
+  use class_parameterizer
+  use class_poly_box
+  implicit none
+  private
 
+  public :: readstate
+  public :: writestate
+  public :: init
+  public :: finalizeOutput
+  public :: io_write_parameters
+  public :: find_last
+  public :: read_configuration
 
+  character(len = 50), private, save :: molecule_data_file_ = 'simdata.out'
+  integer, private, save :: molecule_data_unit_ = 19
 
-  character(len = 15), private, save :: molecule_data_file_
-  integer, private, save :: molecule_data_unit_
-  logical, private, save :: debug_ = .false. 
-  namelist /io_nml/ molecule_data_file_, molecule_data_unit_, debug_
-  private :: io_nml
-
-
+  interface init
+    module procedure init_old, init_with, init_molfile
+  end interface
 
   contains
 
-  
-
-  subroutine init
-    implicit none
-    molecule_data_file_ = "simdata.out"
-    molecule_data_unit_ = 19
+  subroutine init_molfile(statefile)
+    character(len = *), intent(in) :: statefile
+    molecule_data_file_ = statefile
     call open_output
-  end subroutine init
+  end subroutine
 
+  subroutine init_old
+    integer :: id
+    character(len = 10) :: id_char
+    integer :: ierr
+    call mpi_comm_rank(MPI_COMM_WORLD, id, ierr)
+    write(id_char, '(I9)') id
+    molecule_data_file_ = 'simdata.out' // '.' // trim(adjustl(id_char))  
+    call open_output
+  end subroutine
 
+  subroutine init_with(reader)
+    type(parameterizer), intent(in) :: reader
+    call get_parameter(reader, 'molecule_file', molecule_data_file_)
+    if ('' == molecule_data_file_) then
+      call init
+    else 
+      call open_output
+    end if 
+  end subroutine
+
+  subroutine io_write_parameters(writer)
+    type(parameter_writer), intent(in) :: writer
+    call write_parameter(writer, 'molecule_file', molecule_data_file_)
+  end subroutine
 
   subroutine finalizeOutput()
-    implicit none
     close(molecule_data_unit_) 
   end subroutine finalizeOutput
 
-
-
-  !! Saves the module state to @p write_unit
-  !!
-  !! Precondition: Module state must be initialized.
-  !! 
-  subroutine save_state(write_unit)
-    implicit none
-    integer, intent(in) :: write_unit
-    write(write_unit, NML = io_nml)
-  end subroutine save_state
-
-
-
-  !! Restores the module state from @p read_unit
-  !!
-  !! Precondition: module state must not be initialized by other means
-  !! Postcondition: module state is initialized. 
-  !!  
-  subroutine load_state(read_unit)
-    implicit none
-    integer, intent(in) :: read_unit
-    read(read_unit, NML = io_nml)
-    call open_output
-  end subroutine load_state
-
-
-
   subroutine open_output
-    implicit none
     integer :: ios
     open(UNIT = molecule_data_unit_, FILE = molecule_data_file_, & 
       & POSITION = 'APPEND', FORM = 'FORMATTED', IOSTAT = ios) 
@@ -69,12 +71,9 @@ module io
     end if
   end subroutine open_output
 
-
-
-  !Kirjoittaa tilan eli sylinterin s‰teen, korkeuden ja partikkelitaulukon
-  !tiedostoon  
+  ! Kirjoittaa tilan eli sylinterin s‰teen, korkeuden ja partikkelitaulukon
+  ! tiedostoon  
   subroutine writestate(particles, n_particles, radius, height)
-    implicit none
     type(particledat), dimension(:), intent(in) :: particles
     integer, intent(in) :: n_particles    
     real(dp), intent(in) :: radius
@@ -96,50 +95,84 @@ module io
     write(molecule_data_unit_, *) '$R:',radius,'$Lz:',height
     write(molecule_data_unit_, *) '$N:',n_particles,'$GB:',GB,'$Xe:',Xe
     write(molecule_data_unit_, *) '$x:'
-    write(molecule_data_unit_, '(G12.6)') particles(1:n_particles)%x
+    write(molecule_data_unit_, '(' // fmt_char_dp() // ')') particles(1:n_particles)%x
     write(molecule_data_unit_, *) '$y:'
-    write(molecule_data_unit_, '(G12.6)') particles(1:n_particles)%y
+    write(molecule_data_unit_, '(' // fmt_char_dp() // ')') particles(1:n_particles)%y
     write(molecule_data_unit_, *) '$z:'
-    write(molecule_data_unit_, '(G12.6)') particles(1:n_particles)%z
+    write(molecule_data_unit_, '(' // fmt_char_dp() // ')') particles(1:n_particles)%z
     write(molecule_data_unit_, *) '$rod:'
     write(molecule_data_unit_, *) help(1:n_particles)
     write(molecule_data_unit_, *) '$ux:'
-    write(molecule_data_unit_, '(G12.6)') particles(1:n_particles)%ux
+    write(molecule_data_unit_, '(' // fmt_char_dp() // ')') particles(1:n_particles)%ux
     write(molecule_data_unit_, *) '$uy:'
-    write(molecule_data_unit_, '(G12.6)') particles(1:n_particles)%uy
+    write(molecule_data_unit_, '(' // fmt_char_dp() // ')') particles(1:n_particles)%uy
     write(molecule_data_unit_, *) '$uz:'
-    write(molecule_data_unit_, '(G12.6)') particles(1:n_particles)%uz
+    write(molecule_data_unit_, '(' // fmt_char_dp() // ')') particles(1:n_particles)%uz
     deallocate(help)
   end subroutine writestate
 
-
-
-  subroutine readstate(filename, particles, n_particles, radius, height)
-    implicit none
-    character(len=*), intent(in) :: filename
+  subroutine read_configuration(read_unit, simbox, particles, n_particles)
+    integer, intent(in) :: read_unit
+    type(poly_box), intent(out) :: simbox
     type(particledat), dimension(:), pointer :: particles
     integer, intent(out) :: n_particles
-    real(dp), intent(out) :: radius,height
-    integer,parameter :: readunit=17   
-    integer :: ios,astat,i
-    character(len=3) :: charvar
-    integer,dimension(:),allocatable :: help
-    radius=0.
-    height=0.  
+    real(dp) :: radius, height
+    integer :: astat, i
+    character(len = 3) :: charvar
+    integer, dimension(:), allocatable :: help
+    character(len = 200) :: box_string
+    read(read_unit, *) charvar, radius, charvar, height
+    read(read_unit, *) charvar, n_particles
+    write(box_string, '(A, 3' // fmt_char_dp() // ', A)') 'cylinder', 2._dp * radius, 2._dp * radius, height, ' F F T'
+    call create_box(simbox, box_string)
+    read(read_unit, *) charvar
+    allocate(particles(n_particles), help(n_particles), stat = astat)
+    if (astat /= 0) then
+      stop 'Error! Allocation of memory failed in read_configuration'
+    end if   
+    read(read_unit,*) particles(1:n_particles)%x
+    read(read_unit,*) charvar
+    read(read_unit,*) particles(1:n_particles)%y
+    read(read_unit,*) charvar,particles(1:n_particles)%z
+    read(read_unit,*) charvar
+    read(read_unit,*) help(1:n_particles)
+    read(read_unit,*) charvar
+    read(read_unit,*) particles(1:n_particles)%ux
+    read(read_unit,*) charvar,particles(1:n_particles)%uy
+    read(read_unit,*) charvar,particles(1:n_particles)%uz
+    close(read_unit) 
+    do i = 1, n_particles
+      if(help(i) == 1) then
+        particles(i)%rod = .true.
+      else
+        particles(i)%rod = .false.
+      end if
+    end do
+  end subroutine
+
+  subroutine readstate(filename, particles, n_particles, radius, height)
+    character(len = *), intent(in) :: filename
+    type(particledat), dimension(:), pointer :: particles
+    integer, intent(out) :: n_particles
+    real(dp), intent(out) :: radius, height
+    integer,parameter :: readunit = 17   
+    integer :: ios, astat, i
+    character(len = 3) :: charvar
+    integer, dimension(:), allocatable :: help
     !Koittaa avata annetun tiedoston
-    open(readunit,file=filename,status='old',iostat=ios)
-    if (ios/=0) then
-      write(*,*) 'Tiedoston ',filename,' avaaminen ep‰onnistui.'
+    open(readunit, file = filename, status = 'old', iostat = ios)
+    if (ios /= 0) then
+      write(*,*) 'Tiedoston ', filename,' avaaminen ep‰onnistui, virhekoodi: ', ios
       write(*,*) 'Ohjelman suoritus keskeytyy.'
       stop;
     end if
     !Luetaan hiukkasten lukum‰‰r‰ tiedostosta
-    read(readunit,*) charvar,radius,charvar,height
-    read(readunit,*) charvar,n_particles
+    read(readunit,*) charvar, radius, charvar, height
+    read(readunit,*) charvar, n_particles
     read(readunit,*) charvar
     !Varaa muistin taulukolle
-    allocate(particles(n_particles),help(n_particles),stat=astat)
-    if (astat/=0) then
+    allocate(particles(n_particles), help(n_particles), stat = astat)
+    if (astat /= 0) then
       write(*,*) 'readstate:Virhe varattaessa muistia: particles,help'
       stop;
     end if   
@@ -158,143 +191,167 @@ module io
     read(readunit,*) charvar,particles(1:n_particles)%uy
     read(readunit,*) charvar,particles(1:n_particles)%uz
     close(readunit) 
-    do i=1,n_particles
-      if(help(i)==1) then
-        particles(i)%rod=.true.
+    do i = 1, n_particles
+      if(help(i) == 1) then
+        particles(i)%rod = .true.
       else
-        particles(i)%rod=.false.
+        particles(i)%rod = .false.
       end if
     end do
-  end subroutine readstate
+  end subroutine
 
+  subroutine read_state(readunit, simbox, particles, n_particles)
+    integer, intent(in) :: readunit
+    type(poly_box), intent(out) :: simbox
+    type(particledat), dimension(:), pointer :: particles 
+    integer, intent(out) :: n_particles
+    character(len = 3) :: charvar
+    character(len = 500) :: particle_string
+    character(len = 200) :: box_string
+    integer :: astat, i
+    type(particledat) :: particle_read
+    read(readunit, '(A200)') box_string
+    call create_box(simbox, box_string)
+    read(readunit, *) charvar, n_particles
+    allocate(particles(n_particles), stat = astat)
+    if (astat /= 0) then
+      stop 'Error: Could not allocate memory for particle array. Stopping.'
+    end if
+    do i = 1, n_particles
+      read(readunit, '(A500)') particle_string
+      particle_read = create_particle(particle_string)
+      particles(i) = particle_read 
+    end do
+  end subroutine
 
-
-  subroutine readerror(filename)
-    implicit none
-    character(len=*) :: filename
-    write(*,*) 'Tiedoston ',filename,' muoto on v‰‰r‰.'
-    write(*,*) 'Tilaa ei voitu lukea. Ohjelman suoritus keskeytyy'
-    stop;
-  end subroutine readerror
-
-
-
-  !Aliohjelma parameterien lukemiseen tiedostosta paramsfile.
-  !Tekij‰: Juho Lintuvuori
-  !muutokset: Jouni Karjalainen
-  subroutine ReadParams(file, Nrelax, Nprod, Nratio, T, pres, anchor, voltyp,&
-                        Kw, seed, epses, eps0, rsphere, spmyy, epsphere, &
-                        sigma0, siges, allign, debug, adjusttype, moveratio,&
-                        scalingratio, maxtranslation, maxrotation)
-    implicit none
-    ! Alkuper√§inen aliohjelma Antti Kurosen k√§sialaa; kurssilta johdatus
-    ! atomistisiin simulaatioihin
-
-    integer,intent(out) :: Nrelax,Nprod,Nratio,anchor,seed, adjusttype
-    integer,intent(out) :: allign,debug,voltyp
-    real(dp),intent(out) :: T,pres,epses,eps0,rsphere,spmyy,epsphere 
-    real(dp),intent(out) :: sigma0,siges,Kw
-    real(dp), intent(out) :: moveratio, scalingratio 
-    real(dp), intent(out) :: maxtranslation, maxrotation
-    character(len=*), parameter :: paramsfile='gbcyl.in'
-    integer, parameter :: input=20
-    character(len = 78), intent(out) :: file  
-
-    integer :: i, ios
-    character(len=100) :: line
-    character(len=80) :: string
-    real(dp) :: x
-
-    open(input,file=paramsfile,status='old',iostat=ios)
-   
+  !! Reads the last state of @p particles and @p simbox written in @p filename.
+  !! 
+  !! @p filename the name of the file to be read from
+  !! @p simbox the simulation box to be read.
+  !! @p particles the dynamic array to which particles will be read
+  !! @p n_particles the number of particles in @p particles
+  !!
+  !! :NOTE: it may be better to discard this routine and use the two routines
+  !! find_last and read_state when needed. This only aggregates those two and
+  !! file opening/closing.
+  !!
+  subroutine read_last_state(filename, begin, end, simbox, particles, n_particles)
+    character(len = *), intent(in) :: filename
+    character(len = *), intent(in) :: begin
+    character(len = *), intent(in) :: end
+    type(particledat), dimension(:), pointer :: particles
+    integer, intent(out) :: n_particles
+    integer, parameter :: readunit = 17   
+    integer :: ios
+    type(poly_box), intent(out) :: simbox
+    open(readunit, file = filename, status = 'old', iostat = ios)
     if (ios /= 0) then
-      write(*,*) 'Open error for file ',paramsfile,'.' 
+      write(6, *) 'Error: Could not open file ', filename, '. Stopping.'
+      stop 
+    end if
+    call find_last(readunit, begin, end)
+    call read_state(readunit, simbox, particles, n_particles)
+    close(readunit) 
+  end subroutine
+
+  subroutine find_last(read_unit, begin, end)
+    integer, intent(in) :: read_unit
+    character(len = *), intent(in) :: begin
+    character(len = *), intent(in) :: end
+    character(len = 80) :: line
+    integer :: ios
+    ! size of line to max begin, end
+    !! Go to end of file
+    line = ''
+    do 
+      read(read_unit, iostat = ios ,fmt = '(A80)') line
+      if(ios < 0) then
+        exit
+      end if
+    end do
+    !! Backspace until end of geometry found
+    if('EOF' /= end) then
+      do while(end /= trim(adjustl(line)))
+        backspace(read_unit, iostat = ios)
+        backspace(read_unit, iostat = ios)
+        if (0 /= ios) then
+          !! This doesn't make a difference if the file ends or there is some 
+          !! other reading problem! 
+          write(6, *) 'find_last: no end geometry line found in file ' !!// filename
+          stop
+        end if
+        read(read_unit, *) line
+      end do
+    end if
+    !! Backspace until beginning of geometry found
+    do while(begin /= trim(adjustl(line)))
+      backspace(read_unit, iostat = ios) !! Should it be used twice?
+      backspace(read_unit, iostat = ios)
+      if (0 /= ios) then 
+        !! This doesn't make a difference if the file ends or there is some 
+        !! other reading problem! 
+        write(6, *) 'find_last: no begin geometry line found in file ' !// filename
+        stop
+      end if
+      read(read_unit, *) line
+    end do
+    backspace(read_unit, iostat = ios)
+  end subroutine
+
+!  subroutine readerror(filename)
+!    implicit none
+!    character(len = *) :: filename
+!    write(*,*) 'Tiedoston ', filename,' muoto on v‰‰r‰.'
+!    write(*,*) 'Tilaa ei voitu lukea. Ohjelman suoritus keskeytyy'
+!    stop;
+!  end subroutine
+
+  !! Reads simulation parameters from the file gbcyl.in. 
+  !!
+  !! Original subroutine authored by Antti Kuronen for the course Atomistiset 
+  !! simulaatiot
+  !!
+  !! @author: Juho Lintuvuori
+  !! @author: Jouni Karjalainen
+  !!
+  subroutine ReadParams(pt_freq)
+    implicit none
+    integer, intent(out) :: pt_freq
+    character(len = *), parameter :: paramsfile = 'gbcyl.in'
+    integer, parameter :: input = 20
+    integer :: i, ios
+    character(len = 100) :: line
+    character(len = 80) :: string
+    real(dp) :: x
+    open(input, file = paramsfile, status = 'old', iostat = ios)   
+    if (ios /= 0) then
+      write(*,*) 'Open error for file ', paramsfile,'.' 
       STOP 
     end if
     ! Loop through file and find all variables
-    i=0
+    i = 0
     do
-       i=i+1
-
+       i = i + 1
        ! First read in line in whole.
        read(input,fmt='(A80)',iostat=ios) line
        ! Check for probable end-of-file
        if (ios < 0) exit
-
        ! If not parameter line, cycle
        if (line(1:1) /= '$') cycle
-
-     if (line(1:10)=='$initstate') then
-       read(unit=line,fmt=*,iostat=ios) string,file
-     else       
        ! Attempt to parse line into command and value
        ! using internal formatted read
-       read(unit=line,fmt=*,iostat=ios) string,x
+       read(unit=line,fmt=*,iostat=ios) string, x
        if (ios > 0) then
           print *,'ERROR: Data read in error on line',i
           stop 'Invalid parameter file'
-       endif
-
        ! From here on line should be in variable format
-     
        ! Look for variable-defining strings
-
-       if (string=='$Nrelax') then
-         Nrelax=int(x+0.5)
-       else if (string=='$Nprod') then
-         Nprod=int(x+0.5);
-       else if (string=='$Nratio') then
-         Nratio=int(x+0.5);
-       else if (string=='$T') then
-         T=x 
-       else if (string=='$pres') then
-         pres=x
-       else if (string=='$anchor') then
-         anchor=int(x+0.5)
-       else if (string=='$voltyp') then
-         voltyp=int(x+0.5)
-       else if (string=='$seed') then
-         seed=int(x+0.5)
-       else if(string=='$epses')then
-         epses=x;
-       else if(string=='$eps0')then
-         eps0=x;
-       else if(string=='$rsphere')then
-         rsphere=x;
-       else if(string=='$spmyy')then
-         spmyy=x;
-       else if(string=='$epsphere')then
-         epsphere=x;
-       else if(string=='$allign')then
-         allign=int(x+0.5);
-       else if(string=='$debug')then
-         debug=int(x+0.5);
-       else if(string=='$sigma0')then
-         sigma0=x;
-       else if(string=='$siges')then
-         siges=x;
-       else if(string=='$Kw')then
-         Kw=x;
-       else if(string=='$adjusttype')then
-         adjusttype=int(x+0.5);
-       else if(string=='$moveratio')then
-         moveratio=x;
-       else if(string=='$scalingratio')then
-         scalingratio=x;
-       else if(string=='$maxtranslation')then
-         maxtranslation=x;
-       else if(string=='$maxrotation') then
-         maxrotation=x;
-       else
-         print '(A,A)','Unknown parameter',string
-         stop 'Parameter read in error'
+       else if(string == '$PT_movefreq') then
+         pt_freq = int(x + 0.5_dp);
        endif
-     end if
-     if(debug_) print '(A,A16,A,G13.6)','Read in parameter ',string,' value',x
+       print '(A,A16,A,' // fmt_char_dp() // ')','Read in parameter ', string, ' value ', x
     enddo
     close(input)
   end subroutine ReadParams
 
-  
-
-end module io
+end module
