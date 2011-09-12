f26361f3782b27a0d4dadb00cff81ef5e9c2fcfb
jounikarjalainen <>
2011-09-12 14:57:45 +0000

    Restored the radius of the wall to be the simulation box size.

--- gbcyl/src/particlewall.f90
+++ gbcyl/src/particlewall.f90
@@ -8,8 +8,16 @@ module particlewall
   use class_poly_box
   use class_parameterizer
   use class_parameter_writer
+  use nrtype
+  use ljcylinder
   implicit none
   private
+
+  !! :TODO: Divide this module into 1. the part that calculates the potential
+  !! :TODO: and 2. the part that wraps the first part in an object-oriented 
+  !! :TODO: way for better usability. Part 1. should not depend on any modules
+  !! :TODO: or routines except the ones that calculate parts of the 
+  !! :TODO: interaction.
 
   public :: new_sitewallpotential
   public :: new_gbwallpotential
@@ -18,7 +26,7 @@ module particlewall
   public :: repwall
   public :: attwall
   public :: particlewall_writeparameters
-  public :: energy
+!  public :: energy
   public :: sitewallpotential
   public :: gbwallpotential
     
@@ -29,20 +37,21 @@ module particlewall
   real(dp), save :: alphaB = 1._dp
   real(dp), save :: Kw = 8._dp
   real(dp), save :: sig=1._dp
+  !real(dp), save :: radius=-1._dp
   
   !! The distance of LJ interaction sites from the Gay-Berne particle center
   !! along the unique axis.
   real(dp), save :: LJdist = 1.7_dp 
   logical, save :: isuniformalignment = .false.
 
-!! LJ-GB interaction consists of interactions of the two sites. Basicly
-!! That could be in a different module which then would use this module. 
+!! LJ-GB interaction consists of interactions of the two sites. Basically
+!! that could be in a different module which then would use this module. 
 !! Keep in mind that the Xe-wall interaction may be of the same form as 
 !! the GB-wall interaction. Things that characterize a typical Lennard-Jones
 !! interaction are the contact distance sigma and the potential well-depth 
-!! parameter epsilon. What is added here is basicly only the alpha parameter. 
-!! In addition to that the strength parameter Kw includes both the well depth
-!! and the density of the wall plus some additional constants.  
+!! parameter epsilon. What is added here is only the alpha parameter and the 
+!! angular term. In addition to that the strength parameter Kw includes both 
+!! the well depth and the density of the wall plus some additional constants.  
 
 type sitewallpotential
   private
@@ -67,9 +76,9 @@ interface particlewall_potential
   module procedure gbwall
 end interface
 
-interface energy
-  module procedure sitewallpotential_energy
-end interface
+!interface energy
+!  module procedure sitewallpotential_energy
+!end interface
 
 contains 
 
@@ -120,6 +129,7 @@ end subroutine
     call getparameter(reader, 'LJ_dist', LJdist) 
     call getparameter(reader, 'is_uniform_alignment', isuniformalignment)
     call getparameter(reader, 'sigwall', sig)
+    !call getparameter(reader, 'radius', radius)
   end subroutine
 
   subroutine particlewall_writeparameters(writer)
@@ -130,8 +140,18 @@ end subroutine
     call writeparameter(writer, 'Kw', Kw)
     call writeparameter(writer, 'LJ_dist', LJdist) 
     call writeparameter(writer, 'is_uniform_alignment', isuniformalignment)
+    !call writeparameter(writer, 'radius', radius)
   end subroutine
 
+  !! Calculates the potential energy of a rodlike particle with two embedded 
+  !! Lennard-Jones interaction sites with respect to a wall of a cylindrical 
+  !! cavity.
+  !! 
+  !! @p gbparticle the rodlike particle.
+  !! @p simbox the simulation box defining the dimensions of the cavity.
+  !! @p Eptwall the interaction energy of the wall and the particle.
+  !! @p ovrlp tells if the @p gbparticle has penetrated the wall too much.
+  !! 
   subroutine gbwall(gbparticle, simbox, Eptwall,ovrlp)
     implicit none
     type(particledat), intent(in) :: gbparticle
@@ -145,27 +165,20 @@ end subroutine
     Eptwall = 0._dp
     !! :TODO: Remove this dependency by making a cylinder wall object.
     Rc = getx(simbox)/2._dp 
+    !Rc = radius
     call rArB(gbparticle, rsiteA, rsiteB)
     if(rsiteA >= Rc .or. rsiteB >= Rc) then
       ovrlp = .true.
       return
     end if
-    rAperR  = rsiteA / Rc
-    rBperR  = rsiteB / Rc
-    attracA = attwall(rAperR, Rc, sig)
-    repulA  = repwall(rAperR, Rc, sig)
-    attracB = attwall(rBperR, Rc, sig)
-    repulB  = repwall(rBperR, Rc, sig)
     if (isuniformalignment) then
       fu = angular(gbparticle)  
     else 
       fu = 1._dp
     end if
-    Eptwall = fu * Kw * ((repulA - alphaA * attracA) + (repulB - alphaB &
-      * attracB))     
+    Eptwall = fu * ljcylinderpotential(Kw, sig, alphaA, rsiteA, Rc) + &
+    ljcylinderpotential(Kw, sig, alphaB, rsiteB, Rc)
   end subroutine gbwall
-
-
 
 subroutine sitewallpotential_energy(sitepotential, r, rwall, energy, overlap)
   type(sitewallpotential), intent(in) :: sitepotential
@@ -179,8 +192,9 @@ subroutine sitewallpotential_energy(sitepotential, r, rwall, energy, overlap)
     overlap = .true.
     return
   end if
-  energy = sitepotential%Kw * (repwall(r/rwall, rwall, sitepotential%sigma) &
-  - sitepotential%alpha * attwall(r/rwall, rwall, sitepotential%sigma))
+  !energy = sitepotential%Kw * (repwall(r/rwall, rwall, sitepotential%sigma) &
+  !- sitepotential%alpha * attwall(r/rwall, rwall, sitepotential%sigma))
+  energy = ljcylinderpotential(sitepotential%Kw, sitepotential%sigma, sitepotential%alpha, r, rwall)
 end subroutine
 
   !! Returns the distances of the interaction sites from the axis of the 
@@ -189,6 +203,7 @@ end subroutine
   !! @p particle the particle of which interaction sites are calcula 
   !! @p ra the distance of site A from the cylinder center
   !! @p rb the distance of site B from the cylinder center
+  !!
   subroutine rarb(particle, ra, rb)
     implicit none
     intrinsic sqrt
@@ -203,86 +218,16 @@ end subroutine
     rb=sqrt(xb**2 + yb**2)
   end subroutine
 
+
+  !! Returns the angular dependence of the potential with respect to the 
+  !! cylinder axis (z-direction). 
+  !!
+  !! @p particle the particle to which the potential is calculated. 
+  !! 
   real(dp) function angular(particle)
     implicit none
     type(particledat), intent(in) :: particle
     angular=(particle%uz)**2
   end function angular
-
-!! Calculates the absolute value of the repulsive part of the interaction
-!! between a smooth infinitely thick cylinder shape wall and a Lennard-Jones
-!! particle inside it. 
-!! x=r/rc where r=particles distance from the center of the cylinder,
-!! rc=radius of the cylinder
-!! sigwall=contact distance between the particle and the wall
-!!
-function repwall(xa, rc, sigwall)
-use nrtype
-use nr, only : rf, rd
-implicit none
-real(dp), intent(in) :: xa, rc, sigwall
-real(dp) :: repwall
-real(dp), dimension(5) :: as, bs
-real(dp) :: pa, pb, z
-real(dp) :: rfk, rdk
-real(dp) :: Ek, Kk, coeff
-real(dp) :: cc, q
-interface 
-function horner(a, n, x) 
-  use nrtype
-  real(dp), dimension(:), intent(in) :: a
-  integer, intent(in) :: n 
-  real(dp), intent(in) :: x
-  real(dp) :: horner
-end function horner
-end interface
-  z = xa**2
-  cc = 0._dp
-  q = 1._dp - z
-  rfk = real(rf(real(cc, sp), real(q, sp), 1._sp), dp)
-  rdk = real(rd(real(cc, sp), real(q, sp), 1._sp), dp)
-  Ek = rfk - z * rdk / 3._dp   !! Complete elliptic integrals of the second and
-  Kk = rfk                     !! first kind
-
-  !! as are the coefficients for the polynomial in equation (11) and
-  !! bs are the coefficients for the polynomial in equation (12).
-  !! pa and pb are the corresponding polynomial values. 
-  !! They have been multiplied by 315 and divided by two just to make things 
-  !! look a bit cleaner.  
-  !! Coefficient of the highest order term is first on the array as required by
-  !! the horner function. 
-  as(1:5) = (/35._dp, 4052._dp, 16434._dp, 11156._dp, 1091._dp/)
-  bs(1:5) = (/1400._dp, 6304._dp, -1200._dp, -5728._dp, -776._dp/)
-  
-  pa = horner(as, 5, z)
-  pb = horner(bs, 5, z)
-  coeff = 4._dp * atan(1._dp) * (sigwall**12) / (128._dp * 45._dp * ((rc) * &
-  (1._dp - z))**9)
-  repwall = coeff * (pa * Ek + pb * Kk)
-end function repwall
-
-
-
-function attwall(xa, rc, sigwall)
-use nrtype
-use nr, only : rf, rd
-implicit none
-real(dp), intent(in) :: xa, rc, sigwall
-real(dp) :: attwall
-real(dp) :: coeff, z, Ek, Kk
-real(dp) :: rfk, rdk
-real(dp) :: cc, q
-z = xa**2
-cc = 0._dp
-q = 1._dp - z
-rfk = real(rf(real(cc, sp), real(q, sp), 1._sp), dp)
-rdk = real(rd(real(cc, sp), real(q, sp), 1._sp), dp)
-Ek = rfk - z * rdk / 3._dp
-Kk = rfk
-coeff = 4._dp * atan(1._dp) * sigwall / (12._dp * (rc * (1._dp - z))**3)
-attwall = coeff * ((7._dp + z) * Ek + 4._dp * (z - 1._dp) * Kk)
-end function attwall
-
-
 
 end module particlewall
