bbb58b9ec4577a80669c5b98ac65b55bf1ec38b3
jounikarjalainen <>
2012-04-12 12:47:58 +0000

    Made initialization serial.

--- gbcyl/src/gbcyl.f90
+++ gbcyl/src/gbcyl.f90
@@ -1,15 +1,27 @@
-
+!> This is the main program of the ptgbcyl liquid crystal simulation software.
+!! Its purpose is just to call the necessary routines from the simulation 
+!! engine which should be exchangeable.
+!!
 program gbcyl
   use mc_engine, only: init, run, finalize
   use mpi
   implicit none
   integer :: ierr
+  integer :: id
+  integer :: i
+  integer :: ntasks
   call mpi_init(ierr)
   if (ierr /= 0) then
     stop 'MPI initialization failed. Check your MPI environment!'
   end if
-  call init
+  call mpi_comm_rank(MPI_COMM_WORLD, id, ierr)
+  call MPI_COMM_SIZE(MPI_COMM_WORLD, ntasks, ierr)
+  do i=0, ntasks-1
+    if (i == id) call init(id, ispt = .true.)
+    call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+  end do
   call run 
-  call finalize
+  call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+  call finalize(id)
   call mpi_finalize(ierr)
 end program gbcyl
