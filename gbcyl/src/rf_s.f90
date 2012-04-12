7a4eb797558a02db870e160cbfb95bbb64b3fa45
jounikarjalainen <>
2012-04-12 14:11:57 +0000

    Some fiddling with the parameters. For stability?

--- gbcyl/src/rf_s.f90
+++ gbcyl/src/rf_s.f90
@@ -3,14 +3,16 @@ USE nrtype; USE nrutil, ONLY : assert
 IMPLICIT NONE 
 REAL(SP), INTENT(IN) :: x,y,z 
 REAL(SP) ::rf_s 
-REAL(SP), PARAMETER :: ERRTOL=0.0025_sp,TINY=1.5e-38_sp,BIG=3.0e37_sp,& 
+REAL(SP), PARAMETER :: ERRTOL=0.0025_sp !!,TINY=1.5e-38_sp,
+REAL(SP), PARAMETER :: BIG=3.0e37_sp,& 
   THIRD=1.0_sp/3.0_sp,&
   C1=1.0_sp/24.0_sp,C2=0.1_sp,C3=3.0_sp/44.0_sp,C4=1.0_sp/14.0_sp 
   !! Computes Carlson?s elliptic integral of the first kind, RF(x,y,z). 
-  !! x, y, andz must be nonnegative, and at most one can be zero. TINY must be   !! at least 5 times the machine underflow limit, BIG at most one-fifth the
+  !! x, y, and z must be nonnegative, and at most one can be zero. TINY must be   
+  !! at least 5 times the machine underflow limit, BIG at most one-fifth the
   !! machine overflow limit. 
 REAL(SP) ::alamb,ave,delx,dely,delz,e2,e3,sqrtx,sqrty,sqrtz,xt,yt,zt 
-call assert(min(x,y,z) >=0.0, min(x+y,x+z,y+z) >= TINY, & 
+call assert(min(x,y,z) >=0.0, min(x+y,x+z,y+z) >= 5._sp*TINY(1._sp), & 
 max(x,y,z) <= BIG, 'rf_s args') 
 xt=x
 yt=y
