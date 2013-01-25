efd8e46c03b51adcb65176236083c38cde5ad303
jounik <>
2013-01-25 10:56:34 +0000

    ftnunit unit test framework added for convencience.

---
+++ gbcyl/ftnunit/ftnunit_test.f90
@@ -0,0 +1,10 @@
+! ftnunit_test.f90 --
+!     Include file containing the source code for the
+!     general "test" subroutine
+!
+!     Note:
+!     This file is no longer needed. It is provided for backward
+!     compatibility only. See the documentation.
+!
+!     $Id$
+!
