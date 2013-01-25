1a4b4bbd9a1951e774cb85bc6fe13215f19915f4
jounik <>
2013-01-25 13:35:27 +0000

    Calculate orientational ordering tensor in cylindrical coordinates.

---
+++ gbcyl/analysis/cylindrical_orientation.f90
@@ -0,0 +1,35 @@
+module cylindrical_orientation
+  use nrtype, only: dp
+  use particle
+  use utils
+  use orientational_ordering, only: orientational_ordering_eigens => eigens
+  implicit none  
+
+  
+  contains
+
+
+
+  subroutine eigens(particles, n_particles, values, vectors)
+    implicit none
+    type(particledat), dimension(:), intent(in) :: particles
+    integer, intent(in) :: n_particles
+    real(dp), dimension(3), intent(out) :: values
+    real(dp), dimension(3, 3), intent(out) :: vectors
+    type(particledat), dimension(:), allocatable :: temp_particles
+    integer :: i
+    allocate(temp_particles(n_particles))
+    temp_particles(1:n_particles) = particles(1:n_particles)
+    do i = 1, n_particles
+      if(temp_particles(i)%rod) then 
+        call setorientation(temp_particles(i), unitvec(orientation(temp_particles(i)), position(temp_particles(i))))
+      end if
+    end do
+    call orientational_ordering_eigens(temp_particles, n_particles, &
+      & values, vectors)
+    deallocate(temp_particles)
+  end subroutine
+
+
+
+end module 
---
+++ gbcyl/src/cylindrical_orientation_tensor.f90
@@ -0,0 +1,40 @@
+!! Usage: program < configuration_file
+!!
+!! Program reads the given file configuration by configuration. a For each 
+!! configuration it calls the routines calculate and print which redirects
+!! calls to analysis routines and prints the results to files. 
+!!
+program analysis
+  use class_factory
+  use nrtype, only: dp
+  use particle, only : particledat
+  use class_poly_box
+  use utils
+!  use orientational_ordering
+  use cylindrical_orientation, only: eigens
+  implicit none
+  !real(dp) :: radius, height
+  type(particledat), dimension(:), pointer :: particles
+  integer :: io_status
+  integer, parameter :: stdin = 5
+  type(factory) :: afactory
+  !real(dp) :: p2
+  !real(dp), dimension(3) :: director
+  type(poly_box) :: simbox
+  real(dp), dimension(3) :: values
+  real(dp), dimension(3, 3) :: vectors
+  do  
+    call readstate(afactory, stdin, simbox, particles, io_status)
+    if (io_status < 0) then
+      exit
+    else
+      !call orientation_parameter(particles, size(particles), p2, director)
+      !write(*, '( 4('// fmt_char_dp() //',1X))') p2, director
+      call eigens(particles, size(particles), values, vectors)
+      write(*, '(12('//fmt_char_dp()//',1X))') values(1:3), vectors(1:3, 1), vectors(1:3, 2), vectors(1:3, 3) 
+    end if
+  end do
+  if(associated(particles)) deallocate(particles)
+end program analysis
+
+
