5c8d910bb28efe9f181cb226614833ebb5caf61f
jounik <>
2013-01-25 12:04:40 +0000

    Director-independent versions of tau1 and psi6.

---
+++ gbcyl/analysis/smcpsi6.f90
@@ -0,0 +1,44 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!> Calculates and prints the bond-orientational ordering parameter psi6 for a
+!! Sm-C or Sm-A phase. Other information printed is the layer distance, the 
+!! orientational ordering parameter of the local layer normalss and the 
+!! reference direction, the global layer normal used in the calculation of 
+!! psi6.
+!!
+!! Usage: smctau1 < configurationfile
+!!
+program smcpsi6
+use particle
+use class_poly_box
+use utils
+use nrtype
+!use class_cylformatter
+use class_factory
+use layernormal
+use class_poly_box
+use psi6_module
+implicit none
+
+real(dp), parameter :: sigma0 = 1._dp 
+type(particledat), dimension(:), pointer :: particles
+real(dp) :: cutoff
+integer :: io_status
+!type(cylformatter) :: cf
+real(dp) :: p2
+real(dp), dimension(3) :: direction
+type(poly_box) :: simbox
+type(factory) :: afactory
+integer, parameter :: stdin = 5
+!! The loop goes through all configurations fed to standard input of this 
+!! program.
+!cf = new_cylformatter()
+cutoff = 2._dp * sigma0
+do  
+  !call readstate(cf, simbox, particles, io_status)
+  call readstate(afactory, stdin, simbox, particles, io_status)
+  if (io_status < 0) exit
+  call globalnormal(simbox, particles, cutoff, p2, direction)
+  write(*,'(5('// fmt_char_dp() // ',1X))') psi6_bulk(simbox, particles, direction), p2, direction
+end do
+
+end program
---
+++ gbcyl/src/smctau1.f90
@@ -0,0 +1,64 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!> Calculates and prints the translational tau1 parameter for a Sm-C or Sm-A 
+!! phase. Other information printed is the layer distance, the orientational
+!! ordering parameter of the local layer normalss and the reference direction,
+!! the global layer normal used in the calculation of the tau1 parameter.
+!!
+!! Usage: smctau1 < configurationfile
+!!
+program smctau1
+
+!! These are needed for the types read from configuration files
+use particle
+use class_poly_box
+
+!! For printing
+use utils 
+
+!! For type definitions
+use nrtype
+
+!! For reading configurations from disk
+use class_factory
+
+!! To determine the normal of the layers
+use layernormal
+
+!! To calculate the smectic order parameter tau1
+use tau1_module
+implicit none
+
+real(dp) :: sigma0 = 1._dp 
+!real(dp), dimension(:, :), allocatable :: localnormals
+type(particledat), dimension(:), pointer :: particles
+!integer :: nparticles
+real(dp) :: cutoff
+!real(dp) :: rcyl, lz
+integer :: io_status
+!!type(cylformatter) :: cf
+real(dp) :: p2
+real(dp), dimension(3) :: direction
+real(sp) :: tau1_value
+real(sp) :: layerdistance
+type(poly_box) :: simbox
+type(factory) :: afactory
+integer, parameter :: stdin = 5
+
+!! The loop goes through all configurations fed to standard input of this 
+!! program.
+!!cf = new_cylformatter()
+do  
+  call readstate(afactory, stdin, simbox, particles, io_status)
+  if (io_status < 0) exit
+  !if (allocated(localnormals) .and. size(localnormals)/3 /= nparticles) then
+  !  deallocate(localnormals)
+  !end if
+  !if (.not. allocated(localnormals)) allocate(localnormals(nparticles, 3))
+  !simbox = new_cylinder(rcyl, lz)
+  cutoff = 2._dp * sigma0
+  call globalnormal(simbox, particles, cutoff, p2, direction)
+  call tau1_routine(particles, real(direction, sp), tau1_value, layerdistance)
+  write(*, '(6('//fmt_char_dp()//',1X))') tau1_value, layerdistance, p2, direction
+end do
+
+end program
