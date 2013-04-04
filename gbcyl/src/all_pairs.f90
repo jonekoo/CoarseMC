038b694a19b47d847ddfd6b96c04a1985a29112c
jounik <>
2013-04-04 12:49:32 +0000

    Moved minimum image calculation and cutoff handling from class_pair_potential to all_pairs.

--- gbcyl/src/all_pairs.f90
+++ gbcyl/src/all_pairs.f90
@@ -3,12 +3,16 @@ module all_pairs
   use nrtype
   use class_poly_box
   use class_pair_potential
+  use class_parameterizer
+  use class_parameter_writer
   implicit none
   private
 
   public :: pairinteractions
   public :: stublist
-  public :: new_stublist
+  public :: ap_init
+  public :: ap_writeparameters
+  public :: cutoff
 
   type stublist
     private
@@ -16,25 +20,38 @@ module all_pairs
   end type
 
   interface pairinteractions
-    module procedure allpairinteractions, singleparticlepairs, sl_allpairinteractions, sl_singleparticlepairs
+    module procedure singleparticlepairs, ap_allpairinteractions, ap_singleparticlepairs!, allpairinteractions
   end interface
+
+  real(dp), save :: rcutoff = 5.5_dp
 
   contains
 
-  function new_stublist()
-    type(stublist) :: new_stublist
+  elemental function cutoff() result(r)
+    real(dp) :: r
+    r = rcutoff
   end function
 
-  subroutine sl_allpairinteractions(sl, simbox, particles, energy, overlap)
+  subroutine ap_init(reader)
+    type(parameterizer), intent(in) :: reader
+    call getparameter(reader, 'r_cutoff', rcutoff)
+  end subroutine
+
+  subroutine ap_writeparameters(writer)
+    type(parameter_writer), intent(in) :: writer
+    call writeparameter(writer, 'r_cutoff', rcutoff)
+  end subroutine
+
+  subroutine ap_allpairinteractions(sl, simbox, particles, energy, overlap)
     type(stublist), intent(in) :: sl
     type(poly_box), intent(in) :: simbox
     type(particledat), dimension(:), intent(in) :: particles
     real(dp), intent(out) :: energy
     logical, intent(out) :: overlap
-    call pairinteractions(simbox, particles, energy, overlap)
+    call allpairinteractions(simbox, particles, energy, overlap)
   end subroutine
 
-  subroutine sl_singleparticlepairs(sl, simbox, particles, i, energy, overlap)
+  pure subroutine ap_singleparticlepairs(sl, simbox, particles, i, energy, overlap)
     type(stublist), intent(in) :: sl
     type(poly_box), intent(in) :: simbox
     type(particledat), dimension(:), intent(in) :: particles
@@ -44,7 +61,7 @@ module all_pairs
     call pairinteractions(simbox, particles, i, energy, overlap)
   end subroutine
 
-  subroutine allpairinteractions(simbox, particles, energy, overlap)
+  pure subroutine allpairinteractions(simbox, particles, energy, overlap)
     type(poly_box), intent(in) :: simbox
     type(particledat), dimension(:), intent(in) :: particles
     real(dp), intent(out) :: energy
@@ -52,23 +69,36 @@ module all_pairs
     integer :: nparticles
     integer :: i, j
     real(dp) :: epair
+    real(dp) :: rij(3)
     energy = 0._dp
     overlap = .false.
     nparticles = size(particles)
     do i = 1, nparticles - 1
       do j = i + 1, nparticles
-        call pairV(particles(i), particles(j), simbox, epair, overlap)
+        rij = minimage(simbox, position(particles(j))-position(particles(i)))
+        call pairV(particles(i), particles(j), rij, simbox, epair, overlap)
         if(overlap) then
           return
         else
-          if(epair < -1000._dp) write(*, *) 'epair = ', epair
           energy = energy + epair
         end if
       end do
     end do
   end subroutine
 
-  subroutine singleparticlepairs(simbox, particles, i, energy, overlap) 
+  !> Calls the pair potential calculation for particles(i) and all particles in
+  !! @p particles for which the center-to-center distance is less than cutoff
+  !! radius.
+  !! 
+  !! @param simbox the simulation box for the minimum image calculation.
+  !! @param particles the array of particles.
+  !! @param i the particle to which the interactions are calculated.
+  !! @param energy sum of all pair interactions for particles(i) and other
+  !! particles.
+  !! @param overlap is true if some two particles overlap. Correct energy is not
+  !! guaranteed when overlap = .true. 
+  !! 
+  pure subroutine singleparticlepairs(simbox, particles, i, energy, overlap)
     type(particledat), dimension(:), intent(in) :: particles
     type(poly_box), intent(in) :: simbox
     integer, intent(in) :: i
@@ -77,17 +107,42 @@ module all_pairs
     integer :: nparticles
     integer :: j
     real(dp) :: epair
+    logical :: cutoff_mask(size(particles))
+    real(dp) :: rijs(3, size(particles))
+    logical :: overlap_ij
     energy = 0._dp
     overlap = .false.
+    overlap_ij = .false.
     nparticles = size(particles)
+
+    !! Select the calculated interactions by cutoff already here
     do j = 1, nparticles
-      if (j /= i) then
-        call pairV(particles(i), particles(j), simbox, epair, overlap)
-        if(overlap) then
-          return
-        else
+      !! :NOTE: The chosen way is better than
+      !! :NOTE: rij = minimage(simbox, position(particlei), position(particlej))
+      !! It is notably faster to use direct references to particle coordinates.
+      rijs(:, j) = minimage(simbox, (/particles(j)%x-particles(i)%x,&
+        particles(j)%y-particles(i)%y, particles(j)%z-particles(i)%z/))
+    end do
+    do j = 1, nparticles
+      cutoff_mask(j) = dot_product(rijs(:,j), rijs(:,j)) < rcutoff**2
+    end do
+    !! Remove the particle i from the cutoff mask 
+    cutoff_mask(i) = .false. 
+
+    !! :TODO: test overlap conditions also here?
+
+    !! :TODO: sort particles by type here as well?  
+    do j = 1, nparticles
+      if (cutoff_mask(j)) then
+      !if (dot_product(rijs(:, j), rijs(:, j)) < rcutoff**2 .and. j /= i) then
+        call pairV(particles(i), particles(j), rijs(:,j), simbox, epair, &
+          overlap)
+        overlap = overlap .or. overlap_ij
+        !if(overlap) then
+        !  return
+        !else
           energy = energy + epair
-        end if
+        !end if
       end if
     end do
   end subroutine
--- gbcyl/src/class_pair_potential.f90
+++ gbcyl/src/class_pair_potential.f90
@@ -10,7 +10,6 @@ implicit none
 private
 
 public :: pairv
-public :: cutoff
 public :: pp_init
 public :: pp_writeparameters
 
@@ -22,7 +21,7 @@ type pairpotential
 end type
 
 interface pp_init
-  module procedure pp_initwith, pp_initvalue
+  module procedure pp_initwith
 end interface
 
 contains
@@ -36,39 +35,22 @@ contains
 !! 
 subroutine pp_initwith(reader)
   type(parameterizer), intent(in) :: reader
-  call getparameter(reader, 'r_cutoff', rcutoff)
   call gayberne_init(reader)
   call lj_init(reader)
 end subroutine
 
-subroutine pp_initvalue(cutoff)
-  real(dp), intent(in) :: cutoff
-  rcutoff = cutoff
-end subroutine
-
-!elemental function new_pairpotential(cutoff) result(pp)
-!  real(dp), intent(in) :: cutoff
-!  type(pairpotential) :: pp
-!  pp%cutoff = cutoff
-!end function
-
 subroutine pp_writeparameters(writer)
   type(parameter_writer), intent(in) :: writer
-  call writeparameter(writer, 'r_cutoff', rcutoff)
   call gb_writeparameters(writer)
   call lj_writeparameters(writer)
 end subroutine
-
-elemental function cutoff() result(r)
-  real(dp) :: r
-  r = rcutoff
-end function
 
 !! Calculates the interaction energy of a pair of particles. 
 !! 
 !! @p particlei the first particle
 !! @p particlej the second particle
 !! @p simbox the simulation cell
+!! @p rij is the (minimum image) vector from particle i to particle j
 !! @p potE the interaction energy
 !! @p overlap is true if the two particles overlap each other
 !! 
@@ -78,39 +60,28 @@ end function
 !! A poly_particle class could be used. This class could be quered for
 !! the true type of the particles since it knows it anyway.
 !! 
-elemental subroutine pairv(particlei, particlej, simbox, potE, overlap)
+pure subroutine pairv(particlei, particlej, rij, simbox, potE, overlap)
   type(particledat), intent(in) :: particlei 
   type(particledat), intent(in) :: particlej
+  real(dp), intent(in) :: rij(3)
   type(poly_box), intent(in) :: simbox
   real(dp), intent(out) :: potE
   logical, intent(out) :: overlap
-  real(dp), dimension(3) :: rij, ui, uj
-  !potE = 0._dp
-  !overlap = .false.
-  !! :NOTE: The chosen way is better than
-  !! :NOTE: rij = minimage(simbox, position(particlei), position(particlej))
-  !! It is notably faster to use direct references to particle coordinates.
-  rij = minimage(simbox, (/particlej%x-particlei%x,particlej%y-particlei%y,&
-  & particlej%z-particlei%z/))
-  if (dot_product(rij, rij) < rcutoff**2) then
-    ui(1)=particlei%ux
-    ui(2)=particlei%uy
-    ui(3)=particlei%uz
-    uj(1)=particlej%ux
-    uj(2)=particlej%uy
-    uj(3)=particlej%uz
-    if (particlei%rod .and. particlej%rod) then
-      call potential(ui, uj, rij, potE, overlap)
-    else if (particlei%rod) then
-      call gblj_potential(ui, rij, potE, overlap)
-    else if (particlej%rod) then
-      call gblj_potential(uj, -rij, potE, overlap)
-    else
-      potE = lj_potential(sqrt(dot_product(rij, rij)))
-      overlap = .false.
-    end if
+  real(dp), dimension(3) :: ui, uj
+  ui(1)=particlei%ux
+  ui(2)=particlei%uy
+  ui(3)=particlei%uz
+  uj(1)=particlej%ux
+  uj(2)=particlej%uy
+  uj(3)=particlej%uz
+  if (particlei%rod .and. particlej%rod) then
+    call potential(ui, uj, rij, potE, overlap)
+  else if (particlei%rod) then
+    call gblj_potential(ui, rij, potE, overlap)
+  else if (particlej%rod) then
+    call gblj_potential(uj, -rij, potE, overlap)
   else
-    potE = 0._dp
+    potE = lj_potential(sqrt(dot_product(rij, rij)))
     overlap = .false.
   end if
 end subroutine
