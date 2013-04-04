d998868c22c98294c79ac8bcdec51f018256cff7
jounik <>
2013-04-04 11:35:33 +0000

    Made initialization parallel to speed it up a little. Added test for particle exchange although this feature will be deprecated soon.

--- gbcyl/src/gbcyl.f90
+++ gbcyl/src/gbcyl.f90
@@ -5,7 +5,7 @@
 program gbcyl
   use mc_engine, only: init, run, finalize
   use mpi
-  !!use pt
+  use pt
   implicit none
   integer :: ierr
   integer :: id
@@ -17,9 +17,13 @@ program gbcyl
   end if
   call mpi_comm_rank(MPI_COMM_WORLD, id, ierr)
   call MPI_COMM_SIZE(MPI_COMM_WORLD, ntasks, ierr)
-  do i=0, ntasks-1
-    if (i == id) call init(id, ntasks)
-  end do
+  !! call init(input_configuration_reader, input_parameters_reader)
+  !do i=0, ntasks-1
+  call init(id, ntasks)
+  !end do
+  call pt_test_particle_exchange
+  ! call sample(n_equilibration, output_configuration_writer, output_parameters_writer, restart_configuration_writer, restart_parameters_writer, adjust=.true.)
+  ! call sample(n_production, output_configuration_writer, output_parameters_writer, restart_configuration_writer, restart_parameters_writer)
   call run 
   call MPI_BARRIER(MPI_COMM_WORLD, ierr)
   call finalize(id)
