ec5aaabfe22e0827620b7607ada26f86bf51bfc5
jounik <>
2014-05-14 08:04:01 +0000

    Printpotentials should now print all possible pair interactions.

--- gbcyl/src/printpotentials.f90
+++ gbcyl/src/printpotentials.f90
@@ -1,65 +1,143 @@
 program printpotentials
-!! A program which takes as input the coordinates of two Gay-Berne particles
-!! and dimensions of a cylindrical cavity and outputs the potential energies 
-!! for both. Cavity wall consists of smoothly and evenly distributed Lennard-
-!! Jones (LJ) particles. The molecules have two embedded LJ sites which 
-!! interact with the wall. 
-!! 
 use nrtype
-use gayberne, only: gayberne_init, potential
+use class_pair_potential
 use utils, only : fmt_char_dp
-use ljcylinder
+use m_fileunit
+use particlewall
+use class_poly_box
+use particle
+use class_factory
+use class_parameterizer
 implicit none
-real(dp), dimension(3), parameter :: ui = (/0._dp, 0._dp, 1._dp/)
-real(dp), dimension(3) :: uj 
+
 real(dp), dimension(3), parameter :: ex = (/1._dp, 0._dp, 0._dp/)
-real(dp), dimension(3) :: ri = (/0._dp, 0._dp, 0._dp/)
-real(dp), dimension(3) :: rj = (/0._dp, 0._dp, 0._dp/)
+real(dp), dimension(3), parameter :: ey = (/0._dp, 1._dp, 0._dp/)
+real(dp), dimension(3), parameter :: ez = (/0._dp, 0._dp, 1._dp/)
+character(4) :: idchar
+logical :: is_wall_on
+
+type(factory) :: coordinatereader
+integer :: coordinateunit, ios
+character(len=200) :: statefile
+type(parameterizer) :: reader
+
+type(particledat), allocatable :: particles(:)
+type(poly_box) :: simbox
+
+type(particledat) :: testparticles(4)
+character(len=3) :: particle_descriptions(4)
+
+integer :: i, j, k
 integer, parameter :: n = 1000
-integer :: i
-real(dp) :: rmax = 9._dp
-real(dp) :: egbgb
-real(dp) :: ewall
-logical :: overlap = .false.
-logical :: gboverlap
-real(dp) :: rcyl = 9._dp
-real(dp) :: alpha = 1._dp
-real(dp) :: Kw = 8._dp
-real(dp) :: sigmalb = 0.5_dp
-real(dp) :: sigmaub = 1.0_dp
-real(dp) :: sigma
-integer :: isigma
-integer :: nsigma = 1 
-real(dp) :: gbshift = 0._dp
-real(dp) :: rhoj 
-real(dp) :: kappasigma = 4.4_dp
-real(dp) :: yj = 0._dp
-real(dp) :: zj = 0._dp
-namelist /inputnml/ Kw, sigmaub, sigmalb, nsigma, alpha, gbshift, rcyl, rmax, yj, zj
-uj = ui
-call gayberne_init(kappasigma, 20._dp, 1._dp, 1._dp, 1._dp, 1._dp)
-read(*, nml=inputnml)
-!!write(*, nml=inputnml)
-rj(2) = yj
-rj(3) = zj
-ri = (rcyl - gbshift) * ex
-do i = 1, n
-  rj = rj + rmax/real(n, dp) * ex
-  rhoj = sqrt(rj(1)**2 + rj(2)**2)
-  if (rhoj >= rcyl) exit
-  call potential(ui, uj, rj-ri, egbgb, gboverlap)
-  overlap = gboverlap
-  write(*, '(2('//fmt_char_dp()//',1X))', ADVANCE='NO') rcyl-rj(1), egbgb
-  if (nsigma > 1) then
-    do isigma = 1, nsigma
-      sigma = sigmalb + (sigmaub-sigmalb)/real(nsigma-1, dp) * real(isigma-1,dp)
-      ewall = ljcylinderpotential(Kw, sigma, alpha, rhoj, rcyl) 
-      write(*, '(2('//fmt_char_dp()//',1X))', ADVANCE='NO') ewall, egbgb + 2._dp * ewall
-    end do
-    write(*, *) ''
-  else if (nsigma == 1) then 
-    ewall = ljcylinderpotential(Kw, sigmalb, alpha, rhoj, rcyl) 
-    write(*, '(2('//fmt_char_dp()//',1X))') ewall, egbgb + 2._dp * ewall
+real(dp) :: energy
+logical :: overlap
+type(particledat) :: temp
+real(dp), parameter :: step = 0.01_dp
+real(dp) :: r
+real(dp) :: rmax
+
+read(*, *) idchar
+
+!! Read inputparameters
+reader = new_parameterizer('inputparameters.'//trim(adjustl(idchar)), logfile = "printpotentials_log."//trim(adjustl(idchar)))
+call pp_init(reader)
+
+call getparameter(reader, 'is_wall_on', is_wall_on)
+if (is_wall_on) then
+  call initptwall(reader)
+end if
+
+!! Read inputconfiguration
+coordinateunit = fileunit_getfreeunit()
+statefile = 'inputconfiguration.'//trim(adjustl(idchar))
+!! In any case the output should be appended to configurations.(id) and restartfile
+!! should be used only for reading once and then overwriting the file.
+open(file=statefile, unit=coordinateunit, action='READ', status='OLD', iostat=ios)
+call factory_readstate(coordinatereader, coordinateunit, simbox, particles, ios)
+if (0/=ios) then 
+  write(*, *) 'Error ', ios,' reading ', statefile, ' Stopping.' 
+  stop
+end if
+close(coordinateunit)
+
+!! :TODO: Create only the kinds of particle pairs that exist in the inputconfiguration
+
+!! Look for GB particles:
+j=1
+do i = 1, size(particles)
+  if (particles(i)%rod) then
+    testparticles(j) = particles(i)
+    call setorientation(testparticles(j), ex)
+    particle_descriptions(j) = 'GBx'
+    j = j + 1
+    testparticles(j) = particles(i)
+    call setorientation(testparticles(j), ey)
+    particle_descriptions(j) = 'GBy'
+    j = j + 1
+    testparticles(j) = particles(i)
+    call setorientation(testparticles(j), ez)
+    particle_descriptions(j) = 'GBz'
+    j = j + 1
+    exit
   end if
+end do
+
+!! Look for LJ particles:
+do i = 1, size(particles)
+  if (.not. particles(i)%rod) then
+    testparticles(j) = particles(i)
+    particle_descriptions(j) = 'LJ'
+    j = j + 1
+    exit
+  end if
+end do
+
+rmax = simbox%lx / 2._dp
+do i = 1, size(testparticles)
+  call setposition(testparticles(i), (/rmax, 0._dp, 0._dp/))
+end do
+
+
+write(*, '(A23,1X)', advance='no') 'r'
+do j = 1, size(particle_descriptions)
+  temp = testparticles(j)
+  temp%x = testparticles(j)%x - r  
+  if (is_wall_on) then
+      write(*, '(A23,1X)', advance='no') trim(particle_descriptions(j))// "-Wall"
+    end if
+    do k = 1, j
+      write(*, '(A23,1X)', advance='no') trim(particle_descriptions(j)) // "-" // particle_descriptions(k)
+    end do
+  end do
+  write(*, '(A)') ''
+
+
+r = 0._dp
+!! Calculate all possible pair potentials:
+do i = 1, n
+  r = r + step
+  if (r > rmax) exit
+  write(*, fmt='(G23.15E3,1X)', advance='no') r
+  do j = 1, size(testparticles)
+    temp = testparticles(j)
+    temp%x = testparticles(j)%x - r  
+    if (is_wall_on) then
+      call particlewall_potential(temp, simbox, energy, overlap)
+      if (overlap) then 
+        write(*, '(A23,1X)', advance='no') 'NaN'
+      else
+        write(*, '(G23.15E3,1X)', advance='no') energy
+      end if
+    end if
+    do k = 1, j
+      call pair_potential(temp, testparticles(k), position(testparticles(k))-position(temp), energy, overlap)
+      if (overlap) then 
+        write(*, '(A23,1X)', advance='no') 'NaN'
+      else
+        write(*, '(G23.15E3,1X)', advance='no') energy
+      end if
+    end do
+  end do
+  write(*, '(A)') ''
 end do
 end program
