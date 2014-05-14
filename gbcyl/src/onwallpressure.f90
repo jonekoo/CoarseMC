6eb74a02165f6edd614daf659c21d83e26402915
jounik <>
2014-05-14 11:54:55 +0000

    Gathered the pressure computation to a separate routine.

--- gbcyl/src/onwallpressure.f90
+++ gbcyl/src/onwallpressure.f90
@@ -4,25 +4,21 @@ use particle
 use class_poly_box
 use class_parameterizer
 use m_fileunit
-use energy
+use particlewall
 use nrtype
 use utils
 implicit none
 type(factory) :: coordinatereader
 integer :: coordinateunit
-integer :: nparticles
 integer :: ios
 type(particledat), allocatable :: particles(:)
 type(poly_box) :: simbox
 type(parameterizer) :: reader
 character(len=3) :: idchar = '0'
-real(dp), parameter :: dr=1.e-6_dp
-real(dp) :: f01, f12, f, totv0, totv1, totv2
-logical :: overlap
 read(*, *) idchar
 reader = new_parameterizer('inputparameters.'//trim(adjustl(idchar)))
 !! Initialize the modules needed
-call energy_init(reader)
+call initptwall(reader)
 coordinateunit = fileunit_getfreeunit()
 open(unit=coordinateunit, file='configurations.' //trim(adjustl(idchar)), action='READ', status='OLD')
 do
@@ -30,16 +26,33 @@ do
   if (ios /= 0) then
     exit
   end if
-  nparticles = size(particles)
-  call totwallprtclV(simbox, particles, totv1, overlap)
-  call setx(simbox, getx(simbox)+dr)
-  call totwallprtclV(simbox, particles, totv2, overlap)
-  call setx(simbox, getx(simbox)-2._dp*dr)
-  call totwallprtclV(simbox, particles, totv0, overlap)
-  f01=(totv1-totv0)/dr
-  f12=(totv2-totv1)/dr
-  f=0.5_dp*(f01+f12)
-  write(*, *) f, -f/(getz(simbox)*4._dp*atan(1._dp)*getx(simbox))
+  write(*, *) pressure(simbox, particles)
 end do
+
+contains
+
+function pressure(simbox, particles)
+  implicit none
+  type(poly_box), intent(in) :: simbox
+  type(particledat), intent(in) :: particles(:)
+  real(dp) :: pressure
+  real(dp) :: f(3)
+  real(dp) :: radius
+  integer :: i
+  real(dp) :: sum_f(3)
+  real(dp), parameter :: pi = 4._dp * atan(1._dp)
+  real(dp) :: r(3)
+  radius = getx(simbox) / 2._dp
+  pressure = 0._dp
+  sum_f = 0._dp
+  do i = 1, size(particles)
+    f = particlewall_force(particles(i), simbox)
+    r = position(particles(i))
+    r(3) = 0._dp
+    r = r / sqrt(dot_product(r, r))
+    pressure = pressure + dot_product(f, r)
+  end do
+  pressure = pressure / (2 * pi * radius * getz(simbox))
+end function
 
 end program
