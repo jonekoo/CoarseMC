bb4dff94d80bb38ad513924bd4e5a343d0ae6761
jounik <>
2014-05-14 11:35:39 +0000

    Added routines for computing the particle-wall forces. Added possibility to parametrize the module with well depth (epsilon) and wall density instead of the single K_w parameters)

--- gbcyl/src/particlewall.f90
+++ gbcyl/src/particlewall.f90
@@ -1,9 +1,5 @@
-!! Jatkuvasti ja tasaisesti jakautuneista LJ-partikkeleista
-!! koostuvan seinän ja LJ-partikkelin väliseen vuorovaikutukseen
-!! liittyviä parametreja ja aliohjelmia.  
-
 module particlewall
-  use particle, only: particledat 
+  use particle, only: particledat, position, orientation 
   use nrtype
   use class_poly_box
   use class_parameterizer
@@ -11,142 +7,75 @@ module particlewall
   use nrtype
   use ljcylinder
   implicit none
-  private
-
-  !! :TODO: Divide this module into 1. the part that calculates the potential
-  !! :TODO: and 2. the part that wraps the first part in an object-oriented 
-  !! :TODO: way for better usability. Part 1. should not depend on any modules
-  !! :TODO: or routines except the ones that calculate parts of the 
-  !! :TODO: interaction.
-
-  public :: new_sitewallpotential
-  public :: new_gbwallpotential
-  public :: initptwall
-  public :: particlewall_potential
-  public :: repwall
-  public :: attwall
-  public :: particlewall_writeparameters
-  public :: sitewallpotential
-  public :: gbwallpotential
     
   !! @see Micheletti et. al. Journal of Chemical Physics 123, 224705 for 
   !! definitions of these parameters.
   !!
   real(dp), save :: alphaA = 1._dp
   real(dp), save :: alphaB = 1._dp
-  real(dp), save :: Kw = 8._dp
+  real(dp), save :: eps = 1._dp
   real(dp), save :: sig=1._dp
-  !real(dp), save :: radius=-1._dp
   
   !! The distance of LJ interaction sites from the Gay-Berne particle center
   !! along the unique axis.
   real(dp), save :: LJdist = 1.7_dp 
   logical, save :: isuniformalignment = .false.
 
-  real(dp), save :: Kw_lj = 10.72_dp
+  real(dp), save :: epswall_lj = 1._dp
   real(dp), save :: alpha_lj = 1._dp
   real(dp), save :: sigwall_lj = 0.8_dp
 
-
-!! LJ-GB interaction consists of interactions of the two sites. Basically
-!! that could be in a different module which then would use this module. 
-!! Keep in mind that the Xe-wall interaction may be of the same form as 
-!! the GB-wall interaction. Things that characterize a typical Lennard-Jones
-!! interaction are the contact distance sigma and the potential well-depth 
-!! parameter epsilon. What is added here is only the alpha parameter and the 
-!! angular term. In addition to that the strength parameter Kw includes both 
-!! the well depth and the density of the wall plus some additional constants.  
-
-type sitewallpotential
-  private
-  real(dp) :: alpha = 1._dp    ! relative strength of attraction 
-  real(dp) :: sigma = 1._dp    ! LJ contact distance 
-  real(dp) :: Kw = 8._dp       ! interaction strength 
-end type
-
-type gbwallpotential
-  private
-  type(sitewallpotential) :: sitea
-  type(sitewallpotential) :: siteb
-  real(dp) :: separation = 1.7_dp
-  logical :: isuniformalignment = .false.
-end type
+  !! General wall properties
+  real(dp), save :: wall_density = 1._dp
 
 interface initptwall
-  module procedure initptwallparameterizer, initptwallregular
+  module procedure initptwallparameterizer
 end interface
 
 contains 
 
-function new_sitewallpotential(Kw, sigma, alpha) result(potential)
-  real(dp), intent(in), optional :: Kw
-  real(dp), intent(in), optional :: sigma
-  real(dp), intent(in), optional :: alpha
-  type(sitewallpotential) :: potential
-  if(present(Kw)) potential%Kw = Kw
-  if(present(sigma)) potential%sigma = sigma
-  if(present(alpha)) potential%alpha = alpha
-end function
-
-function new_gbwallpotential(sitea, siteb, separation, isuniformalignment) &
-result(potential)
-  type(sitewallpotential), intent(in), optional :: sitea
-  type(sitewallpotential), intent(in), optional :: siteb
-  real(dp), intent(in), optional :: separation
-  logical, intent(in), optional :: isuniformalignment
-  type(gbwallpotential) :: potential
-  if(present(sitea)) potential%sitea = sitea
-  if(present(siteb)) potential%siteb = siteb
-  if(present(separation)) potential%separation = separation
-  if(present(isuniformalignment)) potential%isuniformalignment = &
-  isuniformalignment 
-end function
-
-subroutine initptwallregular(Kwin, alphaAin, alphaBin, LJdistin, isunifin, sigwallin)
-  real(dp), intent(in), optional :: Kwin
-  real(dp), intent(in), optional :: alphaAin
-  real(dp), intent(in), optional :: alphaBin
-  real(dp), intent(in), optional :: LJdistin
-  logical, intent(in), optional :: isunifin
-  real(dp), intent(in), optional :: sigwallin
-  if(present(Kwin)) Kw = Kwin
-  if(present(alphaAin)) alphaA = alphaAin
-  if(present(alphaBin)) alphaB = alphaBin
-  if(present(LJdistin)) LJdist = LJdistin
-  if(present(isunifin)) isuniformalignment = isunifin
-  if(present(sigwallin)) sig = sigwallin
-end subroutine
-
   subroutine initptwallparameterizer(reader)
     type(parameterizer), intent(in) :: reader
-    !! GB-wall parameters
-    call getparameter(reader, 'Kw', Kw)
+    real(dp) :: Kw_LJ = 5.48819_dp, Kw = 8._dp
+    logical :: found
     call getparameter(reader, 'alpha_A', alphaA) 
     call getparameter(reader, 'alpha_B', alphaB) 
     call getparameter(reader, 'LJ_dist', LJdist) 
     call getparameter(reader, 'is_uniform_alignment', isuniformalignment)
     call getparameter(reader, 'sigwall', sig)
-    !! LJ-wall parameters
-    call getparameter(reader, 'Kw_LJ', Kw_lj)
     call getparameter(reader, 'alpha_LJ', alpha_lj)
     call getparameter(reader, 'sigwall_LJ', sigwall_lj)
+    call getparameter(reader, 'wall_density', wall_density, found)
+    if (.not. found) then
+      !! Try to read old parameters
+      call getparameter(reader, 'Kw', Kw)
+      eps = Kw / 8._dp
+      call getparameter(reader, 'Kw_LJ', Kw_LJ)
+      epswall_lj = Kw_LJ / (Kw * (sigwall_lj/sig)**3) 
+    else
+      !! Use the new parameters epswall_lj and epswall
+      call getparameter(reader, 'epswall_LJ', epswall_lj)
+      call getparameter(reader, 'epswall', eps)
+    end if 
   end subroutine
 
   subroutine particlewall_writeparameters(writer)
     type(parameter_writer), intent(in) :: writer
+    call writecomment(writer, 'General wall parameters')
+    call writeparameter(writer, 'wall_density', wall_density)
     !! GB-wall parameters
     call writecomment(writer, 'GB-wall interaction parameters')
     call writeparameter(writer, 'alpha_A', alphaA) 
     call writeparameter(writer, 'alpha_B', alphaB) 
-    call writeparameter(writer, 'Kw', Kw)
     call writeparameter(writer, 'LJ_dist', LJdist) 
     call writeparameter(writer, 'is_uniform_alignment', isuniformalignment)
     call writeparameter(writer, 'sigwall', sig)
+    call writeparameter(writer, 'epswall', eps)
     !! LJ-wall parameters
     call writecomment(writer, 'LJ-wall interaction parameters')
-    call writeparameter(writer, 'Kw_LJ', Kw_lj)
     call writeparameter(writer, 'alpha_LJ', alpha_lj)
     call writeparameter(writer, 'sigwall_LJ', sigwall_lj)
+    call writeparameter(writer, 'epswall_LJ', epswall_lj)
   end subroutine
 
 
@@ -161,6 +90,18 @@ end subroutine
       call ljwall(particle, simbox, energy, overlap)
     end if
   end subroutine
+
+
+  pure function particlewall_force(particle, simbox) result(f)
+    type(particledat), intent(in) :: particle
+    type(poly_box), intent(in) :: simbox
+    real(dp) :: f(3)
+    if (particle%rod) then
+      f = gbwall_force(particle, simbox)
+    else
+      f = ljwall_force(particle, simbox)
+    end if
+  end function
 
 
   !> Calculates the potential energy of a Lennard-Jones (LJ) particle with 
@@ -180,7 +121,7 @@ end subroutine
     real(dp), intent(out) :: energy
     logical, intent(out) :: ovrlp
     real(dp) :: r
-    real(dp) :: fu, Rc
+    real(dp) :: Rc
     ovrlp = .false.
     energy = 0._dp
     Rc = getx(simbox)/2._dp 
@@ -189,8 +130,23 @@ end subroutine
       ovrlp = .true.
       return
     end if
-    energy = ljcylinderpotential(Kw_lj, sigwall_lj, alpha_lj, r, Rc)
+    energy = ljcylinderpotential(epswall_lj, wall_density, sigwall_lj, alpha_lj, r, Rc)
   end subroutine
+
+  !! Returns the force exerted to a LJ particle by the wall
+  pure function ljwall_force(ljparticle, simbox) result(f)
+    implicit none
+    type(particledat), intent(in) :: ljparticle
+    type(poly_box), intent(in) :: simbox
+    real(dp) :: f(3)
+
+    real(dp) :: r
+    real(dp) :: Rc
+    Rc = getx(simbox)/2._dp 
+    r = sqrt(ljparticle%x**2 + ljparticle%y**2)
+    f = [ljparticle%x, ljparticle%y, 0._dp]
+    f = f / r * ljcylinder_force(epswall_lj, wall_density, sigwall_lj, alpha_lj, r, Rc)
+  end function
 
   !> Calculates the potential energy of a rodlike particle with two embedded 
   !! Lennard-Jones interaction sites with respect to a wall of a cylindrical 
@@ -224,26 +180,37 @@ end subroutine
     else 
       fu = 1._dp
     end if
-    energy = fu * (ljcylinderpotential(Kw, sig, alphaA, rsiteA, Rc) + &
-    ljcylinderpotential(Kw, sig, alphaB, rsiteB, Rc))
+    energy = fu * (ljcylinderpotential(eps, wall_density, sig, alphaA, rsiteA, Rc) + &
+    ljcylinderpotential(eps, wall_density, sig, alphaB, rsiteB, Rc))
   end subroutine gbwall
 
-subroutine sitewallpotential_energy(sitepotential, r, rwall, energy, overlap)
-  type(sitewallpotential), intent(in) :: sitepotential
-  real(dp), intent(in) :: r
-  real(dp), intent(in) :: rwall
-  real(dp), intent(out) :: energy
-  logical, intent(out) :: overlap
-  energy = 0._dp
-  overlap = .false.
-  if (r >= rwall) then
-    overlap = .true.
-    return
-  end if
-  !energy = sitepotential%Kw * (repwall(r/rwall, rwall, sitepotential%sigma) &
-  !- sitepotential%alpha * attwall(r/rwall, rwall, sitepotential%sigma))
-  energy = ljcylinderpotential(sitepotential%Kw, sitepotential%sigma, sitepotential%alpha, r, rwall)
-end subroutine
+  pure function gbwall_force(gbparticle, simbox) result(f)
+    implicit none
+    type(particledat), intent(in) :: gbparticle
+    type(poly_box), intent(in) :: simbox
+    real(dp) :: f(3)
+    real(dp) :: fA(3), fB(3)
+    real(dp) :: rsiteA, rsiteB
+    real(dp) :: fu, Rc
+    Rc = getx(simbox)/2._dp 
+    call rArB(gbparticle, rsiteA, rsiteB)
+    if (isuniformalignment) then
+      fu = angular(gbparticle)  
+    else 
+      fu = 1._dp
+    end if
+
+    fA = position(gbparticle) + orientation(gbparticle) * LJdist
+    fA(3) = 0._dp
+    fA = fA / sqrt(dot_product(fA, fA))
+
+    fB = position(gbparticle) - orientation(gbparticle) * LJdist
+    fB(3) = 0._dp
+    fB = fB / sqrt(dot_product(fB, fB))
+
+    f = fu * (fA * ljcylinder_force(eps, wall_density, sig, alphaA, rsiteA, Rc) + &
+    fB * ljcylinder_force(eps, wall_density, sig, alphaB, rsiteB, Rc))
+  end function
 
   !! Returns the distances of the interaction sites from the axis of the 
   !! cylinder.
