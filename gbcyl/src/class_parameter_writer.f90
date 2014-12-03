ea57353981a459d801877ead2f25585f76f4a709
jounik <>
2014-12-03 16:02:41 +0000

    Added documentation.

--- gbcyl/src/class_parameter_writer.f90
+++ gbcyl/src/class_parameter_writer.f90
@@ -1,14 +1,17 @@
+!> Implements functions to write parameters to a file in a consistent
+!! format readable by parameterizer instances.
 module class_parameter_writer
 use nrtype
 use utils
 implicit none
-private
 
 public :: new_parameter_writer
 public :: delete
 public :: parameter_writer
 public :: writeparameter
 public :: writecomment
+
+private
 
 type parameter_writer
   private
@@ -29,25 +32,30 @@ end interface
 
 contains
 
+!> Returns a new parameter writer which will direct its output to
+!! @p pwunit.
 function new_parameter_writer(pwunit) result(pw)
-  !character(len = *), intent(in) :: parameterfilename
   integer, intent(in) :: pwunit
   type(parameter_writer) :: pw
   pw%unit = pwunit
 end function
 
+!> Deletes the parameter writer @p p by closing the file.
 subroutine pw_delete(p)
   type(parameter_writer), intent(inout) :: p
   !! If opened
   close(p%unit) 
 end subroutine
 
+!> Writes the string @p comment to the output unit of @p p.
 subroutine pw_writecomment(p, comment)
   type(parameter_writer), intent(in) :: p
   character(len = *), intent(in) :: comment
   write(p%unit, *) comment
 end subroutine
 
+!> Writes the parameter with @p key and a string @p value to the output
+!! unit of @p p.
 subroutine pw_writestring(p, key, value)
   type(parameter_writer), intent(in) :: p
   character(len = *), intent(in) :: key
@@ -55,6 +63,8 @@ subroutine pw_writestring(p, key, value)
   call pw_writeparameter(p, key, '"'//trim(adjustl(value))//'"')  
 end subroutine
 
+!> Writes the parameter with @p key and @p value to the output unit of
+!! @p p. 
 subroutine pw_writeparameter(p, key, value)
   type(parameter_writer), intent(in) :: p
   character(len = *), intent(in) :: key
@@ -66,6 +76,8 @@ subroutine pw_writeparameter(p, key, value)
   write(p%unit, '(A30, A50)') adjustl(keyfield), adjustl(valuefield)
 end subroutine
 
+!> Converts the real parameter with @p key and @p value to string and
+!! writes it to the output unit of @p p.
 subroutine pw_writereal(p, key, value)
   type(parameter_writer), intent(in) :: p
   character(len = *), intent(in) :: key
@@ -75,6 +87,8 @@ subroutine pw_writereal(p, key, value)
   call pw_writeparameter(p, key, valuestring) 
 end subroutine
 
+!> Converts the integer parameter with @p key and @p value to string
+!! and writes it to the output unit of @p p.
 subroutine pw_writeint(p, key, value)
   type(parameter_writer), intent(in) :: p 
   character(len = *), intent(in) :: key 
@@ -84,6 +98,8 @@ subroutine pw_writeint(p, key, value)
   call pw_writeparameter(p, key, valuestring)
 end subroutine
 
+!> Converts the logical variable with @p key and @p value to string
+!! and writes it to the output unit of @p p.
 subroutine pw_writelogical(p, key, value)
   type(parameter_writer), intent(in) :: p
   character(len = *), intent(in) :: key
