5bdadc14a3ac222c547d7576b6ebe246e2a5d49c
jounik <>
2014-12-03 10:13:31 +0000

    Added documentation. Made some cosmetic changes.

--- gbcyl/src/bin_indices.f90
+++ gbcyl/src/bin_indices.f90
@@ -1,9 +1,17 @@
-subroutine bin_indices(particles, n_particles, xfunc, bin_width, indices, offset, direction)
+!> Labels @p particles with bin indices.
+!!
+!! @param[in] particles the particles to label.
+!! @param[in] xfunc the function defining the shape of the bins.
+!! @param[in] bin_width the width of each bin.
+!! @param[out] indices the bin index of each particle.
+!! @param[in] offset the offset for binning.
+!! @param[in] direction the direction of indexing.
+!!
+subroutine bin_indices(particles, xfunc, bin_width, indices, offset, direction)
 use nrtype
 use particle
 implicit none
 type(particledat), dimension(:), intent(in) :: particles
-integer, intent(in) :: n_particles
 real(dp), intent(in) :: bin_width
 real(dp), intent(in), optional :: offset
 integer, intent(in), optional :: direction 
@@ -20,14 +28,14 @@ integer, dimension(:), intent(out) :: indices
   integer :: i
   integer :: i_bin
   if (present(offset) .and. present(direction)) then
-    do i=1, n_particles
-      i_bin=int((offset + real(direction, dp) * xfunc(particles(i))) / bin_width) + 1
-      indices(i)=i_bin
+    do i = 1, size(particles)
+      i_bin = int((offset + direction * xfunc(particles(i))) / bin_width) + 1
+      indices(i) = i_bin
     end do
   else
-    do i=1, n_particles
-      i_bin=int(xfunc(particles(i)) / bin_width) + 1
-      indices(i)=i_bin
+    do i = 1, size(particles)
+      i_bin = int(xfunc(particles(i)) / bin_width) + 1
+      indices(i) = i_bin
     end do
   end if
 end subroutine bin_indices
