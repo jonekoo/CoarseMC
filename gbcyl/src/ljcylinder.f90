6300c11dc22056c766d9dc9e11356fc5403ce973
jounik <>
2015-02-16 14:11:17 +0000

    Moved attwall2 and repwall2 to an external file, so that their implementation could be changed without touching the interfaces. Removed old repwall and attwall. Added documentation. Removed implementation-specific interfaces for the elliptic integrals.

--- gbcyl/src/ljcylinder.f90
+++ gbcyl/src/ljcylinder.f90
@@ -1,227 +1,111 @@
+!> Implements the interaction for a spherical particle inside a
+!! cylindrical cavity. The particle interacts with the elements in the
+!! cavity wall through the Lennard-Jones 12-6 potential. The total
+!! interaction is integrated over the volume of the infinitely thick
+!! wall of an infinitely long cavity.
+!!
+!! @see X. Zhang and W. Wang and G. Jiang, "A potential model for
+!! interaction between the Lennard-Jones cylindrical wall and fluid
+!! molecules". Fluid Phase Equilibria, 218(2), 239-246, 2004.
+
+
 module ljcylinder
 use nrtype
-use nr, only : rf, rd
 implicit none
 
-
 interface 
-  pure function ljcylinder_force(eps, density, sigma, alpha, r, rwall) result(f)
-    use nrtype, only: dp
-    real(dp), intent(in) :: eps, density, sigma, alpha, r, rwall
-    real(dp) :: f
-  end function
-end interface
+   !> Returns the repulsive term of the interaction between a LJ
+   !! particle and the smooth wall of the long cylindrical cavity.
+   !! @p k = r / rc, where r is the distance from the cavity center
+   !! and @p rc is the inner radius of the cavity. The routine works in
+   !! reduced LJ units, where distances are in sigma_0:s and energies
+   !! in epsilon_0:s.
+   pure function repwall2(k, rc)
+     use nrtype, only: dp
+     real(dp), intent(in) :: k, rc
+     real(dp) :: repwall2
+   end function repwall2
 
+   !> Returns the attractive term of the interaction between a LJ
+   !! particle and the smooth wall of the long cylindrical cavity.
+   !! @p k = r / rc, where r is the distance from the cavity center
+   !! and @p rc is the inner radius of the cavity. The routine works in
+   !! reduced LJ units, where distances are in sigma_0:s and energies
+   !! in epsilon_0:s.
+   pure function attwall2(k, rc)
+     use nrtype, only: dp
+     real(dp), intent(in) :: k, rc
+     real(dp) :: attwall2
+   end function attwall2
 
-interface
-pure function EllipticE(k_sqrd) result(Ek)
-  use nrtype, only: dp
-  use nr, only: rd, rf
-  real(dp), intent(in) :: k_sqrd
-  real(dp) :: Ek
-end function
-end interface
+   !> Returns the repulsive term of the force acting on a LJ
+   !! particle by the smooth wall of the long cylindrical cavity.
+   !! @p k = r / rc, where r is the distance from the cavity center
+   !! and @p rc is the inner radius of the cavity. The routine works in
+   !! reduced LJ units, where distances are in sigma_0:s and energies
+   !! in epsilon_0:s.
+   pure real(dp) function d_repwall(k, rc)
+     use nrtype, only: dp
+     real(dp), intent(in) :: k, rc
+   end function d_repwall
 
-interface
-pure function EllipticK(k_sqrd) result(Kk)
-  use nrtype, only: dp
-  use nr, only: rf
-  implicit none
-  real(dp), intent(in) :: k_sqrd
-  real(dp) :: Kk
-end function
+   !> Returns the attractive term of the force acting on a LJ
+   !! particle by the smooth wall of the long cylindrical cavity.
+   !! @p k = r / rc, where r is the distance from the cavity center
+   !! and @p rc is the inner radius of the cavity. The routine works in
+   !! reduced LJ units, where distances are in sigma_0:s and energies
+   !! in epsilon_0:s.
+   pure real(dp) function d_attwall(k, rc)
+     use nrtype, only: dp
+     real(dp), intent(in) :: k, rc
+   end function d_attwall
 end interface
 
 contains
 
-!! Returns the potential energy for a Lennard-Jones (LJ) site with respect to 
-!! a cylindrical wall consisting of smoothly and evenly distributed LJ 
-!! particles.
+!> Returns the potential energy for a Lennard-Jones (LJ) site with
+!! respect to a cylindrical wall consisting of smoothly and evenly
+!! distributed LJ particles.
 !! 
-!! @p eps the parameter for LJ-particle - wall well-depth
-!! @p density the wall density
-!! @p sigma the parameter setting the contact distance for the first layer in
-!! the wall and  the LJ site.
-!! @p alpha controls the amount of attraction with respect to repulsion. 
-!! alpha=1 is the regular LJ interaction while alpha=0 makes the interaction
-!! purely repulsive.
-!! @p r the distance of LJ site center from the cylinder axis.
-!! @p rwall the radius of the cylinder.
+!! @param eps the parameter for LJ-particle - wall well-depth.
+!! @param density the wall density.
+!! @param sigma the parameter setting the contact distance between a
+!!        wall element and the LJ site.
+!! @param alpha controls the amount of attraction with respect to
+!!        repulsion. alpha=1 is the regular LJ interaction while
+!!        alpha=0 makes the interaction purely repulsive.
+!! @param r the distance of LJ site center from the cylinder axis.
+!! @param rwall the radius of the cylinder.
 !! 
-pure function ljcylinderpotential(eps, density, sigma, alpha, r, rwall) result(potential)
-  real(dp), intent(in) :: eps 
-  real(dp), intent(in) :: density 
-  real(dp), intent(in) :: sigma
-  real(dp), intent(in) :: alpha
-  real(dp), intent(in) :: r
-  real(dp), intent(in) :: rwall
+pure function ljcylinderpotential(eps, density, sigma, alpha, r, rwall) &
+     result(potential)
+  real(dp), intent(in) :: eps, density, sigma, alpha, r, rwall
   real(dp) :: potential
-  potential = eps * density * sigma**3 * (repwall2(r/rwall, rwall/sigma) &
-  - alpha * attwall2(r/rwall, rwall/sigma))
+  potential = eps * density * sigma**3 * (repwall2(r / rwall, rwall / sigma) &
+       - alpha * attwall2(r / rwall, rwall / sigma))
 end function
 
-
-!! Returns the absolute value of the repulsive part of the interaction
-!! between an infinitely thick  wall of a cylindrical cavity consisting of 
-!! smoothly and evenly distributed Lennard-Jones particles and a LJ particle.
+!> Returns the value of force acting on a LJ particle due to a
+!! cylindrical wall consisting of smoothly and evenly distributed LJ
+!! particles. The direction of the force is from the center of the
+!! cavity towards the wall. 
 !!
-!! @p xa is the ratio r/rc where r is particle's distance from the center of 
-!! the cylinder.
-!! @p rc is the radius of the cylinder in reduced LJ units.
-!!
-pure function repwall(xa, rc)
-  real(dp), intent(in) :: xa, rc !, sigwall
-  real(dp) :: repwall
-  real(dp), dimension(5) :: as, bs
-  real(dp) :: pa, pb, z
-  real(dp) :: rfk, rdk
-  real(dp) :: Ek, Kk, coeff
-  real(dp) :: cc, q
-  interface 
-  pure function horner(a, n, x) 
-    use nrtype
-    real(dp), dimension(:), intent(in) :: a
-    integer, intent(in) :: n 
-    real(dp), intent(in) :: x
-    real(dp) :: horner
-  end function horner
-  end interface
-  z = xa**2
-  cc = 0._dp
-  q = 1._dp - z
-  rfk = real(rf(real(cc, sp), real(q, sp), 1._sp), dp)
-  rdk = real(rd(real(cc, sp), real(q, sp), 1._sp), dp)
-  Ek = rfk - z * rdk / 3._dp   !! Complete elliptic integrals of the second and
-  Kk = rfk                     !! first kind
-  !! as are the coefficients for the polynomial in equation (11) and
-  !! bs are the coefficients for the polynomial in equation (12).
-  !! pa and pb are the corresponding polynomial values. 
-  !! They have been multiplied by 315 and divided by two just to make things 
-  !! look a bit cleaner.  
-  !! Coefficient of the highest order term is first on the array as required by
-  !! the horner function. 
-  as(1:5) = (/35._dp, 4052._dp, 16434._dp, 11156._dp, 1091._dp/)
-  bs(1:5) = (/1400._dp, 6304._dp, -1200._dp, -5728._dp, -776._dp/)
-  pa = horner(as, 5, z)
-  pb = horner(bs, 5, z)
-  coeff = 4._dp * atan(1._dp) / (128._dp * 45._dp * ((rc) * &
-  (1._dp - z))**9)
-  repwall = coeff * (pa * Ek + pb * Kk)
-end function repwall
-
-!! Returns the absolute value of the repulsive part of the interaction
-!! between an infinitely thick  wall of a cylindrical cavity consisting of 
-!! smoothly and evenly distributed Lennard-Jones particles and a LJ particle.
-!!
-!! @p k is the ratio r/rc where r is particle's distance from the center of 
-!! the cylinder.
-!! @p rc is the radius of the cylinder in reduced LJ units.
-!!
-pure function repwall2(k, rc)
-  real(dp), intent(in) :: k, rc !, sigwall
-  real(dp) :: repwall2
-  real(dp) :: rfk, rdk
-  real(dp) :: Ek, Kk
-  real(dp) :: cc, q, k_sqrd
-  real(dp), parameter :: pi = 4._dp * atan(1._dp)
-
-  !! as are the coefficients for the polynomial in equation (11) and
-  !! bs are the coefficients for the polynomial in equation (12).
-  !! Coefficient of the highest order term is first on the array as required by
-  !! the horner function. 
-  real(dp), parameter :: as(5) = (/2._dp / 9._dp, 8104._dp / 315._dp, &
-  3652._dp / 35._dp, 22312._dp / 315._dp, 2182._dp / 315._dp/)
-  real(dp), parameter :: bs(5) = (/80._dp / 9._dp, 12608._dp / 315._dp, &
-  -160._dp / 21._dp, -11456._dp / 315._dp, -1552._dp / 315._dp /)
-  !! C_E9 and C_K9 are the corresponding polynomial values. 
-  real(dp) :: C_E9, C_K9
-  real(dp) :: I_10 !! As defined by equation (16) with m = 10.
-
-  interface 
-  pure function horner(a, n, x) 
-    use nrtype
-    real(dp), dimension(:), intent(in) :: a
-    integer, intent(in) :: n 
-    real(dp), intent(in) :: x
-    real(dp) :: horner
-  end function horner
-  end interface
-
-  k_sqrd = k**2
-  cc = 0._dp
-  q = 1._dp - k_sqrd
-  rfk = real(rf(real(cc, sp), real(q, sp), 1._sp), dp)
-  rdk = real(rd(real(cc, sp), real(q, sp), 1._sp), dp)
-  Ek = rfk - k_sqrd * rdk / 3._dp !! Complete elliptic integrals of the second
-  Kk = rfk                        !! and first kind
-
-  C_E9 = horner(as, 5, k_sqrd)
-  C_K9 = horner(bs, 5, k_sqrd)
-
-  I_10 = 2._dp / (9._dp * ((rc) * (1._dp - k_sqrd))**9) * &
-         (C_E9 * Ek + C_K9 * Kk) 
-
-  repwall2 = 63._dp * pi / 64._dp * I_10
-end function
-
-
-!! Returns the absolute value of the attractive part of the interaction
-!! between an infinitely thick  wall of a cylindrical cavity consisting of 
-!! smoothly and evenly distributed Lennard-Jones particles and a LJ particle.
-!!
-!! @p xa is the ratio r/rc where r is particle's distance from the center of 
-!! the cylinder.
-!! @p rc is the radius of the cylinder in reduced LJ units.
-!!
-pure function attwall(xa, rc)
-  real(dp), intent(in) :: xa, rc !, sigwall
-  real(dp) :: attwall
-  real(dp) :: coeff, z, Ek, Kk
-  real(dp) :: rfk, rdk
-  real(dp) :: cc, q
-  z = xa**2
-  cc = 0._dp
-  q = 1._dp - z
-  rfk = real(rf(real(cc, sp), real(q, sp), 1._sp), dp)
-  rdk = real(rd(real(cc, sp), real(q, sp), 1._sp), dp)
-  Ek = rfk - z * rdk / 3._dp
-  Kk = rfk
-  coeff = 4._dp * atan(1._dp) / (12._dp * (rc * (1._dp - z))**3)
-  attwall = coeff * ((7._dp + z) * Ek + 4._dp * (z - 1._dp) * Kk)
-end function attwall
-
-
-!! Returns the absolute value of the attractive part of the interaction
-!! between an infinitely thick  wall of a cylindrical cavity consisting of 
-!! smoothly and evenly distributed Lennard-Jones particles and a LJ particle.
-!!
-!! @p k is the ratio r/rc where r is particle's distance from the center of 
-!! the cylinder.
-!! @p rc is the radius of the cylinder in reduced LJ units.
-!!
-pure function attwall2(k, rc)
-  real(dp), intent(in) :: k, rc !, sigwall
-  real(dp) :: attwall2
-  real(dp) :: k_sqrd, Ek, Kk
-  real(dp) :: rfk, rdk
-  real(dp) :: cc, q
-  real(dp) :: I_4         !! As defined by eq. (16) with m = 4.
-  real(dp) :: C_E3, C_K3  !! Polynomials of eqs. (13) and (14), respectively. 
-  real(dp), parameter :: pi = 4._dp * atan(1._dp)
-  k_sqrd = k**2
-  cc = 0._dp
-  q = 1._dp - k_sqrd
-  rfk = real(rf(real(cc, sp), real(q, sp), 1._sp), dp)
-  rdk = real(rd(real(cc, sp), real(q, sp), 1._sp), dp)
-  Ek = rfk - k_sqrd * rdk / 3._dp
-  Kk = rfk
-
-  C_E3 = (14._dp + 2._dp * k_sqrd) / 3._dp 
-  C_K3 = 8._dp * (k_sqrd - 1._dp) / 3._dp 
-
-  I_4 = 2._dp / (3._dp * (rc * (1._dp - k_sqrd))**3) * (C_E3 * Ek + C_K3 * Kk)
-
-  attwall2 = (3._dp * pi / 2._dp) * I_4
+!! @param eps the parameter for LJ-particle - wall well-depth
+!! @param density the wall density
+!! @param sigma the parameter setting the contact distance for the
+!!        wall element and the LJ site.
+!! @param alpha controls the amount of attraction with respect to
+!!        repulsion. alpha=1 is the regular LJ interaction while
+!!        alpha=0 makes the interaction purely repulsive.
+!! @param r the distance of LJ site center from the cylinder axis.
+!! @param rwall the radius of the cylinder.
+!! 
+pure function ljcylinderforce(eps, density, sigma, alpha, r, rwall)
+  real(dp), intent(in) :: eps, density, sigma, alpha, r, rwall 
+  real(dp) :: ljcylinderforce
+  ljcylinderforce = eps * density * sigma**3 * &
+       (d_repwall(r / rwall, rwall / sigma) - &
+       alpha * d_attwall(r / rwall, rwall / sigma)) / rwall
 end function
 
 end module
