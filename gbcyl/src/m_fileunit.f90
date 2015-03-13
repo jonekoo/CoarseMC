2f6ca22f544be4b9ac251ff6e565d94990e63ed6
jounik <>
2015-03-13 14:43:09 +0000

    Added license text.

--- gbcyl/src/m_fileunit.f90
+++ gbcyl/src/m_fileunit.f90
@@ -1,4 +1,3 @@
-!
 ! m_fileunit.f90 --
 !
 !   The component provides services to manage fortran file units.
@@ -51,6 +50,7 @@
 ! - allow to "unlock" one unit, or all units at once.
 !
 ! Copyright (c) 2008 Michael Baudin michael.baudin@gmail.com
+! See bottom of the file for the license.
 !
 module m_fileunit
   implicit none
@@ -326,3 +326,30 @@ contains
   end subroutine fileunit_set_stoponerror
 end module m_fileunit
 
+!
+! LICENSE (as in flibs 0.9):
+!
+! All rights reserved.
+!
+! Redistribution and use in source and binary forms, with or without 
+! modification, are permitted provided that the following conditions are met:
+!
+! Redistributions of source code must retain the above copyright notice,
+! this list of conditions and the following disclaimer.
+! Redistributions in binary form must reproduce the above copyright notice,
+! this list of conditions and the following disclaimer in the documentation
+! and/or other materials provided with the distribution.
+! Neither the name of the author nor the names of the contributors
+! may be used to endorse or promote products derived from this software
+! without specific prior written permission.
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+! THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+! PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+! CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+! EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+! OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+! WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+! OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+! ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
