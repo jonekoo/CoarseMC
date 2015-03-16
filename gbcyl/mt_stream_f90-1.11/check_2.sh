8e80e9bea4460d4994c64282854a6d71d4e64725
jounik <>
2015-03-16 18:20:07 +0000

    Added check_2.sh to tests.

--- gbcyl/mt_stream_f90-1.11/check_2.sh
+++ gbcyl/mt_stream_f90-1.11/check_2.sh
@@ -2,9 +2,12 @@
 
 echo "check_stream_2 : consistency check #2."
 echo "check_stream_2 : checking multiple stream consistency with huge jump."
+ 
+#tempfile=check_stream_2_`date "+%Y-%m-%dT%H:%M:%S"`.out
+#./check_stream_2 > $tempfile
 
-./check_stream_2 > $1
-diff $1 sample_2.out
+
+diff test_2.out sample_2.out
 if [ $? -ne 0 ]; then
   echo "check_stream_2 : Check Error."
   exit -1
@@ -12,4 +15,4 @@ else
   echo "check_stream_2 : Check OK."
   exit 0
 fi
-
+#rm tempfile
