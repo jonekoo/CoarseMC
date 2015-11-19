047cd98a2b745126fe2495e1c8687d53b82c097e
jounik <>
2015-11-19 12:44:56 +0000

    Added a new downcast_assign in addition to the generic type-bound assignments. Simplified trial move generation to a single routine.

--- gbcyl/src/particle.f90
+++ gbcyl/src/particle.f90
@@ -1,70 +1,73 @@
 !> Implements the basic functions needed to handle data for simple, 
 !! uniaxial particles such as in the Gay-Berne potential.
 module particle
-use nrtype, only: dp
-use utils
-use class_poly_box, only: poly_box, minimage
-include 'rng.inc'
-use particle_mover, only: transmove, rotate
-implicit none
-
-!> Holds data of a uniaxial particle, e.g. Gay-Berne 
-!! particle. Using the rod variable one can use the same datatype to describe
-!! two different particles.
-!! 
-!! x, y, z are the cartesian positional coordinates of the particle.
-!! ux, uy, uz are the components of the unit vector that describes
-!! the 
-!! orientation of the particle.
-!! rod tells if the particle is a rodlike particle or e.g. spherically
-!! symmetric.
-!!
-!! :TODO: Remove rod variable. This module should only describe GB particles.
-!!
-type particledat
-   real(dp) :: x = 0._dp
-   real(dp) :: y = 0._dp
-   real(dp) :: z = 0._dp
-   real(dp) :: ux = 0._dp
-   real(dp) :: uy = 0._dp
-   real(dp) :: uz = 1._dp
-   logical :: rod = .true.
- contains
-   procedure :: particledat_assign
-   generic :: assignment(=) => particledat_assign
-   procedure :: energy => particle_potential
-   procedure :: nvt_update => moveparticle_2
-   procedure :: pair_energy
-end type particledat
-
-interface   
-   !> Returns the potential energy of @p particles(@p i).
-   !! 
-   !! @param simbox the simulation box where the @p particles are.
-   !! @param particles the particles in the system.
-   !! @param i the index of the particle in @p particles.
-   !! @param energy the potential energy of @p particles(@p i)
-   !! @param overlap is .true. if e.g. @p particles(@p i) is too close to
-   !!        some other particle.
-   !!
-   pure subroutine particle_energy(simbox, particles, i, &
-        energy, overlap)
-     import particledat, poly_box, dp
-     type(poly_box), intent(in) :: simbox
-     type(particledat), intent(in) :: particles(:)
-     integer, intent(in) :: i
-     real(dp), intent(out) :: energy
-     logical, intent(out) :: overlap
-   end subroutine particle_energy
-
-   pure subroutine single_energy(aparticle, simbox, energy, err)
-     import particledat, poly_box, dp
-     type(particledat), intent(in) :: aparticle
-     type(poly_box), intent(in) :: simbox
-     real(dp), intent(out) :: energy
-     integer, intent(out) :: err
-   end subroutine single_energy
-end interface
+  use utils, only: fmt_char_dp, acceptchange
+  use class_poly_box, only: poly_box, minimage
+  include 'rng.inc'
+  use particle_mover, only: transmove, rotate
+  use iso_fortran_env, only: error_unit, output_unit, dp => REAL64
+  implicit none
+  
+  !> Holds data of a uniaxial particle, e.g. Gay-Berne 
+  !! particle. Using the rod variable one can use the same datatype to describe
+  !! two different particles.
+  !! 
+  !! x, y, z are the cartesian positional coordinates of the particle.
+  !! ux, uy, uz are the components of the unit vector that describes
+  !! the 
+  !! orientation of the particle.
+  !! rod tells if the particle is a rodlike particle or e.g. spherically
+  !! symmetric.
+  !!
+  !! :TODO: Remove rod variable. This module should only describe GB particles.
+  !!
+  type particledat
+     real(dp) :: x = 0._dp
+     real(dp) :: y = 0._dp
+     real(dp) :: z = 0._dp
+     real(dp) :: ux = 0._dp
+     real(dp) :: uy = 0._dp
+     real(dp) :: uz = 1._dp
+     logical :: rod = .true.
+   contains
+     procedure :: downcast_assign => particledat_downcast_assign
+     procedure :: particledat_assign
+     generic :: assignment(=) => particledat_assign
+     procedure :: energy => particle_potential
+     procedure :: nvt_update => moveparticle_2
+     procedure :: pair_energy
+     procedure :: to_stdout
+     procedure :: move => particledat_move
+  end type particledat
+  
+  interface   
+     !> Returns the potential energy of @p particles(@p i).
+     !! 
+     !! @param simbox the simulation box where the @p particles are.
+     !! @param particles the particles in the system.
+     !! @param i the index of the particle in @p particles.
+     !! @param energy the potential energy of @p particles(@p i)
+     !! @param overlap is .true. if e.g. @p particles(@p i) is too close to
+     !!        some other particle.
+     !!
+     pure subroutine particle_energy(simbox, particles, i, &
+          energy, overlap)
+       import particledat, poly_box, dp
+       type(poly_box), intent(in) :: simbox
+       type(particledat), intent(in) :: particles(:)
+       integer, intent(in) :: i
+       real(dp), intent(out) :: energy
+       logical, intent(out) :: overlap
+     end subroutine particle_energy
+     
+     pure subroutine single_energy(aparticle, simbox, energy, err)
+       import particledat, poly_box, dp
+       type(particledat), intent(in) :: aparticle
+       type(poly_box), intent(in) :: simbox
+       real(dp), intent(out) :: energy
+       integer, intent(out) :: err
+     end subroutine single_energy
+  end interface
 
 type particlearray_wrapper
    class(particledat), allocatable :: arr(:)
@@ -108,30 +111,21 @@ abstract interface
    end function get_cutoff
 end interface
 
-!interface assignment(=)
-!   module procedure particledat_assign, wrapper_assign 
-!end interface assignment(=)
-
 contains
 
-  impure elemental subroutine wrapper_delete(this)
-    type(particlearray_wrapper), intent(inout) :: this
-    if (allocated(this%arr)) deallocate(this%arr)
-    if (allocated(this%mask)) deallocate(this%mask)
-  end subroutine wrapper_delete
+  subroutine particledat_downcast_assign(this, a_particle, err)
+    class(particledat), intent(inout) :: this
+    class(particledat), intent(in) :: a_particle
+    integer, intent(out), optional :: err
+    select type (a_particle)
+    type is (particledat)
+       this = a_particle
+    class default
+       if (present(err)) err = 3
+    end select
+  end subroutine particledat_downcast_assign
 
-  impure elemental subroutine wrapper_assign(dest, src)
-    class(particlearray_wrapper), intent(inout) :: dest
-    type(particlearray_wrapper), intent(in) :: src
-    if (allocated(dest%arr)) deallocate(dest%arr)
-    allocate(dest%arr(size(src%arr)), source=src%arr)
-    if (allocated(dest%mask)) deallocate(dest%mask)
-    allocate(dest%mask(size(src%arr)), source=src%mask)
-    !dest%arr = src%arr
-    !dest%mask = src%mask
-  end subroutine wrapper_assign
-  
-!> Writes @p aparticle to the outputunit @p writeunit.
+  !> Writes @p aparticle to the outputunit @p writeunit.
 subroutine writeparticle(writeunit, aparticle)
   integer, intent(in) :: writeunit
   type(particledat), intent(in) :: aparticle
@@ -145,6 +139,21 @@ subroutine writeparticle(writeunit, aparticle)
        typeid, aparticle%x, aparticle%y, aparticle%z, aparticle%ux, &
        aparticle%uy, aparticle%uz 
 end subroutine writeparticle
+
+!> Writes @p aparticle to the outputunit @p writeunit.
+subroutine to_stdout(this)
+  class(particledat), intent(in) :: this
+  character(len = 2) :: typeid
+  if (this%rod) then
+     typeid = 'gb'
+  else 
+     typeid = 'lj'
+  end if
+  write(output_unit, '(A,1X,6(' // fmt_char_dp() // ',1X))', advance='no') &
+       typeid, this%x, this%y, this%z, this%ux, &
+       this%uy, this%uz 
+end subroutine to_stdout
+
 
 !> Reads @p aparticle from @p readunit. If the reading succeeds, 
 !! @p ios==0.
@@ -183,10 +192,11 @@ end subroutine particledat_assign
 !! random number generator state. After the move, @p dE contains the
 !! change in energy of the system and @p isaccepted == .true. if the
 !! move was accepted. 
-subroutine moveparticle_2(this, genstates, simbox, temperature, nbrs, &
+pure subroutine moveparticle_2(this, genstates, simbox, temperature, nbrs, &
      pair_ia, subr_single_energy, dE, n_trials, n_accepted)
   class(particledat), intent(inout) :: this
-  class(particlearray_wrapper), intent(in) :: nbrs(:)
+  !! Could be type(particlearray_wrapper) if beneficial:
+  class(particlearray_wrapper), intent(in) :: nbrs(:) 
   type(poly_box), intent(in) :: simbox
   real(dp), intent(in) :: temperature
   type(rngstate), intent(inout) :: genstates(0:)
@@ -201,18 +211,17 @@ subroutine moveparticle_2(this, genstates, simbox, temperature, nbrs, &
   real(dp) :: enew
   real(dp) :: eold
   logical :: isaccepted
-  
-  write(*, *) 'size(nbrs(1)%arr)', size(nbrs(1)%arr)
+ 
   enew = 0._dp
   eold = 0._dp
   dE = 0._dp
   isaccepted = .false.
   allocate(newparticle, source=this)
-  call move(newparticle, genstates(0))
+  call newparticle%move(genstates(0))
   call setposition(newparticle, minimage(simbox, position(newparticle)))
   allocate(oldparticle, source=this)
   this = newparticle
-
+ 
   call this%energy(nbrs, pair_ia, simbox, subr_single_energy, enew, err)
   
   this = oldparticle
@@ -226,19 +235,16 @@ subroutine moveparticle_2(this, genstates, simbox, temperature, nbrs, &
      end if
   end if
 
-  !if (present(n_trials)) n_trials = 1
-  !if (present(n_accepted)) then
-     if (isaccepted) then
-        n_accepted = 1
-     else
-        n_accepted = 0
-     end if
-  !end if
+  if (isaccepted) then
+     n_accepted = 1
+  else
+     n_accepted = 0
+  end if
   n_trials = 1
 end subroutine moveparticle_2
 
 
-subroutine particle_potential(this, nbrs, pair_ia, simbox, &
+pure subroutine particle_potential(this, nbrs, pair_ia, simbox, &
      subr_single_energy, energy, err)
   class(particledat), intent(in) :: this
   class(particlearray_wrapper), intent(in) :: nbrs(:)
@@ -266,8 +272,13 @@ subroutine particle_potential(this, nbrs, pair_ia, simbox, &
         !! :NOTE: rij = minimage(simbox, position(particlei), 
         !! position(particlej)) It is significantly faster to use direct
         !! references to particle coordinates.
-        rijs(:, i) = minimage(simbox, (/nbrs(i_nbr)%arr(i)%x-this%x,& 
-             nbrs(i_nbr)%arr(i)%y-this%y, nbrs(i_nbr)%arr(i)%z-this%z/))
+        rijs(:, i) = minimage(simbox, nbrs(i_nbr)%arr(i)%x-this%x,& 
+             nbrs(i_nbr)%arr(i)%y-this%y, nbrs(i_nbr)%arr(i)%z-this%z)
+
+        if (all(rijs(:, i) == 0.) .and. nbrs(i_nbr)%mask(i)) then
+           err = 2
+           return
+        end if
      end do !! ifort does not vectorize
 
      if (allocated(cutoff_mask)) deallocate(cutoff_mask)
@@ -278,12 +289,15 @@ subroutine particle_potential(this, nbrs, pair_ia, simbox, &
      cutoff_mask = cutoff_mask .and. nbrs(i_nbr)%mask
 
      do i = 1, size(nbrs(i_nbr)%arr)
-        !if (norm2(rijs(:, i)) < rcutoff) then ! .and. nbrs(i_nbr)%mask(i)) then
         if (cutoff_mask(i)) then
            call pair_ia%pair_potential(this, nbrs(i_nbr)%arr(i), rijs(:, i), &
                 e, err)
-           if (err /= 0) exit
-           energy = energy + e
+           if (err /= 0) then
+              energy = huge(energy)
+              return
+           else
+              energy = energy + e
+           end if
         end if
      end do
   end do
@@ -341,26 +355,35 @@ end subroutine setorientation
 
 !> Performs a combined translation and rotation of @p particle. 
 !! @p genstate is the random number generator state.
-pure subroutine move(aparticle, genstate)    
-  type(particledat), intent(inout) :: aparticle
+pure subroutine particledat_move(this, genstate)    
+  class(particledat), intent(inout) :: this
   type(rngstate), intent(inout) :: genstate
-  type(particledat) :: temp
-  call move2(aparticle, temp, genstate)
-  aparticle = temp
-end subroutine move
-
-!> Creates a new particle @p newp by applying a combined translation
-!! and rotation to @p oldp. @p genstate is the random number
-!! generator state.
-pure subroutine move2(oldp, newp, genstate)
-  type(particledat), intent(in) :: oldp
-  type(particledat), intent(out) :: newp
-  type(rngstate), intent(inout) :: genstate
-  newp = oldp
-  call transmove(oldp%x,oldp%y,oldp%z,newp%x,newp%y,newp%z, genstate)
-  if (oldp%rod) then
-     call rotate(oldp%ux,oldp%uy,oldp%uz,newp%ux,newp%uy,newp%uz, genstate)
+  real(dp) :: xn, yn, zn, uxn, uyn, uzn
+  call transmove(this%x, this%y, this%z, xn, yn, zn, genstate)
+  this%x = xn
+  this%y = yn
+  this%z = zn
+  if (this%rod) then
+     call rotate(this%ux, this%uy, this%uz, uxn, uyn, uzn, genstate)
+     this%ux = uxn
+     this%uy = uyn
+     this%uz = uzn
   end if
-end subroutine move2
+end subroutine particledat_move
+
+  impure elemental subroutine wrapper_delete(this)
+    type(particlearray_wrapper), intent(inout) :: this
+    if (allocated(this%arr)) deallocate(this%arr)
+    if (allocated(this%mask)) deallocate(this%mask)
+  end subroutine wrapper_delete
+
+  impure elemental subroutine wrapper_assign(this, src)
+    class(particlearray_wrapper), intent(inout) :: this
+    type(particlearray_wrapper), intent(in) :: src
+    if (allocated(this%arr)) deallocate(this%arr)
+    allocate(this%arr(size(src%arr)), source=src%arr)
+    if (allocated(this%mask)) deallocate(this%mask)
+    allocate(this%mask(size(src%arr)), source=src%mask)
+  end subroutine wrapper_assign
   
 end module particle
