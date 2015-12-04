18220d63dca5678cd1f3266f6f6623941c75bb11
jounik <>
2015-12-04 15:24:52 +0000

    Added json initialization and corresponding input file parameter to mc_engine.

--- gbcyl/src/gbcyl.f90
+++ gbcyl/src/gbcyl.f90
@@ -7,17 +7,20 @@
 program gbcyl
   use mc_engine
   use mpi
+  use json_module
   implicit none
   integer :: ierr
   integer :: id
   integer :: ntasks
   call mpi_init(ierr)
+  call json_initialize()
   if (ierr /= 0) then
     stop 'MPI initialization failed. Check your MPI environment!'
   end if
   call mpi_comm_rank(MPI_COMM_WORLD, id, ierr)
   call MPI_COMM_SIZE(MPI_COMM_WORLD, ntasks, ierr)
-  call mce_init(id, ntasks)
+  !call mce_init(id, ntasks)
+  call mce_init_json(id, ntasks, 'input.json')
   call run 
   call MPI_BARRIER(MPI_COMM_WORLD, ierr)
   call finalize(id)
