ba4397c55c4fcaaf6573cbd05acb148eb149aec1
jounik <>
2015-12-18 08:46:52 +0000

    Added downcasting to both particle types to be used with the MC moves.

--- gbcyl/src/m_point.f90
+++ gbcyl/src/m_point.f90
@@ -6,6 +6,9 @@ module m_point
   type, extends(particledat) :: point
    contains
      procedure :: from_str => point_from_str
+     procedure :: downcast_assign => point_downcast_assign
+     procedure :: point_assign
+     generic :: assignment(=) => point_assign
   end type point
 
 contains
@@ -17,4 +20,24 @@ contains
     read(str, fmt=*, iostat=ios) this%x, this%y, this%z
   end subroutine point_from_str
   
+  pure subroutine point_downcast_assign(this, a_particle, err)
+    class(point), intent(inout) :: this
+    class(particledat), intent(in) :: a_particle
+    integer, intent(out), optional :: err
+    select type (a_particle)
+    type is (point)
+       this = a_particle
+    class default
+       if (present(err)) err = 3
+    end select
+  end subroutine point_downcast_assign
+
+  pure subroutine point_assign(this, another)
+    class(point), intent(inout) :: this
+    type(point), intent(in) :: another
+    this%x = another%x
+    this%y = another%y
+    this%z = another%z
+  end subroutine point_assign
+
 end module m_point
--- gbcyl/src/m_rod.f90
+++ gbcyl/src/m_rod.f90
@@ -6,6 +6,9 @@ module m_rod
   type, extends(particledat) :: rod
    contains
      procedure :: rod_from_str
+     procedure :: downcast_assign => rod_downcast_assign
+     procedure :: rod_assign
+     generic :: assignment(=) => rod_assign
   end type rod
 
 contains
@@ -18,4 +21,27 @@ contains
          this%uy, this%uz
   end subroutine rod_from_str
   
+  pure subroutine rod_downcast_assign(this, a_particle, err)
+    class(rod), intent(inout) :: this
+    class(particledat), intent(in) :: a_particle
+    integer, intent(out), optional :: err
+    select type (a_particle)
+    type is (rod)
+       this = a_particle
+    class default
+       if (present(err)) err = 3
+    end select
+  end subroutine rod_downcast_assign
+
+  pure subroutine rod_assign(this, another)
+    class(rod), intent(inout) :: this
+    type(rod), intent(in) :: another
+    this%x = another%x
+    this%y = another%y
+    this%z = another%z
+    this%ux = another%ux
+    this%uy = another%uy
+    this%uz = another%uz
+  end subroutine rod_assign
+
 end module m_rod
