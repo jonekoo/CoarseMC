eb74ce378e433e48701613775837bd89d6a030de
jounik <>
2015-12-26 18:38:03 +0000

    Moved responsibilities from particledat to point and rod. Equality operator added for testing purposes.

--- gbcyl/src/m_point.f90
+++ gbcyl/src/m_point.f90
@@ -1,6 +1,8 @@
 module m_point
   use particle, only: particledat
   use num_kind, only: dp
+  use particle_mover, only: transmove, rotate
+  include 'rng.inc'
   implicit none
 
   type, extends(particledat) :: point
@@ -9,6 +11,10 @@ module m_point
      procedure :: downcast_assign => point_downcast_assign
      procedure :: point_assign
      generic :: assignment(=) => point_assign
+     procedure :: point_equals
+     generic :: operator(==) => point_equals
+     procedure, nopass :: typestr => point_typestr
+     procedure :: move => point_move
   end type point
 
 contains
@@ -19,6 +25,11 @@ contains
     integer, intent(out) :: ios
     read(str, fmt=*, iostat=ios) this%x, this%y, this%z
   end subroutine point_from_str
+  
+  subroutine point_typestr(str)
+    character(len=:), allocatable, intent(out) :: str
+    str = "point"
+  end subroutine point_typestr
   
   pure subroutine point_downcast_assign(this, a_particle, err)
     class(point), intent(inout) :: this
@@ -39,5 +50,23 @@ contains
     this%y = another%y
     this%z = another%z
   end subroutine point_assign
+
+  elemental function point_equals(this, another) result(res)
+    class(point), intent(in) :: this
+    type(point), intent(in) :: another
+    logical :: res
+    res = (this%x == another%x) .and. (this%y == another%y) .and. &
+         (this%z == another%z)
+  end function point_equals
+  
+  pure subroutine point_move(this, genstate)    
+    class(point), intent(inout) :: this
+    type(rngstate), intent(inout) :: genstate
+    real(dp) :: xn, yn, zn, uxn, uyn, uzn
+    call transmove(this%x, this%y, this%z, xn, yn, zn, genstate)
+    this%x = xn
+    this%y = yn
+    this%z = zn
+  end subroutine point_move
 
 end module m_point
--- gbcyl/src/m_rod.f90
+++ gbcyl/src/m_rod.f90
@@ -1,6 +1,10 @@
 module m_rod
+  use iso_fortran_env, only: output_unit
   use particle, only: particledat
   use num_kind, only: dp
+  use utils, only: fmt_char_dp
+  use particle_mover, only: transmove, rotate
+  include 'rng.inc'
   implicit none
   
   type, extends(particledat) :: rod
@@ -9,6 +13,11 @@ module m_rod
      procedure :: downcast_assign => rod_downcast_assign
      procedure :: rod_assign
      generic :: assignment(=) => rod_assign
+     procedure :: rod_equals
+     generic :: operator(==) => rod_equals
+     procedure, nopass :: typestr => rod_typestr
+     procedure :: to_stdout => rod_to_stdout
+     procedure :: move => rod_move
   end type rod
 
 contains
@@ -20,7 +29,12 @@ contains
     read(str, fmt=*, iostat=ios) this%x, this%y, this%z, this%ux, &
          this%uy, this%uz
   end subroutine rod_from_str
-  
+
+  subroutine rod_typestr(str)
+    character(len=:), allocatable, intent(out) :: str
+    str = "rod"
+  end subroutine rod_typestr
+
   pure subroutine rod_downcast_assign(this, a_particle, err)
     class(rod), intent(inout) :: this
     class(particledat), intent(in) :: a_particle
@@ -43,5 +57,34 @@ contains
     this%uy = another%uy
     this%uz = another%uz
   end subroutine rod_assign
+
+  elemental function rod_equals(this, another) result(res)
+    class(rod), intent(in) :: this
+    type(rod), intent(in) :: another
+    logical :: res
+    res = (this%x == another%x) .and. (this%y == another%y) .and. &
+         (this%z == another%z) .and. (this%ux == another%ux) .and. &
+         (this%uy == another%uy) .and. (this%uz == another%uz)
+  end function rod_equals
+  
+  subroutine rod_to_stdout(this)
+    class(rod), intent(in) :: this
+    write(output_unit, '(6(' // fmt_char_dp() // ',1X))', advance='no') &
+         this%x, this%y, this%z, this%ux, this%uy, this%uz 
+  end subroutine rod_to_stdout
+
+  pure subroutine rod_move(this, genstate)    
+    class(rod), intent(inout) :: this
+    type(rngstate), intent(inout) :: genstate
+    real(dp) :: xn, yn, zn, uxn, uyn, uzn
+    call transmove(this%x, this%y, this%z, xn, yn, zn, genstate)
+    this%x = xn
+    this%y = yn
+    this%z = zn
+    call rotate(this%ux, this%uy, this%uz, uxn, uyn, uzn, genstate)
+    this%ux = uxn
+    this%uy = uyn
+    this%uz = uzn
+  end subroutine rod_move
 
 end module m_rod
