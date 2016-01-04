e1ed97d3400f4ec5b88a7844701a4e6e2496ce9c
jounik <>
2016-01-04 16:00:55 +0000

    Particledat was refactored out of particle. Added default return value for create_pair_interaction. Removed old conditional_pair_interaction.

--- gbcyl/src/m_interaction_factory.f90
+++ gbcyl/src/m_interaction_factory.f90
@@ -1,7 +1,6 @@
 module m_interaction_factory
   use iso_fortran_env
-  use class_pair_potential, only: conditional_pair_interaction
-  use particle, only: pair_interaction, single_interaction
+  use m_particle, only: pair_interaction, single_interaction
   use m_gb_interaction, only: gb_interaction
   use m_lj_interaction, only: lj_interaction
   use m_gblj_interaction, only: gblj_interaction
@@ -15,10 +14,9 @@ contains
     type(json_value), pointer, intent(in) :: json_val
     class(pair_interaction), pointer :: res
     character(kind=CK, len=:), allocatable :: typestr
+    res => null()
     call json_get(json_val, "type", typestr)
-    if (typestr == 'conditional_pair_interaction') then
-       allocate(res, source=conditional_pair_interaction(json_val))
-    else if (typestr == 'gayberne') then
+    if (typestr == 'gayberne') then
        allocate(res, source=gb_interaction(json_val))
     else if (typestr == 'lj') then
        allocate(res, source=lj_interaction(json_val))
