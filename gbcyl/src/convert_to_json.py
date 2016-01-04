9075e5878f869f59d8fc75c95ef919ee641d5cf1
jounik <>
2016-01-04 20:27:59 +0000

    Added outputfile option. Added 'n'. Corrected lj group.

--- gbcyl/src/convert_to_json.py
+++ gbcyl/src/convert_to_json.py
@@ -1,8 +1,14 @@
 #-*- coding: utf-8 -*-
 import sys
+import json
 
 def main():
     filename = sys.argv[1]
+    if len(sys.argv) > 2:
+        try:
+            outfile = open(sys.argv[2], 'w')
+        except:
+            outfile = None
     f = open(filename, 'r')
     i_configuration = 0
     rodgroup = {}
@@ -12,23 +18,27 @@ def main():
 
     pointgroup = {}
     pointgroup['type'] = 'point'
-    pointgroup['name'] = 'gb'
-    pointgroup['description'] = ['x', 'y', 'z', 'ux', 'uy', 'uz']
-    while True:
-        line = f.readline()
+    pointgroup['name'] = 'lj'
+    pointgroup['description'] = ['x', 'y', 'z']
+    for line in f:
         if line.startswith('#configuration'):
             i_configuration += 1
             rodgroup['coordinates'] = []
             pointgroup['coordinates'] = []
         elif line.startswith('#end_configuration'):
             particle_groups = [rodgroup]
+            rodgroup['n'] = len(rodgroup['coordinates'])
             if (len(pointgroup['coordinates']) > 0):
+                pointgroup['n'] = len(pointgroup['coordinates'])
                 particle_groups.append(pointgroup)
-            json.dumps({'particle_groups': particle_groups})
+            if outfile is not None:
+                json.dump({'particle_groups': particle_groups}, outfile, sort_keys=False, indent=4, separators=(',', ': '))
+            else:
+                print json.dumps({'particle_groups': particle_groups}, sort_keys=False, indent=4, separators=(',', ': '))
         elif line.startswith('gb'):
             rodgroup['coordinates'].append(line.split()[1:])
         elif line.startswith('lj'):
             pointgroup['coordinates'].append(line.split()[1:4])
-
+            
 if __name__=='__main__':
     main()
