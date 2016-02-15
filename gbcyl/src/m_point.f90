a2368eca779530d5661eb41480302f952a1f1712
jounik <>
2016-02-15 08:38:54 +0000

    Updated documentation.

--- gbcyl/src/m_point.f90
+++ gbcyl/src/m_point.f90
@@ -1,3 +1,4 @@
+!> Implements the simplest concrete particle type, the point.
 module m_point
   use m_particle, only: particle
   use num_kind, only: dp
@@ -8,42 +9,63 @@ module m_point
   use utils, only: fmt_char_dp
   implicit none
 
+  !> Point-like particle.
   type, extends(particle) :: point
    contains
+     !> Deserializes the coordinates from a string.
      procedure :: from_str => point_from_str
+     !> Downcasts a particle to a point and assigns
      procedure :: downcast_assign => point_downcast_assign
+     !> Assignment operator implementation
      procedure :: point_assign
      generic :: assignment(=) => point_assign
+     !> Equality operator implementation.
      procedure :: point_equals
      generic :: operator(==) => point_equals
+     !> Returns the type of this particle as a string.
      procedure, nopass :: typestr => point_typestr
+     !> Randomly translates the point to a new position.
      procedure :: move => point_move
+     !> Writes point coordinates to a JSON value.
      procedure :: coordinates_to_json => point_coordinates_to_json
+     !> Deserializes point coordinates from json.
      procedure :: from_json => point_from_json
+     !> Returns the names of the point coordinates.
      procedure, nopass :: description => point_description
+     !> Serializes the point to the given Fortran output unit.
      procedure :: to_unit => point_to_unit
   end type point
 
 
 contains
+
   
+  !> Deserializes @p this point from @p str. ios /= 0 if an error
+  !! occurs.
   subroutine point_from_str(this, str, ios)
     class(point), intent(inout) :: this
     character(len=*), intent(in) :: str
     integer, intent(out) :: ios
     read(str, fmt=*, iostat=ios) this%x, this%y, this%z
   end subroutine point_from_str
-  
+
+
+  !> Returns the type of the point in @p str.
   subroutine point_typestr(str)
     character(len=:), allocatable, intent(out) :: str
     str = "point"
   end subroutine point_typestr
-  
+
+
+  !> Returns the names of the point coordinates in @p descr.
   subroutine point_description(descr)
     character(kind=CK, len=3), allocatable, intent(inout) :: descr(:)
     descr = ["x  ", "y  ", "z  "]
   end subroutine point_description
 
+
+  !> Downcasts @p a_particle to a point and assigns it to @p this.
+  !! err = 3 if an error occurs.
   pure subroutine point_downcast_assign(this, a_particle, err)
     class(point), intent(inout) :: this
     class(particle), intent(in) :: a_particle
@@ -57,6 +79,8 @@ contains
     end select
   end subroutine point_downcast_assign
 
+
+  !> Assigns @p another to @p this.
   pure subroutine point_assign(this, another)
     class(point), intent(inout) :: this
     type(point), intent(in) :: another
@@ -65,6 +89,8 @@ contains
     this%z = another%z
   end subroutine point_assign
 
+
+  !> Returns true if coordinates of @p another are equal to @p this.
   elemental function point_equals(this, another) result(res)
     class(point), intent(in) :: this
     type(point), intent(in) :: another
@@ -72,7 +98,10 @@ contains
     res = (this%x == another%x) .and. (this%y == another%y) .and. &
          (this%z == another%z)
   end function point_equals
-  
+
+
+  !> Translates @p this randomly to a new position. @p genstate is the
+  !! random number generator used.
   pure subroutine point_move(this, genstate)    
     class(point), intent(inout) :: this
     type(rngstate), intent(inout) :: genstate
@@ -83,6 +112,9 @@ contains
     this%z = zn
   end subroutine point_move
 
+
+  !> Serializes the coordinates of @p this point to JSON value
+  !! @p json_val.
   subroutine point_coordinates_to_json(this, json_val)
     class(point), intent(in) :: this
     type(json_value), pointer :: json_val
@@ -92,6 +124,8 @@ contains
     call json_add(json_val, '', this%z)
   end subroutine point_coordinates_to_json
 
+
+  !> Deserializes @p this point from JSON value @p json_val.
   subroutine point_from_json(this, json_val)
     class(point), intent(inout) :: this
     type(json_value), pointer, intent(in) :: json_val
@@ -100,6 +134,8 @@ contains
     call get_parameter(json_val, '[3]', this%z)
   end subroutine point_from_json
 
+
+  !> Writes @p this point to @p unit.
   subroutine point_to_unit(this, unit)
     class(point), intent(in) :: this
     integer, intent(in) :: unit
