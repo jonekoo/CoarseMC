3e9c6234755599e39ebd9b7873f6583e82045382
jounik <>
2016-02-29 18:07:44 +0000

    Enhanced documentation. Added the routine gblj_sample to produce plots of the potential values.

--- gbcyl/src/m_gblj_interaction.f90
+++ gbcyl/src/m_gblj_interaction.f90
@@ -1,3 +1,5 @@
+!> Contains the GB-LJ interaction wrapper gblj_interaction and related
+!! procedures.
 module m_gblj_interaction
   use m_gblj
   use m_particle, only: pair_interaction, particle
@@ -9,22 +11,42 @@ module m_gblj_interaction
   use m_point, only: point
   implicit none
 
+  !> Wraps the GB-LJ potential as a pair_interaction.
   type, extends(pair_interaction) :: gblj_interaction
+
+     !> The GB-LJ potential.
      type(gblj_potential) :: pef
+
+     !> The cutoff radius of the interaction.
      real(dp) :: cutoff = 5.5
+
    contains
+
+     !> Computes the GB-LJ interaction energy.
      procedure :: pair_potential => gblj_pair_potential
+
+     !> Computes the force acting between a pair of particles.
      procedure :: pair_force => gblj_pair_force
+
+     !> Returns the cutoff radius of this interaction.
      procedure :: get_cutoff => gblj_get_cutoff
+
+     !> Serializes this interaction to JSON.
      procedure :: to_json => gblj_to_json
+
+     !> Returns a sample of the possible potential energies of the
+     !! interaction.
+     procedure :: sample => gblj_sample
   end type gblj_interaction
 
+  !> Constructor interface.
   interface gblj_interaction
      module procedure gblj_interaction_from_json
   end interface gblj_interaction
   
 contains
 
+  !> Deserializes the GB-LJ interaction from the JSON value @p json_val. 
   function gblj_interaction_from_json(json_val) result(this)
     type(json_value), pointer, intent(in) :: json_val
     type(gblj_interaction) :: this
@@ -32,6 +54,8 @@ contains
     this%pef = gblj_potential(json_val)
   end function gblj_interaction_from_json
 
+
+  !> Serializes @p this interactions to JSON value @p json_val.
   subroutine gblj_to_json(this, json_val)
     class(gblj_interaction), intent(in) :: this
     type(json_value), pointer, intent(inout) :: json_val
@@ -39,6 +63,10 @@ contains
     call this%pef%to_json(json_val)
   end subroutine gblj_to_json
   
+  !> Computes the @p energy of @p this interaction between @p particlei
+  !! and @p particlej. @p rij is the vector from @p particlei to 
+  !! @p particlej. @p err = 1 if the particles overlap. @p err = 79 if
+  !! neither @p particlei or @p particlej is a rod.
   pure subroutine gblj_pair_potential(this, particlei, particlej, rij, &
        energy, err)
     class(gblj_interaction), intent(in) :: this
@@ -50,7 +78,8 @@ contains
     err = 0
     select type (particlei)
     class is (rod)
-          call this%pef%potential(particlei%orientation(), rij, energy, overlap)
+          call this%pef%potential(particlei%orientation(), rij, energy, &
+               overlap)
           if (overlap) err = 1
     class default
        select type (particlej)
@@ -64,12 +93,18 @@ contains
     end select
   end subroutine gblj_pair_potential
 
+
+  !> Returns the cutoff radius of @p this interaction.
   pure function gblj_get_cutoff(this) result(cutoff)
     class(gblj_interaction), intent(in) :: this
     real(dp) :: cutoff
     cutoff = this%cutoff
   end function gblj_get_cutoff
 
+
+  !> Returns the force acting on @p particlei by @p particlej due to
+  !! @p this interaction between them. @p rij is the vector from
+  !! @p particlei to @p particlej.
   pure function gblj_pair_force(this, particlei, particlej, rij) result(f)
     class(gblj_interaction), intent(in) :: this
     class(particle), intent(in) :: particlei, particlej
@@ -90,4 +125,44 @@ contains
     end select
   end function gblj_pair_force
 
+
+  !> Produces a sample of the possible potential energies with @p this
+  !! interaction with rod-to-point distances @p r. The results are
+  !! stored to JSON value @p json_val. Datasets are created for rods
+  !! oriented along x and y axes.
+  subroutine gblj_sample(this, json_val, r)
+    class(gblj_interaction), intent(in) :: this
+    type(json_value), pointer :: json_val
+    type(rod) :: rodsamples(2)
+    type(point) :: lj
+    real(dp), intent(in) :: r(:)
+    type(json_value), pointer :: child
+    real(dp) :: energy
+    integer :: err, i, j, k
+    type(json_value), pointer :: r_json, energy_json
+    character(len=3, kind=CK), parameter :: descriptions(2) = &
+         ['GBx', 'GBy']
+
+    call rodsamples(1)%set_orientation([1._dp, 0._dp, 0._dp])
+    call rodsamples(2)%set_orientation([0._dp, 1._dp, 0._dp])
+    call json_create_object(json_val, 'gblj_interaction')
+    do i = 1, size(rodsamples)   
+       call json_create_object(child, descriptions(i) // '-LJ')
+       call json_create_array(r_json, 'x')
+       call json_create_array(energy_json, 'energy')
+       do k = 1, size(r)
+          call this%pair_potential(rodsamples(i), lj, &
+               [r(k), 0._dp, 0._dp], energy, err)
+          if (err == 0) then
+             call json_add(r_json, '', r(k))
+             call json_add(energy_json, '', energy)
+          end if
+       end do
+       call json_add(child, r_json)
+       call json_add(child, energy_json)
+       call json_add(json_val, child)
+    end do
+  end subroutine gblj_sample
+
+  
 end module m_gblj_interaction
