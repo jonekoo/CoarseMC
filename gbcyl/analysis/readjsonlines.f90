6fdecfdab2d57a800ffa7fc2b4ec273caf6e5304
jounik <>
2016-03-16 18:27:26 +0000

    The starting point for the new analysis program.

---
+++ gbcyl/analysis/readjsonlines.f90
@@ -0,0 +1,47 @@
+program readjsonlines
+  use json_module
+  use iso_fortran_env, only: error_unit
+  implicit none
+  character(len=:), allocatable :: line
+  integer :: u_json = 11
+  open(unit=u_json, file='test.json', action='READ')
+
+  do while(ReadLine(u_json, line))
+     write(*, '(A)') line
+  end do
+
+contains
+
+  ! http://stackoverflow.com/a/21953596
+  function ReadLine(aunit, InLine, trimmed) result(OK)
+    integer, intent(IN) :: aunit
+    character(LEN=:), allocatable, optional, intent(out) :: InLine
+    logical, intent(in), optional :: trimmed
+    integer, parameter :: line_buf_len= 1024*4
+    character(LEN=line_buf_len) :: InS
+    logical :: OK, set
+    integer status, size
+    
+    OK = .false.
+    set = .true.
+    do
+       read (aunit,'(a)',advance='NO',iostat=status, size=size) InS
+       OK = .not. IS_IOSTAT_END(status)
+       if (.not. OK) return
+       if (IS_IOSTAT_END(status)) return
+       if (present(InLine)) then
+          if (set) then
+             InLine = InS(1:size)
+             set=.false.
+          else
+             InLine = InLine // InS(1:size)
+          end if
+       end if
+       if (IS_IOSTAT_EOR(status)) exit
+    end do
+    if (present(trimmed) .and. present(InLine)) then
+       if (trimmed) InLine = trim(adjustl(InLine))
+    end if
+    
+  end function ReadLine
+end program readjsonlines
