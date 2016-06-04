bc94e749d51b3f7d52b94b0bb01a07df0081bc83
jonekoo <jouni.m.karjalainen@gmail.com>
2016-06-04 23:24:38 +0300

    Support for changing the run command from mpirun -np to e.g. srun -n in tests.

--- gbcyl/tests/test_samename.py
+++ gbcyl/tests/test_samename.py
@@ -1,15 +1,21 @@
+#!/usr/bin/env python 
 #-*- coding: utf-8 -*-
+
 """
 Check that the program fails if two groups have the same name.
 """
 import subprocess
 import os.path
 import sys
+import subprocess
 
 def main():
-    if subprocess.call(["mpirun", "-np", "1",
-                        os.path.join("..", "src", "ptgbcyl")]) == 1:
-        sys.exit(0)
+    runcmd = os.environ["RUN_CMD"].replace('"', '')
+    args = runcmd.split() + ["1", os.path.join("..", "src", "ptgbcyl")]
+    p = subprocess.Popen(args, stderr=subprocess.PIPE)
+    for line in p.stderr:
+        if 'error' in line.lower() and 'same name' in line.lower():
+            sys.exit(0)
     else:
         sys.exit(1)
 
