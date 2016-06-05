4ed271d09a4321f6465db4a2e9cfe7de612c99cf
Jouni Karjalainen <jouni.m.karjalainen@gmail.com>
2016-06-05 20:01:49 +0300

    Run command can now be changed.

--- gbcyl/tests/test_missinginput.py
+++ gbcyl/tests/test_missinginput.py
@@ -3,6 +3,7 @@ import json
 import subprocess
 import sys
 import os.path
+import os
 
 
 def main():
@@ -15,8 +16,9 @@ def main():
     with open('test_missinginput-0.json', 'w') as f:
         json.dump(d, f)
     # Run program with input. Catch output.
-    args = ["mpirun", "-np", "1", os.path.join("..", "src", "ptgbcyl"), "-i",
-            "test_missinginput-_I_.json"]
+    runcmd = os.environ['RUN_CMD'].replace('"', '')
+    args = runcmd.split() + ["1", os.path.join("..", "src", "ptgbcyl"), "-i",
+                             "test_missinginput-_I_.json"]
     proc = subprocess.Popen(args, stderr=subprocess.PIPE)
     for line in proc.stderr:
         if "error" in line.lower() and "particle_groups" in line:
